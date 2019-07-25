88086e6d2a37823fbfd23e70ed2ee14be664a739
Timothy Gebhard <mail@timothygebhard.de>
2019-07-25 16:46:23 +0200

    Initialize hsr4hci package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='hsr4hci',
+      version='epsilon',
+      description='hsr4hci: Half-Sibling Regression for High-Contrast Imaging',
+      url='https://github.com/timothygebhard/hsr4hci',
+      install_requires=['astropy',
+                        'h5py',
+                        'matplotlib',
+                        'numpy',
+                        'scikit-learn',
+                        'tqdm'],
+      packages=['hsr4hci'],
+      zip_safe=False)
