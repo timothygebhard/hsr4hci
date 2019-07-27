682c0aac368c259939f239284cbd4af05936d46d
majobodo <m.bonse@gmx.de>
2019-07-27 12:12:07 +0200

    Classical mean and median ADI as util functions

---
+++ hsr4hci/utils/adi_tools.py
@@ -0,0 +1,51 @@
+"""
+Tools for classical angular differential imaging such as frame rotation and
+residual creation
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+import numpy as np
+from astropy.io import fits
+import h5py
+from scipy.ndimage import rotate
+import matplotlib.pyplot as plt
+
+# -----------------------------------------------------------------------------
+# FUNCTION DEFINITIONS
+# -----------------------------------------------------------------------------
+
+
+def de_rotate_frames(stack: np.ndarray,
+                     para_angles: np.ndarray) -> np.ndarray:
+
+    rotated = np.zeros_like(stack)
+    for i in range(stack.shape[0]):
+        rotated[i, :, :] = rotate(np.nan_to_num(stack[i, :, :]),
+                                  -para_angles[i],
+                                  reshape=False)
+
+    return rotated
+
+
+def classical_adi(stack: np.ndarray,
+                  para_angles: np.ndarray,
+                  mean=False) -> np.ndarray:
+
+    # Create the PSF model
+    if mean:
+        psf_frame = np.mean(stack, axis=0)
+    else:
+        psf_frame = np.median(stack, axis=0)
+
+    subtracted = stack - psf_frame
+
+    residual_frames = de_rotate_frames(subtracted, para_angles)
+
+    return np.mean(residual_frames, axis=0)
+
+
+
+
