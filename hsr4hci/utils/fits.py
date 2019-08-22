7dc7b40543cd173df178e879204f4a145b2076db
Timothy Gebhard <mail@timothygebhard.de>
2019-08-22 11:56:02 +0200

    Add support for extended header information to save_fits function

--- hsr4hci/utils/fits.py
+++ hsr4hci/utils/fits.py
@@ -9,6 +9,8 @@ Functions for reading and writing FITS files.
 from astropy.io import fits
 import numpy as np
 
+from typing import Optional
+
 
 # -----------------------------------------------------------------------------
 # FUNCTION DEFINITIONS
@@ -33,6 +35,7 @@ def read_fits(file_path: str) -> np.ndarray:
 
 def save_fits(array: np.ndarray,
               file_path: str,
+              header: Optional[dict] = None,
               overwrite: bool = True):
     """
     Save a numpy array as a FITS file (e.g., to inspect it with DS9).
@@ -40,9 +43,25 @@ def save_fits(array: np.ndarray,
     Args:
         array: The numpy array to be saved to a FITS file.
         file_path: The path where to save the FITS file.
+        header: A dictionary with additional header information.
         overwrite: Whether or not to overwrite an existing FITS file.
     """
 
+    # Create a new HDU for the array
     hdu = fits.PrimaryHDU(array)
-    hdulist = fits.HDUList([hdu])
-    hdulist.writeto(file_path, overwrite=overwrite)
+
+    # If applicable, add header information
+    if header is not None:
+        for key, value in header.items():
+
+            # Take special care of NaN, because FITS can't deal with them
+            if isinstance(value, (list, tuple, np.ndarray)):
+                value = tuple(['NaN' if np.isnan(_) else _ for _ in value])
+            else:
+                value = 'NaN' if np.isnan(value) else value
+
+            # Save value to HDU header
+            hdu.header[key] = value
+
+    # Save the HDU to the specified FITS file
+    fits.HDUList([hdu]).writeto(file_path, overwrite=overwrite)
