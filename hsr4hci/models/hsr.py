254b4bbc2a69d2b595236ed13cf58901b7af757e
Timothy Gebhard <mail@timothygebhard.de>
2019-09-02 15:34:27 +0200

    Add forward modeling to HSR model

--- hsr4hci/models/hsr.py
+++ hsr4hci/models/hsr.py
@@ -12,6 +12,8 @@ import os
 import warnings
 
 from hsr4hci.models.prototypes import ModelPrototype
+from hsr4hci.utils.forward_modeling import get_signal_stack, \
+    get_collection_region_mask, get_collection_region_pixels
 from hsr4hci.utils.model_loading import get_class_by_name
 from hsr4hci.utils.predictor_selection import get_predictor_mask
 from hsr4hci.utils.roi_selection import get_roi_mask, get_roi_pixels
@@ -19,7 +21,7 @@ from hsr4hci.utils.roi_selection import get_roi_mask, get_roi_pixels
 from pathlib import Path
 from sklearn.decomposition import PCA
 from tqdm import tqdm
-from typing import Tuple
+from typing import Optional, Tuple
 
 
 # -----------------------------------------------------------------------------
@@ -28,7 +30,13 @@ from typing import Tuple
 
 class HalfSiblingRegression(ModelPrototype):
     """
-    Wrapper class for a half-sibling regression model.
+    Wrapper class for a half-sibling regression model. This class
+    essentially encapsulates the "outer loop", that is, looping over
+    every pixel in the (spatial) region of interest and learning a
+    model (or a collection of models) for it.
+    
+    Args:
+        config: A dictionary containing the experiment configuration
     """
 
     def __init__(self,
@@ -38,155 +46,252 @@ class HalfSiblingRegression(ModelPrototype):
         self.m__pixscale = config['dataset']['pixscale']
         self.m__roi_ier = config['experiment']['roi']['inner_exclusion_radius']
         self.m__roi_oer = config['experiment']['roi']['outer_exclusion_radius']
-        self.m__model_config = config['experiment']['model']
+        self.m__config_collection = config['experiment']['collection']
+        self.m__config_model = config['experiment']['model']
 
         # Define shortcuts to config elements
         self.m__experiment_dir = config['experiment_dir']
-        self.m__mask_size = (int(config['dataset']['x_size']),
-                             int(config['dataset']['y_size']))
+        self.m__frame_size = config['dataset']['frame_size']
 
         # Get implicitly defined class variables
-        self.m__roi_mask = get_roi_mask(mask_size=self.m__mask_size,
+        self.m__roi_mask = get_roi_mask(mask_size=self.m__frame_size,
                                         pixscale=self.m__pixscale,
                                         inner_exclusion_radius=self.m__roi_ier,
                                         outer_exclusion_radius=self.m__roi_oer)
 
         # Define a models directory and ensure it exists
-        self.m__models_dir = os.path.join(self.m__experiment_dir, 'models')
-        Path(self.m__models_dir).mkdir(exist_ok=True)
+        self.m__models_root_dir = \
+            os.path.join(self.m__experiment_dir, 'models')
+        Path(self.m__models_root_dir).mkdir(exist_ok=True)
 
         # Initialize a dict that will hold all pixel models
-        self.m__predictors = dict()
+        self.m__collections = dict()
+
+    def get_detection_map(self):
+
+        detection_map = np.full(self.m__frame_size, np.nan)
+        for position, collection in self.m__collections.items():
+            detection_map[position] = collection.get_average_signal_coef()
+        return detection_map
 
     def train(self,
-              training_stack: np.ndarray):
+              stack: np.ndarray,
+              parang: Optional[np.ndarray],
+              psf_template: Optional[np.ndarray]):
 
         # Get positions of pixels in ROI
         roi_pixels = get_roi_pixels(self.m__roi_mask)
 
-        # Train a model for every position
+        # Process every position
         for position in tqdm(roi_pixels, total=len(roi_pixels), ncols=80):
             self.train_position(position=position,
-                                training_stack=training_stack)
+                                stack=stack,
+                                parang=parang,
+                                psf_template=psf_template)
 
     def train_position(self,
-                       position: Tuple[int],
-                       training_stack: np.ndarray):
+                       position: Tuple[int, int],
+                       stack: np.ndarray,
+                       parang: np.ndarray,
+                       psf_template: np.ndarray):
 
-        # Get sources mask
-        mask = get_predictor_mask(mask_size=self.m__mask_size,
-                                  position=position,
-                                  n_regions=1,
-                                  region_size=5)
+        # Create a PixelPredictorCollection for this position
+        collection = \
+            PixelPredictorCollection(position=position,
+                                     config_collection=self.m__config_collection,
+                                     config_model=self.m__config_model)
 
-        # Select sources (predictor pixels) and targets from stack
-        sources = training_stack[:, mask]
-        targets = training_stack[:, position[0], position[1]]
+        # Train and save the collection for this position
+        collection.train_collection(stack=stack,
+                                    parang=parang,
+                                    psf_template=psf_template)
 
-        # Train and save a predictor for this position
-        predictor = PixelPredictor(position=position,
-                                   model_config=self.m__model_config)
-        predictor.train(sources=sources, targets=targets)
-        predictor.save(models_dir=self.m__models_dir)
-
-        # Add to dictionary of trained predictors
-        self.m__predictors[position] = predictor
+        # Add to dictionary of trained collections
+        self.m__collections[position] = collection
 
     def predict(self,
-                test_stack: np.ndarray):
-
-        # Instantiate empty array to hold all predictions
-        predictions = np.full(test_stack.shape, np.nan)
-
-        # Loop over all ROI positions / models
-        for position, predictor in tqdm(self.m__predictors.items(), ncols=80,
-                                        total=len(self.m__predictors)):
-
-            # Get sources mask
-            mask = get_predictor_mask(mask_size=self.m__mask_size,
-                                      position=position,
-                                      n_regions=1,
-                                      region_size=5)
-            sources = test_stack[:, mask]
-
-            # Make prediction
-            predictions[:, position[0], position[1]] = \
-                predictor.predict(sources=sources)
-
-        return predictions
+                stack: np.ndarray):
+        raise NotImplementedError()
 
     def load(self):
 
         # Get positions of pixels in ROI
         roi_pixels = get_roi_pixels(self.m__roi_mask)
 
-        # Load model for every position in the ROI
+        # Load collection for every position in the ROI
         for position in roi_pixels:
-            predictor = PixelPredictor(position=position,
-                                       model_config=self.m__model_config)
-            predictor.load(models_dir=self.m__models_dir)
-            self.m__predictors[position] = predictor
+            collection = \
+                PixelPredictorCollection(position=position,
+                                         config_collection=self.m__config_collection,
+                                         config_model=self.m__config_model)
+            collection.load(models_root_dir=self.m__models_root_dir)
+            self.m__collections[position] = collection
 
     def save(self):
 
-        # Save all predictors
-        for _, predictor in self.m__predictors.items():
-            predictor.save(models_dir=self.m__models_dir)
+        # Save all PixelPredictorCollections
+        for _, collection in self.m__collections.items():
+            collection.save(models_root_dir=self.m__models_root_dir)
 
-    def get_pca_of_coefficients(self,
-                                n_components: int = 3,
-                                normalize: bool = False) -> np.ndarray:
-        """
-        Run PCA on the coefficients of the pixel predictors. This
-        dimensionality reduction is useful to investigate if predictors
-        that are spatially close also have similar components (i.e.,
-        they have learned a similar relation between input and output).
+
+# -----------------------------------------------------------------------------
+
+
+class PixelPredictorCollection(object):
+    """
+    Wrapper class ...
+    """
+
+    def __init__(self,
+                 position: Tuple[int, int],
+                 config_collection: dict,
+                 config_model: dict):
+
+        self.m__position = position
+        self.m__predictors = dict()
+        self.m__config_model = config_model
+        self.m__config_collection = config_collection
+        self.m__collection_region = None
         
-        Args:
-            n_components: Number of principal components to project
-                onto. Default is 3, as it allows to interpret the
-                result as an RGB color.
-            normalize: Whether or not to normalize all values in the
-                result into the [0, 1] interval.
+        self.m__collection_name = \
+            f'collection_{self.m__position[0]}_{self.m__position[1]}'
 
-        Returns:
-            A numpy array of shape (n_components, width, height) that
-            contains the dimensionality-reduced model coefficients of
-            every pixel predictor.
-        """
+    def get_average_signal_coef(self):
 
-        # Check if we have some models on which we can run PCA.
-        # Empty dictionaries are evaluated as False by Python.
-        if not self.m__predictors:
-            raise ValueError("No local predictors found. "
-                             "Please train or load models first.")
+        # Get signal coefficients and their uncertainties for all pixel
+        # predictors in the collection
+        signal_coefs = list()
+        signal_sigma_coefs = list()
+        for _, pixel_predictor in self.m__predictors.items():
+            w_p, sigma_p = pixel_predictor.get_signal_coef()
+            signal_coefs.append(w_p)
+            signal_sigma_coefs.append(sigma_p)
+            
+        # Find pixels with very large uncertainties and exclude them
+        threshold = np.percentile(signal_sigma_coefs, 90)
+        excluded_idx = np.where(signal_sigma_coefs > threshold)[0]
+        signal_coefs = np.array(signal_coefs)[~excluded_idx]
+        signal_sigma_coefs = np.array(signal_sigma_coefs)[~excluded_idx]
 
-        # Collect coefficients for all pixel models
-        model_coeffs = {}
-        for position, model in self.m__predictors.items():
-            if model.m__model.coef_ is not None:
-                model_coeffs[position] = model.m__model.coef_
+        # Return the weighted average of the signal coefficients
+        return np.average(a=signal_coefs, weights=1/signal_sigma_coefs)
 
-        # Fit a PCA to the coefficients
-        pca_model = PCA(n_components)
-        pca_model.fit(np.array(list(model_coeffs.values())))
+    def train_collection(self,
+                         stack: np.ndarray,
+                         parang: np.ndarray,
+                         psf_template: np.ndarray):
+    
+        # ---------------------------------------------------------------------
+        # Define shortcuts
+        # ---------------------------------------------------------------------
 
-        # Create result array (default to NaN, which is ignored in plots)
-        result = np.full((n_components,
-                          self.m__mask_size[0],
-                          self.m__mask_size[1]), np.nan)
+        region_size = self.m__config_collection['predictor_region_radius']
+        variance_threshold = \
+            self.m__config_collection['explained_variance_threshold']
+        psf_radius = self.m__config_collection['psf_radius']
+        use_forward_model = self.m__config_collection['use_forward_model']
 
-        # For each pixel model, project the coefficients onto the PCs
-        for position, coeff in model_coeffs.items():
-            result[:, position[0], position[1]] = \
-                pca_model.transform(np.array(coeff).reshape(-1, 1))[0, :]
+        # ---------------------------------------------------------------------
+        # Get signal_stack and collection_region based on use_forward_model
+        # ---------------------------------------------------------------------
 
-        # If desired, normalize result to range [0, 1] to be used as colors
-        if normalize:
-            result = result - np.nanmin(result)
-            result /= np.nanmax(result)
+        if use_forward_model:
+            signal_stack = \
+                get_signal_stack(position=self.m__position,
+                                 frame_size=stack.shape[1:],
+                                 parang=parang,
+                                 psf_template=psf_template,
+                                 psf_radius=psf_radius)
+            collection_region_mask = get_collection_region_mask(signal_stack)
+            self.m__collection_region = \
+                get_collection_region_pixels(collection_region_mask)
+        else:
+            signal_stack = None
+            self.m__collection_region = [self.m__position]
 
-        return result
+        # ---------------------------------------------------------------------
+        # Loop over all positions in the collection region
+        # ---------------------------------------------------------------------
+        
+        for position in self.m__collection_region:
+
+            # Get regression target
+            targets = stack[:, position[0], position[1]]
+
+            # Get planet signal
+            planet_signal = signal_stack[:, position[0], position[1]]
+
+            # Get predictor pixels
+            predictor_mask = \
+                get_predictor_mask(mask_size=tuple(stack.shape[1:]),
+                                   position=position,
+                                   region_size=region_size)
+            sources = stack[:, predictor_mask]
+
+            # Run PCA on sources and truncate based on a threshold criterion
+            # on the explained variance of the principal components
+            pca = PCA()
+            sources = pca.fit_transform(X=sources)
+            n_components = np.where(np.cumsum(pca.explained_variance_ratio_) >
+                                    variance_threshold)[0][0] + 1
+            sources = sources[:, :n_components]
+
+            # Add planet signal from forward modeling to sources
+            sources = np.column_stack([sources,
+                                       np.ones(stack.shape[0]),
+                                       planet_signal.reshape(-1, 1)])
+
+            # Create a new pixel predictor
+            pixel_predictor = PixelPredictor(position=position,
+                                             config_model=self.m__config_model)
+
+            # Train pixel predictor
+            pixel_predictor.train(sources=sources,
+                                  targets=targets)
+
+            # Add trained PixelPredictor to PixelPredictorCollection
+            self.m__predictors[position] = pixel_predictor
+
+    def save(self,
+             models_root_dir: str):
+
+        # Create folder for all models in this collection
+        collection_dir = os.path.join(models_root_dir, self.m__collection_name)
+        Path(collection_dir).mkdir(exist_ok=True)
+
+        # Save file containing the positions of all pixels in the collection
+        # TODO: Maybe use a FITS file instead pickling the data?
+        file_path = os.path.join(collection_dir, 'positions.pkl')
+        joblib.dump(self.m__collection_region, filename=file_path)
+
+        # Loop over all PixelPredictors in the collection and save them
+        for _, pixel_predictor in self.m__predictors.items():
+            pixel_predictor.save(collection_dir=collection_dir)
+
+    def load(self,
+             models_root_dir: str):
+
+        # Construct name of directory that contains this collection, and check
+        # if it exists
+        collection_dir = os.path.join(models_root_dir, self.m__collection_name)
+        if not os.path.isdir(collection_dir):
+            raise NotADirectoryError(f'{collection_dir} does not exist!')
+
+        # Load positions that belong to this collection
+        file_path = os.path.join(collection_dir, 'positions.pkl')
+        self.m__collection_region = joblib.load(filename=file_path)
+
+        # Loop over all positions in the collection region and load
+        # corresponding pixel predictors
+        for position in self.m__collection_region:
+            pixel_predictor = \
+                PixelPredictor(position=position,
+                               config_model=self.m__config_model)
+            pixel_predictor.load(collection_dir=collection_dir)
+            self.m__predictors[position] = pixel_predictor
+
+
+# -----------------------------------------------------------------------------
 
 
 class PixelPredictor(object):
@@ -196,18 +301,23 @@ class PixelPredictor(object):
 
     def __init__(self,
                  position: tuple,
-                 model_config: dict):
+                 config_model: dict):
 
         # Store constructor arguments
         self.m__position = position
-        self.m__model_config = model_config
+        self.m__config_model = config_model
 
         # Create predictor name and placeholder for model
-        self.m__name = f'{position[0]}_{position[1]}__model.pkl'
+        self.m__name = f'model__{position[0]}_{position[1]}.pkl'
         self.m__model = None
 
+        self.sigma_coef_ = None
+
+    def get_signal_coef(self) -> Tuple[float, float]:
+        return self.coef_[-1], self.sigma_coef_[-1]
+
     @property
-    def coef_(self):
+    def coef_(self) -> Optional[np.ndarray]:
 
         # If the base model has a coef_ attribute (which only exists for
         # fitted models), we can return it; otherwise return None
@@ -221,12 +331,15 @@ class PixelPredictor(object):
 
         # Instantiate a new model according to the model_config
         model_class = \
-            get_class_by_name(module_name=self.m__model_config['module'],
-                              class_name=self.m__model_config['class'])
-        self.m__model = model_class(**self.m__model_config['parameters'])
+            get_class_by_name(module_name=self.m__config_model['module'],
+                              class_name=self.m__config_model['class'])
+        self.m__model = model_class(**self.m__config_model['parameters'])
 
         # Fit model to the training data
         self.m__model.fit(X=sources, y=targets)
+
+        # Compute uncertainties for coefficients
+        self.sigma_coef_ = np.diag(np.linalg.pinv(np.dot(sources.T, sources)))
 
     def predict(self,
                 sources: np.ndarray) -> np.ndarray:
@@ -234,16 +347,16 @@ class PixelPredictor(object):
         return self.m__model.predict(X=sources)
 
     def save(self,
-             models_dir: str):
+             collection_dir: str):
 
-        file_path = os.path.join(models_dir, self.m__name)
+        file_path = os.path.join(collection_dir, self.m__name)
         joblib.dump(self.m__model, filename=file_path)
 
     def load(self,
-             models_dir: str):
+             collection_dir: str):
 
         # Try to load the model for this predictor from its *.pkl file
-        file_path = os.path.join(models_dir, self.m__name)
+        file_path = os.path.join(collection_dir, self.m__name)
         if os.path.isfile(file_path):
             self.m__model = joblib.load(filename=file_path)
         else:
