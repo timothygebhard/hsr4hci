Fix crucial bug: sign error in forward model (wrong rotation direction)
(cherry picked from commit 07bdb8af772d0cd9a682c63b36fa6efc0f90bb72)

hsr4hci/utils/forward_modeling.py
@@ -131,7 +131,7 @@ def get_signal_stack(position: Tuple[int, int],
         # the exponential representation of 2D vectors) and convert the
         # resulting position back to Cartesian coordinates.
         theta = np.deg2rad(rotation_angles[i])
-        new_complex_position = r * np.exp(1j * (phi + theta))
+        new_complex_position = r * np.exp(1j * (phi - theta))
         injection_position = (np.imag(new_complex_position) + frame_center[0],
                               np.real(new_complex_position) + frame_center[1])
 