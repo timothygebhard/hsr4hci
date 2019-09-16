90074b89c8aa7393764aadfc5c95daf09e6b2986
Timothy Gebhard <mail@timothygebhard.de>
2019-09-16 21:47:31 +0200

    Add a script to compute figures of merit (SNR etc.) from a FITS file

---
+++ scripts/compute_snr.py
@@ -0,0 +1,121 @@
+"""
+Take a FITS file as input and compute the SNR for a given position.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+import argparse
+
+from hsr4hci.utils.fits import read_fits
+from hsr4hci.utils.evaluation import compute_figures_of_merit
+
+
+# -----------------------------------------------------------------------------
+# FUNCTION DEFINITIONS
+# -----------------------------------------------------------------------------
+
+def get_arguments() -> argparse.Namespace:
+    """
+    Parse and return the command line arguments.
+
+    Returns:
+        An `argparse.Namespace` object containing the command line
+        options that were passed to this script.
+    """
+
+    # Set up a parser
+    parser = argparse.ArgumentParser()
+
+    # Allow us to split the arguments into required and optional ones
+    # noinspection PyProtectedMember
+    # pylint: disable=protected-access
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+
+    # Add required arguments
+    required.add_argument('--file',
+                          metavar='PATH',
+                          type=str,
+                          help='Path to the FITS file containing the data',
+                          required=True)
+    required.add_argument('--position',
+                          metavar='X',
+                          nargs=2,
+                          type=int,
+                          help='Position for which to compute the SNR',
+                          required=True)
+
+    # Add optional arguments
+    optional.add_argument('--aperture-size',
+                          type=float,
+                          metavar='X',
+                          default=2.0,
+                          help='Aperture radius in pixels (default: 2.0)')
+    optional.add_argument('--optimize',
+                          type=str,
+                          default='fpf',
+                          metavar='STR',
+                          choices=['none', 'signal', 'noise_level', 'snr',
+                                   'fpf'],
+                          help='Quantity to optimize, must be in ["none", '
+                               '"signal", "noise_level", "snr", "fpf"] '
+                               '(default: "fpf")')
+
+    # Add mutually exclusive flags --ignore-neighbors / --include-neighbors
+    ignore_neighbors_parser = \
+        optional.add_mutually_exclusive_group(required=False)
+    ignore_neighbors_parser.add_argument('--ignore-neighbors',
+                                         dest='ignore_neighbors',
+                                         action='store_true',
+                                         help='Ignore neighboring apertures '
+                                              '(default)')
+    ignore_neighbors_parser.add_argument('--include-neighbors',
+                                         dest='ignore_neighbors',
+                                         action='store_false',
+                                         help='Include neighboring apertures')
+    optional.set_defaults(ignore_neighbors=True)
+
+    # Make sure optional arguments also show up when using --help
+    # noinspection PyProtectedMember
+    # pylint: disable=protected-access
+    parser._action_groups.append(optional)
+
+    # Parse the command line arguments and return the result
+    return parser.parse_args()
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    print('\nCOMPUTE FIGURES OF MERIT FROM FITS FILE\n', flush=True)
+
+    # Get command line arguments
+    args = get_arguments()
+
+    # Define shortcuts to arguments (and typecast)
+    file_path = args.file
+    position = tuple(args.position)
+    aperture_size = float(args.aperture_size)
+    ignore_neighbors = args.ignore_neighbors
+    optimize = args.optimize if args.optimize != 'none' else None
+
+    # Load the FITS file
+    frame = read_fits(file_path=file_path)
+
+    # Compute figures of merit
+    figures_of_merit = \
+        compute_figures_of_merit(frame=frame,
+                                 position=position,
+                                 aperture_size=aperture_size,
+                                 ignore_neighbors=ignore_neighbors,
+                                 optimize=optimize)
+
+    # Print results
+    for key, value in figures_of_merit.items():
+        print(f'{key:<20}{value}')
+    print()
