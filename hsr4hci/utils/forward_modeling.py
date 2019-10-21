eff0d9b8f88e3e181c86a22bed87690db3d5dbde
Timothy Gebhard <mail@timothygebhard.de>
2019-10-21 16:07:31 +0200

    Change get_signal_stack() to also return planet positions as a list

--- hsr4hci/utils/forward_modeling.py
+++ hsr4hci/utils/forward_modeling.py
@@ -81,7 +81,7 @@ def crop_psf_template(psf_template: np.ndarray,
 def get_signal_stack(position: Tuple[int, int],
                      frame_size: Tuple[int, int],
                      parang: np.ndarray,
-                     psf_cropped: np.ndarray) -> np.ndarray:
+                     psf_cropped: np.ndarray) -> Tuple[np.ndarray, list]:
     """
     Compute the forward model: Assume that at time t=0, the planet is
     at the given `position. The apparent motion of the sky (and thus `
@@ -103,9 +103,11 @@ def get_signal_stack(position: Tuple[int, int],
             PSF template, as it is returned by `crop_psf_template()`.
 
     Returns:
-        A numpy array containing the forward model (i.e., a stack of
-        frames where every frame contains only the "planet signal" at
-        the correct position), computed under the given assumptions.
+        A tuple consisting of a numpy array containing the forward model
+        (i.e., a stack of frames where every frame contains only the
+        "planet signal" at the correct position), computed under the
+        given assumptions, as well as a list of tuples (x, y) containing
+        the position of the planet in the forward model for every frame.
     """
 
     # Define some shortcuts
@@ -120,21 +122,24 @@ def get_signal_stack(position: Tuple[int, int],
     r, phi = polar(complex(position[1] - frame_center[1],
                            position[0] - frame_center[0]))
 
-    # Initialize empty signal stack
+    # Initialize empty signal stack and list of planet positions
     signal_stack = np.zeros((n_frames, frame_width, frame_height))
+    planet_positions = list()
 
     # Loop over all frames and add the cropped and masked PSF template at
     # the position that corresponds to the respective parallactic angle
     for i in range(n_frames):
 
-        # Compute injection position for this frame:
-        # Rotate the initial  position by the respective angle (using
-        # the exponential representation of 2D vectors) and convert the
-        # resulting position back to Cartesian coordinates.
+        # Compute injection position for the current frame: Rotate the initial
+        # position by the correct angle (using the exponential representation
+        # of 2D vectors) and convert the resulting position back to Cartesian
+        # coordinates. Store this value to a list that we will return along
+        # with the signal stack we have constructed.
         theta = np.deg2rad(rotation_angles[i])
         new_complex_position = r * np.exp(1j * (phi - theta))
         injection_position = (np.imag(new_complex_position) + frame_center[0],
                               np.real(new_complex_position) + frame_center[1])
+        planet_positions.append(injection_position)
 
         # Add cropped and masked PSF template at the injection_position.
         # The add_array_with_interpolation() function is able to automatically
@@ -145,7 +150,7 @@ def get_signal_stack(position: Tuple[int, int],
                                          array_small=psf_cropped,
                                          position=injection_position)
 
-    return signal_stack
+    return signal_stack, planet_positions
 
 
 def get_collection_region_mask(signal_stack: np.ndarray) -> np.ndarray:
