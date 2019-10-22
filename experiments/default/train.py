d80efa0c872f12a72c54fc20dfeef1d163f99b34
Timothy Gebhard <mail@timothygebhard.de>
2019-10-22 16:17:19 +0200

    Update default experiment to adjust to new way of pre-processing sources

--- experiments/default/train.py
+++ experiments/default/train.py
@@ -11,8 +11,11 @@ from pathlib import Path
 import os
 import time
 
+import numpy as np
+
 from hsr4hci.utils.config import load_config
 from hsr4hci.utils.data import load_data
+from hsr4hci.utils.fits import save_fits
 from hsr4hci.models.hsr import HalfSiblingRegression
 
 
@@ -47,14 +50,17 @@ if __name__ == '__main__':
     print(f'Stack size:\t {stack.shape}', flush=True)
     print(f'Model type:\t {config["experiment"]["model"]["module"]}.'
           f'{config["experiment"]["model"]["class"]}', flush=True)
-    print(f'pca_mode:  \t {config["experiment"]["sources"]["pca_mode"]}')
+
+    # -------------------------------------------------------------------------
+    # Ensure the results dir exists
+    # -------------------------------------------------------------------------
+
+    results_dir = os.path.join(config['experiment_dir'], 'results')
+    Path(results_dir).mkdir(exist_ok=True)
 
     # -------------------------------------------------------------------------
     # Set up and train model
     # -------------------------------------------------------------------------
-
-    print('\n\nTraining model:')
-    print(80 * '-')
 
     # Instantiate model with this config
     hsr = HalfSiblingRegression(config=config)
@@ -63,8 +69,44 @@ if __name__ == '__main__':
     print('\nTraining models for all pixels in ROI:', flush=True)
     hsr.train(stack=stack, parang=parang, psf_template=psf_template)
 
-    print(80 * '-')
-    print('\n')
+    # -------------------------------------------------------------------------
+    # Get the detection map and save it
+    # -------------------------------------------------------------------------
+
+    # Get detection map
+    print(f'\nComputing detection map:', flush=True)
+    detection_map = hsr.get_detection_map()
+
+    # Store the detection map to a FITS file
+    print(f'Saving detection map to FITS...', end=' ', flush=True)
+    fits_file_path = os.path.join(results_dir, 'detection_map.fits')
+    save_fits(array=detection_map, file_path=fits_file_path)
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Get the residual stack and save it
+    # -------------------------------------------------------------------------
+
+    # Get residual stack
+    print(f'\nComputing residual stack:', flush=True)
+    residual_stack = hsr.get_residual_stack(stack=stack)
+
+    # Store the residual stack to a FITS file
+    print(f'Saving residual stack to FITS...', end=' ', flush=True)
+    fits_file_path = os.path.join(results_dir, 'residual_stack.fits')
+    save_fits(array=residual_stack, file_path=fits_file_path)
+    print('Done!', flush=True)
+
+    # Compute average residual stack
+    print('Averaging residual stack...', end=' ', flush=True)
+    average_residuals = np.nanmedian(residual_stack, axis=0)
+    print('Done!', flush=True)
+
+    # Save average residual stack
+    print('Saving average residuals...', end=' ', flush=True)
+    fits_file_path = os.path.join(results_dir, 'average_residuals.fits')
+    save_fits(average_residuals, fits_file_path)
+    print('Done!', flush=True)
 
     # -------------------------------------------------------------------------
     # Postliminaries
