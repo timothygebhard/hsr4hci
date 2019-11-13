ccb70eceaa7d26e696bc2b4da6ea349108991345
Timothy Gebhard <mail@timothygebhard.de>
2019-11-13 18:33:28 +0100

    Fix csb -> condor_submit_bid (aliases generally do not work in scripts)

--- experiments/HR_8799/01_weight-mode_vs_add-planet-column/02_experiments/submit_experiments.sh
+++ experiments/HR_8799/01_weight-mode_vs_add-planet-column/02_experiments/submit_experiments.sh
@@ -3,7 +3,7 @@
 while read -r line ; do \
   printf "\nSUBMITTING FILE: " ;
   echo "$line/submit.sub" ; \
-  csb 25 "$line/submit.sub" ;
+  condor_submit_bid 25 "$line/submit.sub" ;
   printf "\nGoing to sleep for 10 minutes... " ;
   sleep 10m ;
   printf "Done!\n" ;
