886d806c784ea28e32eb1ba8232e16dfc1af020c
Timothy Gebhard <mail@timothygebhard.de>
2019-11-14 13:53:33 +0100

    Increase wait time between submissions from 10m to 120m

--- experiments/HR_8799/01_weight-mode_vs_add-planet-column/02_experiments/submit_experiments.sh
+++ experiments/HR_8799/01_weight-mode_vs_add-planet-column/02_experiments/submit_experiments.sh
@@ -4,7 +4,7 @@ while read -r line ; do \
   printf "\nSUBMITTING FILE: " ;
   echo "$line/submit.sub" ; \
   condor_submit_bid 25 "$line/submit.sub" ;
-  printf "\nGoing to sleep for 10 minutes... " ;
-  sleep 10m ;
+  printf "\nGoing to sleep for 120 minutes... " ;
+  sleep 120m ;
   printf "Done!\n" ;
 done < list_of_experiments.txt
