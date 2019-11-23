d815183ef00a816e9ceb6f51952b92e44d8f2f1c
Timothy Gebhard <mail@timothygebhard.de>
2019-11-23 01:07:39 +0100

    Add first experiment to test E/M algorithm (currently without boosting)

---
+++ experiments/Beta_Pictoris/06_test-emb/make_htcondor_files.py
@@ -0,0 +1,241 @@
+"""
+Make submit files to train models in parallel on the cluster.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from pathlib import Path
+
+import argparse
+import os
+import shutil
+import time
+
+from hsr4hci.utils.config import load_config
+
+
+# -----------------------------------------------------------------------------
+# AUXILIARY FUNCTIONS
+# -----------------------------------------------------------------------------
+
+def abs_join(*args):
+    """Join *args and call abspath() on the result."""
+    return os.path.abspath(os.path.join(*args))
+
+
+def get_arguments() -> argparse.Namespace:
+    """
+    Parse and return the command line arguments.
+
+    Returns:
+        An `argparse.Namespace` object containing the command line
+        options that were passed to this script.
+    """
+
+    # Set up a parser
+    parser = argparse.ArgumentParser()
+
+    # Add arguments
+    parser.add_argument('--n-jobs',
+                        type=int,
+                        metavar='N',
+                        default=256,
+                        help='Number of concurrent cluster jobs into which to '
+                             'split the training. Default: 256.')
+    parser.add_argument('--n-rounds',
+                        type=int,
+                        metavar='N',
+                        default=10,
+                        help='Number of rounds for the E/M algorithm. '
+                             'Default: 10.')
+
+    # Parse and return the command line arguments
+    return parser.parse_args()
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # -------------------------------------------------------------------------
+    # Preliminaries
+    # -------------------------------------------------------------------------
+
+    script_start = time.time()
+    print('\nMAKE SUBMIT FILE\n', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Load config and define shortcuts
+    # -------------------------------------------------------------------------
+
+    # Get the command line arguments
+    args = get_arguments()
+    n_jobs = int(args.n_jobs)
+    n_rounds = int(args.n_rounds)
+
+    # Load experiment config from JSON
+    experiment_dir = os.path.dirname(os.path.realpath(__file__))
+    config = load_config(os.path.join(experiment_dir, 'config.json'))
+
+    # Define shortcuts
+    frame_size = config['dataset']['frame_size']
+
+    # -------------------------------------------------------------------------
+    # Create directory for htcondor and clusterlogs
+    # -------------------------------------------------------------------------
+
+    # If the htcondor directory exists already, delete it (to get rid of all
+    # old log files). Then, create a fresh version of it
+    htcondor_dir = os.path.join(experiment_dir, 'htcondor')
+    if os.path.isdir(htcondor_dir):
+        shutil.rmtree(htcondor_dir, ignore_errors=True)
+    Path(htcondor_dir).mkdir(exist_ok=True)
+
+    # Create dir for clusterlogs
+    clusterlogs_dir = os.path.join(htcondor_dir, 'clusterlogs')
+    Path(clusterlogs_dir).mkdir(exist_ok=True)
+
+    # -------------------------------------------------------------------------
+    # Create train.sub file
+    # -------------------------------------------------------------------------
+
+    # Path to script that will run the training
+    train_script_path = \
+        os.path.abspath(os.path.join(experiment_dir, 'train_emb.py'))
+
+    # Add file header with requirements
+    submit_file_lines = \
+        ['#' + 78 * '-',
+         '# GENERAL JOB REQUIREMENTS',
+         '#' + 78 * '-',
+         '',
+         'executable = /is/cluster/tgebhard/.virtualenvs/hsr4hci/bin/python3',
+         'getenv = True',
+         '',
+         'request_memory = 16384',
+         'request_cpus = 1',
+         '\n',
+         '#' + 78 * '-',
+         '# JOBS',
+         '#' + 78 * '-']
+
+    # Loop over all regions and add a job for each of them
+    for region_idx in range(n_jobs):
+
+        # Define paths for output, error and log file
+        out_path = \
+            abs_join(clusterlogs_dir, f"train_$(emb_round)_{region_idx}.out")
+        err_path = \
+            abs_join(clusterlogs_dir, f"train_$(emb_round)_{region_idx}.err")
+        log_path = \
+            abs_join(clusterlogs_dir, f"train_$(emb_round)_{region_idx}.log")
+
+        # Gather arguments: script + command line arguments
+        arguments = [train_script_path,
+                     f'--n-regions {n_jobs}',
+                     f'--region-idx {region_idx}',
+                     f'--emb-round $(emb_round)']
+
+        # Collect lines for the current region
+        job_lines = \
+            [f'\n# REGION {region_idx + 1} / {n_jobs}',
+             f'output = {out_path}',
+             f'error = {err_path}',
+             f'log = {log_path}',
+             f'arguments = {" ".join(arguments)}',
+             'priority = -950',
+             'queue']
+
+        # Add lines to the submit file lines
+        submit_file_lines += job_lines
+
+    # Create and save the train.sub file
+    file_path = os.path.join(htcondor_dir, 'train.sub')
+    with open(file_path, 'w') as submit_file:
+        submit_file.write('\n'.join(submit_file_lines))
+
+    # -------------------------------------------------------------------------
+    # Create merge.sub file
+    # -------------------------------------------------------------------------
+
+    # Path to script that will merge the residuals
+    merge_script_path = \
+        os.path.abspath(os.path.join(experiment_dir, 'merge_residuals.py'))
+
+    # Add file header with requirements
+    submit_file_lines = \
+        ['#' + 78 * '-',
+         '# GENERAL JOB REQUIREMENTS',
+         '#' + 78 * '-',
+         '',
+         'executable = /is/cluster/tgebhard/.virtualenvs/hsr4hci/bin/python3',
+         'getenv = True',
+         '',
+         'request_memory = 8192',
+         'request_cpus = 2',
+         '\n',
+         '#' + 78 * '-',
+         '# JOB',
+         '#' + 78 * '-',
+         '\n']
+
+    # Define paths for output, error and log file
+    out_path = abs_join(clusterlogs_dir, f"merge_$(emb_round).out")
+    err_path = abs_join(clusterlogs_dir, f"merge_$(emb_round).err")
+    log_path = abs_join(clusterlogs_dir, f"merge_$(emb_round).log")
+
+    # Gather arguments: script + command line arguments
+    arguments = [merge_script_path,
+                 f'--emb-round $(emb_round)']
+
+    # Collect lines for the job
+    submit_file_lines += \
+        [f'output = {out_path}',
+         f'error = {err_path}',
+         f'log = {log_path}',
+         f'arguments = {" ".join(arguments)}',
+         'priority = -950',
+         'queue']
+
+    # Create and save the train.sub file
+    file_path = os.path.join(htcondor_dir, 'merge.sub')
+    with open(file_path, 'w') as submit_file:
+        submit_file.write('\n'.join(submit_file_lines))
+
+    # -------------------------------------------------------------------------
+    # Create run.dag file
+    # -------------------------------------------------------------------------
+
+    # Add all jobs to DAG file
+    dag_file_lines = list()
+    for i in range(n_rounds):
+        dag_file_lines.append(f'JOB TRAIN{i} train.sub')
+        dag_file_lines.append(f'JOB MERGE{i} merge.sub')
+
+    # Add variables with the EMB round
+    dag_file_lines.append('')
+    for i in range(n_rounds):
+        dag_file_lines.append(f'VARS TRAIN{i} emb_round="{i}"')
+        dag_file_lines.append(f'VARS MERGE{i} emb_round="{i}"')
+
+    # Add PARENT -> CHILD relations to specify dependencies
+    dag_file_lines.append('')
+    for i in range(n_rounds):
+        dag_file_lines.append(f'PARENT TRAIN{i} CHILD MERGE{i}')
+        if i + 1 < n_rounds:
+            dag_file_lines.append(f'PARENT MERGE{i} CHILD TRAIN{i + 1}')
+
+    # Create and save the run.dag file
+    file_path = os.path.join(htcondor_dir, 'run.dag')
+    with open(file_path, 'w') as dag_file:
+        dag_file.write('\n'.join(dag_file_lines))
+
+    # -------------------------------------------------------------------------
+    # Postliminaries
+    # -------------------------------------------------------------------------
+
+    print(f'\nThis took {time.time() - script_start:.1f} seconds!\n')
---
+++ experiments/Beta_Pictoris/06_test-emb/train_emb.py
@@ -0,0 +1,191 @@
+"""
+Train an HSR model (by default with forward modeling).
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from pathlib import Path
+
+import argparse
+import os
+import time
+
+from tqdm import tqdm
+
+import numpy as np
+
+from hsr4hci.utils.adi_tools import derotate_frames
+from hsr4hci.utils.config import load_config
+from hsr4hci.utils.data import load_data
+from hsr4hci.utils.fits import read_fits
+from hsr4hci.models.hsr import HalfSiblingRegression
+
+
+# -----------------------------------------------------------------------------
+# AUXILIARY FUNCTION DEFINITIONS
+# -----------------------------------------------------------------------------
+
+def get_arguments() -> argparse.Namespace:
+    """
+    Parse and return the command line arguments.
+
+    Returns:
+        An `argparse.Namespace` object containing the command line
+        options that were passed to this script.
+    """
+
+    # Set up a parser
+    parser = argparse.ArgumentParser()
+
+    # Add arguments
+    parser.add_argument('--n-regions',
+                        type=int,
+                        metavar='N',
+                        help='Number of regions in total')
+    parser.add_argument('--region-idx',
+                        type=int,
+                        metavar='N',
+                        help='Index of the region for which to run')
+    parser.add_argument('--emb-round',
+                        type=int,
+                        metavar='N',
+                        help='Iteration of the EMB procedure')
+
+    # Parse and store the command line arguments
+    args = parser.parse_args()
+
+    # Run sanity checks on arguments
+    if not 0 <= args.region_idx < args.n_regions:
+        raise ValueError('region_idx must be between 0 and n_regions-1!')
+
+    return args
+
+
+def relu(x):
+    return np.maximum(x, 0)
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # -------------------------------------------------------------------------
+    # Preliminaries
+    # -------------------------------------------------------------------------
+
+    script_start = time.time()
+    print('\nTRAIN HALF-SIBLING REGRESSION MODEL FOR A REGION\n', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Load config and data
+    # -------------------------------------------------------------------------
+
+    # Get command line options
+    args = get_arguments()
+    n_regions = int(args.n_regions)
+    region_idx = int(args.region_idx)
+    emb_round = int(args.emb_round)
+
+    # Load experiment config from JSON
+    experiment_dir = os.path.dirname(os.path.realpath(__file__))
+    config = load_config(os.path.join(experiment_dir, 'config.json'))
+
+    # Load frames and parallactic angles from HDF file
+    stack, parang, psf_template = load_data(dataset_config=config['dataset'])
+
+    # TODO: Double-check if this is correct!
+    parang -= parang[0]
+
+    # Print some basic training information
+    print(f'Stack size:\t {stack.shape}', flush=True)
+    print(f'Model type:\t {config["experiment"]["model"]["module"]}.'
+          f'{config["experiment"]["model"]["class"]}', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Get the last planet signal estimate and construct stack from it
+    # -------------------------------------------------------------------------
+
+    # In the first round, we do not have a (planet) signal estimate yet; thus,
+    # we initialize it as zeros
+    if emb_round == 0:
+        last_signal_estimate = np.zeros(stack.shape[1:])
+
+    # Otherwise, we load the signal estimate from the last round
+    else:
+        last_round_dir = os.path.join(experiment_dir, 'results',
+                                      f'round_{(emb_round - 1):03}')
+        file_path = os.path.join(last_round_dir, 'signal_estimate.fits')
+        last_signal_estimate = read_fits(file_path=file_path)
+
+    # Pass the signal estimate through a ReLU function
+    last_signal_estimate = relu(last_signal_estimate)
+
+    # Construct a signal stack from it and rotate it into the coordinate system
+    # of the original stack from which we are going to subtract it
+    signal_estimate_stack = [last_signal_estimate for _ in range(len(stack))]
+    last_signal_estimate_stack = np.array(signal_estimate_stack)
+    last_signal_estimate_stack = \
+        derotate_frames(stack=last_signal_estimate_stack,
+                        parang=-parang)
+
+    # Get the input stack on which we will train the model
+    input_stack = stack - last_signal_estimate_stack
+
+    # -------------------------------------------------------------------------
+    # Set up and train model
+    # -------------------------------------------------------------------------
+
+    # Instantiate model
+    hsr = HalfSiblingRegression(config=config)
+
+    # Train the model for all pixels in the region specified in the args
+    print(f'\nTraining models for region {region_idx + 1} of {n_regions}:')
+    hsr.train_region(region_idx=region_idx,
+                     n_regions=n_regions,
+                     stack=input_stack,
+                     parang=parang,
+                     psf_template=psf_template)
+
+    # -------------------------------------------------------------------------
+    # Ensure the results dir exists
+    # -------------------------------------------------------------------------
+
+    results_dir = os.path.join(experiment_dir, 'results')
+    Path(results_dir).mkdir(exist_ok=True)
+
+    round_dir = os.path.join(results_dir, f'round_{emb_round:03}')
+    Path(round_dir).mkdir(exist_ok=True)
+
+    positions_dir = os.path.join(round_dir, 'positions')
+    Path(positions_dir).mkdir(exist_ok=True)
+
+    # -------------------------------------------------------------------------
+    # Get the residual stack and save it
+    # -------------------------------------------------------------------------
+
+    # Get residual stack
+    print(f'\nComputing residual stack:', flush=True)
+    residual_stack = hsr.get_residual_stack(stack=stack).astype(np.float32)
+
+    # Loop over all positions for which we computed residuals and store them
+    # separately as numpy arrays (to minimize the amount of wasted memory)
+    print(f'\nSaving residuals to numpy format:', flush=True)
+    for position, _ in tqdm(hsr.m__collections.items(), ncols=80):
+
+        # Select the residuals for the current position
+        residuals = residual_stack[:, position[0], position[1]]
+
+        # Save the residuals as a numpy array
+        file_path = os.path.join(positions_dir,
+                                 f'residuals__{position[0]}_{position[1]}.npy')
+        np.save(file=file_path, arr=residuals)
+
+    # -------------------------------------------------------------------------
+    # Postliminaries
+    # -------------------------------------------------------------------------
+
+    print(f'\nThis took {time.time() - script_start:.1f} seconds!\n')
