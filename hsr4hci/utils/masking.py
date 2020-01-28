239c452c34a6535cb849289dd5bae9f5eaf3bd56
Timothy Gebhard <mail@timothygebhard.de>
2020-01-28 18:24:31 +0100

    Add get_wedge_mask() and get_sausage_mask() functions

--- hsr4hci/utils/masking.py
+++ hsr4hci/utils/masking.py
@@ -6,6 +6,7 @@ Masking utilities.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
+from cmath import polar
 from typing import List, Tuple
 
 import numpy as np
@@ -82,3 +83,129 @@ def get_annulus_mask(mask_size: tuple,
                                           radius=inner_radius),
                           get_circle_mask(mask_size=mask_size,
                                           radius=outer_radius))
+
+
+def get_wedge_mask(mask_size: Tuple[int, int],
+                   orientation_angle: float,
+                   opening_angle: float) -> np.ndarray:
+    """
+    Create a wedge-shaped (i.e., a circle sector) mask of a given size.
+    The wedge is always centered at the center of the mask.
+
+    Args:
+        mask_size: A tuple (width, height) specifying the size of the
+            mask to be created.
+        orientation_angle: The angle which defines the orientation of
+            the wedge (in degrees). The orientation is given by the
+            angle bisector (see below).
+        opening_angle: The opening angle (in degrees); the wedge will
+            mask all pixels in the sector:
+                [orientation_angle - opening_angle / 2,
+                 orientation_angle + opening_angle / 2]
+
+    Returns:
+        A numpy array containing a wedge-shaped binary mask.
+    """
+
+    # Convert angles from degree to radian (and use shorter names)
+    theta = np.deg2rad(orientation_angle)
+    phi = np.deg2rad(opening_angle)
+
+    # Create a suitable grid
+    x_, y_ = np.ogrid[:mask_size[0], :mask_size[1]]
+    x = x_ - mask_size[0] / 2
+    y = y_ - mask_size[1] / 2
+
+    # Catch two quick corner cases: All pixels or no pixels
+    if opening_angle == 0:
+        return np.zeros(mask_size)
+    if opening_angle == 360:
+        return np.ones(mask_size)
+
+    # The wedge is defined by two straight lines. To see if a point on the
+    # grid belongs to the wedge, we need to know on which side of the lines
+    # it falls, which we can find out by taking the scalar product with the
+    # normal vector of said straight lines.
+    scalarproduct_v = \
+        (x * np.cos(theta + phi / 2.0) - y * np.sin(theta + phi / 2.0))
+    scalarproduct_w = \
+        (x * np.cos(theta - phi / 2.0) - y * np.sin(theta - phi / 2.0))
+
+    # Ultimately, we only need to combine the two sets (i.e., half planes).
+    # The way we do this depends whether the opening angle is smaller or
+    # greater than 180 degrees.
+    if opening_angle < 180:
+        wedge_mask = np.logical_and(scalarproduct_v <= 0, scalarproduct_w >= 0)
+    else:
+        wedge_mask = np.logical_or(scalarproduct_v <= 0, scalarproduct_w >= 0)
+
+    return wedge_mask
+
+
+def get_sausage_mask(mask_size: Tuple[int, int],
+                     position: Tuple[int, int],
+                     radius: float,
+                     opening_angle: float) -> np.ndarray:
+    """
+    Get a "sausage"-shaped mask, which corresponds to the shape you get
+    if you place a circle with `sausage_radius` at the given `position`
+    and then slide it along an arc with the given `opening_angle` around
+    the center of the mask (the `position` bisects this arc).
+
+    This type of mask can be used to get the "exclusion region" of
+    pixels that can not be used a predictors for a given pixel (because
+    they may contain planet signal if the pixel at `position` contains
+    planet signal). In this case, the opening angle should be *twice*
+    the field rotation of the data set.
+
+    Args:
+        mask_size: A tuple (width, height) specifying the size of the
+            mask to be created.
+        position: A tuple (x, y) specifying the position which defines
+            the sausage mask (i.e., the center of the "sausage").
+        radius: The radius (in pixels) which defines the width of the
+            "sausage".
+        opening_angle: The angle (in degrees) which defines the length
+            of the "sausage".
+
+    Returns:
+        A numpy array containing a "sausage"-shaped binary mask.
+    """
+
+    # Compute the center of the mask
+    center = tuple([_ / 2 for _ in mask_size])
+
+    # Convert the given position to polar coordinates
+    r, phi = polar(complex(position[1] - center[1],
+                           position[0] - center[0]))
+
+    # Get star and end position in cartesian coordinates
+    start_position = r * np.exp(1j * (phi - np.deg2rad(opening_angle / 2)))
+    start_position = (np.imag(start_position) + center[0],
+                      np.real(start_position) + center[1])
+    end_position = r * np.exp(1j * (phi + np.deg2rad(opening_angle / 2)))
+    end_position = (np.imag(end_position) + center[0],
+                    np.real(end_position) + center[1])
+
+    # Get the annulus mask
+    annulus_mask = get_annulus_mask(mask_size=mask_size,
+                                    inner_radius=(r - radius),
+                                    outer_radius=(r + radius))
+
+    # Get the wedge mask
+    wedge_mask = get_wedge_mask(mask_size=mask_size,
+                                orientation_angle=np.rad2deg(phi),
+                                opening_angle=opening_angle)
+
+    # Get "end cap" masks
+    end_cap_mask_1 = get_circle_mask(mask_size=mask_size,
+                                     radius=radius,
+                                     center=start_position)
+    end_cap_mask_2 = get_circle_mask(mask_size=mask_size,
+                                     radius=radius,
+                                     center=end_position)
+
+    # Compute the final mask by intersecting the annulus mask with the wedge
+    # mask and then adding the "end cap" masks.
+    return np.logical_or(np.logical_and(annulus_mask, wedge_mask),
+                         np.logical_or(end_cap_mask_1, end_cap_mask_2))
