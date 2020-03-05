f37ae3d88c6a0260229a3aaa4f2f871af2064904
Timothy Gebhard <mail@timothygebhard.de>
2020-03-05 13:00:15 +0100

    Remove everything related to forward modeling
    This commit will remove all experiments and most of the methods
    that were intended to be used with forward modeling in order to
    drastically simplify the code base.

--- experiments/HR_8799/01_weight-mode_vs_add-planet-column/02_experiments/merge_residuals.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while read -r line ; do \
-  printf "Merging residuals for for: " ;
-  echo "$line" ; \
-  python "$line/merge_residuals.py" ;
-  printf "\n\n" ;
-done < list_of_experiments.txt
--- experiments/HR_8799/02_gradient-boosting_weight-mode/01_scripts/merge_residuals.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while read -r line ; do \
-  printf "Merging residuals for for: " ; \
-  echo "$line" ; \
-  python "../02_experiments/$line/merge_residuals.py" ; \
-  printf "\n\n" ; \
-done < ../02_experiments/list_of_experiments.txt
