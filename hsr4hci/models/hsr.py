7107b21fe75cc1a61f5a6c1a037e675b66296eff
Timothy Gebhard <mail@timothygebhard.de>
2020-05-01 19:02:45 +0200

    Major refactoring of HSR class
    Instead of hard-coding the functionality that is used to store model
    properties or debugging information (which has the disadvantage that
    the HSR class code needs to be changed every time we want to try a new
    type of model), this is now achieved by specifying special callback
    functions in the experiment configuration which can be exchanged or
    modified without having to touch the HSR code itself.

--- hsr4hci/models/hsr.py
+++ hsr4hci/models/hsr.py
@@ -7,16 +7,17 @@ Provides the basic HalfSiblingRegression class.
 # -----------------------------------------------------------------------------
 
 from pathlib import Path
-from typing import Dict, NoReturn, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Type, Union
 
 import os
 
 from astropy import units
 from joblib import Parallel, delayed
-from sklearn.metrics import r2_score
 from tqdm import tqdm
 
 import numpy as np
+
+from hsr4hci.models.callbacks import DebuggingCollector
 
 from hsr4hci.utils.derotating import derotate_combine
 from hsr4hci.utils.fits import save_fits
@@ -42,46 +43,74 @@ class HalfSiblingRegression:
 
     def __init__(self,
                  config: dict,
-                 results_dir: str,
-                 train_mask: Optional[np.ndarray] = None,
-                 store_coefficients: bool = False,
-                 store_predictions: bool = False,
-                 store_r_squared: bool = False):
+                 results_dir: str):
 
         # Store the constructor arguments
         self.config = config
         self.results_dir = results_dir
-        self.store_coefficients = store_coefficients
-        self.store_predictions = store_predictions
-        self.store_r_squared = store_r_squared
 
         # Define a few useful shortcuts to the configuration
         self.config_model = config['base_model']
         self.experiment_dir = config['experiment_dir']
-        self.frame_size = config['dataset']['frame_size']
+        self.frame_size = tuple(config['dataset']['frame_size'])
 
         # Compute implicitly defined class variables (i.e., the ROI mask)
-        self.roi_mask = \
+        self.roi_mask: np.ndarray = \
             get_roi_mask(mask_size=self.frame_size,
                          inner_radius=config['roi_mask']['inner_radius'],
                          outer_radius=config['roi_mask']['outer_radius'])
 
-        # Compute the train mask (which is either the ROI itself, or a subset)
-        self.train_mask = np.copy(self.roi_mask)
-        if train_mask is not None:
-            self.train_mask *= train_mask
+        # Initialize debugging callbacks and their results storage
+        self.debugging_callbacks = \
+            self._initialize_debugging_callbacks(config['debugging_callbacks'])
+        self.debugging_results: Dict[str, np.ndarray] = dict()
 
-        # Initialize additional *required* class variables
-        self.field_rotation = None
-        self.residuals = None
+        # Initialize additional class variables
+        self.field_rotation: Optional[float] = None
+        self.predictions: Optional[np.ndarray] = None
+        self.residuals: Optional[np.ndarray] = None
+        self.train_test_splitter: Optional[TrainTestSplitter] = None
 
-        # Initialize additional *optional* class variables
-        if self.store_coefficients:
-            self.coefficients = None
-        if self.store_predictions:
-            self.predictions = None
-        if self.store_r_squared:
-            self.r_squared = None
+    @property
+    def splitter(self) -> TrainTestSplitter:
+
+        # Create a function to split the stack into training and test
+        if self.train_test_splitter is None:
+            self.train_test_splitter = \
+                TrainTestSplitter(**self.config['train_test_splitter'])
+        return self.train_test_splitter
+
+    @staticmethod
+    def _initialize_debugging_callbacks(list_of_callback_names: List[str]) \
+            -> List[Type[DebuggingCollector]]:
+        """
+        Turn a list of callback class names (e.g., 'AlphaCollector')
+        into a list of actual classes that can be instantiated.
+
+        Args:
+            list_of_callback_names: A list of strings specifying the
+                names of the callbacks from the debugging module that
+                we want to use during the training; for example:
+                "AlphaCollector" for a callback that collects the
+                .alpha_ parameter of the trained models.
+
+        Returns:
+            A list of classes that can be used as callbacks during the
+            training of the models (must be from the debugging module).
+        """
+
+        # Initialize result list
+        debugging_callbacks = list()
+
+        # Loop over the list of callback names and import the corresponding
+        # classes from the debugging module
+        for callback_name in list_of_callback_names:
+            debugging_callback: Type[DebuggingCollector] = \
+                get_member_by_name(module_name='hsr4hci.models.debugging',
+                                   member_name=callback_name)
+            debugging_callbacks.append(debugging_callback)
+
+        return debugging_callbacks
 
     def _ensure_results_dir(self):
         """
@@ -118,9 +147,19 @@ class HalfSiblingRegression:
 
         return model
 
+    def _get_field_rotation(self,
+                            parang: np.ndarray) -> units.Quantity:
+
+        # If we have not computed the field rotation of the data set we are
+        # using for training, do it now and store the result
+        if self.field_rotation is None:
+            self.field_rotation = np.abs(parang[-1] - parang[0])
+            self.field_rotation = units.Quantity(self.field_rotation, 'degree')
+        return self.field_rotation
+
     def train(self,
               stack: np.ndarray,
-              parang: np.ndarray) -> NoReturn:
+              parang: np.ndarray) -> None:
         """
         Train models for all positions (pixels) in the ROI.
 
@@ -139,15 +178,12 @@ class HalfSiblingRegression:
         n_processes = self.config['multiprocessing']['n_processes']
         n_splits = self.config['train_test_splitter']['n_splits']
 
-        # Initialize result variables
-        self.predictions = np.full(stack.shape, np.nan)
-        self.r_squared = np.full((n_splits, *self.frame_size), np.nan)
-        self.residuals = np.full(stack.shape, np.nan)
-        self.coefficients = \
-            np.full((n_splits, *self.frame_size, *self.frame_size), np.nan)
+        # ---------------------------------------------------------------------
+        # Train models for all positions
+        # ---------------------------------------------------------------------
 
         # Get positions in ROI as a list
-        roi_positions = get_positions_from_mask(self.train_mask)
+        roi_positions = get_positions_from_mask(self.roi_mask)
 
         # Define a partial application for self.train_position() that
         # fixes the stack and the parallactic angles
@@ -174,24 +210,36 @@ class HalfSiblingRegression:
             for position in tqdm(roi_positions, ncols=80):
                 results.append(train_position(position=position))
 
+        # ---------------------------------------------------------------------
+        # Collect and combine results from individual positions
+        # ---------------------------------------------------------------------
+
+        # Initialize result variables
+        self.predictions = np.full(stack.shape, np.nan)
+        self.debugging_results = \
+            {_.result_name: np.full(_.shape(n_splits=n_splits,
+                                            frame_size=self.frame_size),
+                                    np.nan) for _ in self.debugging_callbacks}
+
         # Once all positions in the ROI have been processed (either in
         # parallel or sequentially), combine all these individual pixel
         # results into a single "global" result
         for result in results:
 
-            # Get the position of the current sub-result
-            pos = result['position']
+            # Unpack the current result dictionary
+            position = result['position']
+            predictions = result['predictions']
 
-            # Store residuals
-            self.residuals[:, pos[0], pos[1]] = result['residuals']
+            # Define the fancy index to select position in result arrays
+            position_idx = (slice(None), ) + position
 
-            # Save additional optional results
-            if hasattr(self, 'predictions'):
-                self.predictions[:, pos[0], pos[1]] = result['predictions']
-            if hasattr(self, 'r_squared'):
-                self.r_squared[:, pos[0], pos[1]] = result['r_squared']
-            if hasattr(self, 'coefficients'):
-                self.coefficients[:, pos[0], pos[1]] = result['coefficients']
+            # Store predictions and results of debugging callbacks
+            self.predictions[position_idx] = predictions
+            for key in self.debugging_results.keys():
+                self.debugging_results[key][position_idx] = result[key]
+
+        # Finally, compute the residuals from predictions
+        self.residuals = stack - self.predictions
 
     def train_position(self,
                        position: Tuple[int, int],
@@ -210,70 +258,59 @@ class HalfSiblingRegression:
                 respective parallactic angles for each image.
         """
 
-        # Define shortcuts for number of frames and splits
+        # Define shortcuts for number of frames
         n_frames = stack.shape[0]
-        n_splits = self.config['train_test_splitter']['n_splits']
-
-        # If we have not computed the field rotation of the data set we are
-        # using for training, do it now and store the result
-        if self.field_rotation is None:
-            self.field_rotation = np.abs(parang[-1] - parang[0])
-            self.field_rotation = units.Quantity(self.field_rotation, 'degree')
+        field_rotation = self._get_field_rotation(parang=parang)
 
         # Get the selection_mask for this position, that is, the mask that
         # selects the (spatial) pixels to be used as predictors
         selection_mask = get_selection_mask(mask_size=self.frame_size,
                                             position=position,
-                                            field_rotation=self.field_rotation,
+                                            field_rotation=field_rotation,
                                             **self.config['selection_mask'])
 
-        # Create a function to split the stack into training and test
-        splitter = TrainTestSplitter(**self.config['train_test_splitter'])
-
-        # Initialize result variables
+        # Initialize result variables and set up debugging callbacks
         predictions = np.full(n_frames, np.nan)
-        residuals = np.full(n_frames, np.nan)
-        r_squared = np.full(n_splits, np.nan)
-        coefficients = np.full((n_splits, *self.frame_size), np.nan)
+        debugging_callbacks = [callback(n_splits=self.splitter.n_splits,
+                                        selection_mask=selection_mask)
+                               for callback in self.debugging_callbacks]
 
-        # Loop over all train/test splits, train models and make predictions
-        for split_idx, (train_idx, test_idx) in \
-                enumerate(splitter.split(n_frames)):
+        # Loop over all train/apply splits, train models and make predictions
+        for split_idx, (train_idx, apply_idx) in \
+                enumerate(self.splitter.split(n_frames)):
 
-            # Select training predictors and targets
+            # Select predictors and targets for both training and application
             train_predictors = stack[train_idx][:, selection_mask]
-            train_targets = stack[train_idx, position[0], position[1]]
+            apply_predictors = stack[apply_idx][:, selection_mask]
+            train_targets = stack[train_idx, position[0], position[1]].reshape(-1, 1)
+            apply_targets = stack[apply_idx, position[0], position[1]].reshape(-1, 1)
 
-            # Instantiate a new model to be trained on these data
+            # Instantiate a new model and fit it to the training data
             model = self._get_base_model_instance()
+            model.fit(X=train_predictors, y=train_targets.ravel())
 
-            # Fit the model to the training data
-            model.fit(X=train_predictors, y=train_targets)
-
-            # Store the coefficients of the model
-            if hasattr(model, 'coef_'):
-                coefficients[split_idx][selection_mask] = model.coef_
-
-            # Apply the learned model to the test data to get predictions
-            test_predictors = stack[test_idx][:, selection_mask]
-            tmp_predictions = model.predict(X=test_predictors)
+            # Apply the learned model to the data that was held out
+            tmp_predictions = model.predict(X=apply_predictors)
 
             # Store the predictions
-            predictions[test_idx] = tmp_predictions
+            predictions[apply_idx] = tmp_predictions
 
-            # Compute the R^2 value (i.e., fit quality) and store it
-            y_true = stack[test_idx, position[0], position[1]]
-            r_squared[split_idx] = r2_score(y_true, tmp_predictions)
+            # Run debugging callbacks to collect their data
+            for debugging_callback in debugging_callbacks:
+                debugging_callback.collect(model=model,
+                                           split_idx=split_idx,
+                                           y_true=apply_targets,
+                                           y_pred=tmp_predictions)
 
-            # Compute and store the residuals
-            residuals[test_idx] = \
-                stack[test_idx, position[0], position[1]] - tmp_predictions
+        # Initialize results with the indispensables (position and predictions)
+        results = dict(position=position,
+                       predictions=predictions)
 
-        return dict(position=position,
-                    predictions=predictions,
-                    r_squared=r_squared,
-                    residuals=residuals,
-                    coefficients=coefficients)
+        # Loop over debugging callbacks and add their data to the results
+        for debugging_callback in debugging_callbacks:
+            results[debugging_callback.result_name] = debugging_callback.result
+
+        return results
 
     def get_signal_estimate(self,
                             parang: np.ndarray,
@@ -308,35 +345,18 @@ class HalfSiblingRegression:
 
         return signal_estimate
 
-    def save_predictions(self):
+    def save_predictions(self) -> None:
         """
         Save predictions as a FITS file to the results directory.
         """
 
-        if self.store_predictions:
-            print(f'Saving predictions to FITS...', end=' ', flush=True)
-            self._ensure_results_dir()
-            file_path = os.path.join(self.results_dir, 'predictions.fits')
-            save_fits(array=self.predictions, file_path=file_path)
-            print('Done!', flush=True)
-        else:
-            print('No predictions to save!')
+        print(f'Saving predictions to FITS...', end=' ', flush=True)
+        self._ensure_results_dir()
+        file_path = os.path.join(self.results_dir, 'predictions.fits')
+        save_fits(array=self.predictions, file_path=file_path)
+        print('Done!', flush=True)
 
-    def save_r_squared(self):
-        """
-        Save R^2 values as a FITS file to the results directory.
-        """
-
-        if self.store_r_squared:
-            print(f'Saving R^2 values to FITS...', end=' ', flush=True)
-            self._ensure_results_dir()
-            file_path = os.path.join(self.results_dir, 'r_squared.fits')
-            save_fits(array=self.r_squared, file_path=file_path)
-            print('Done!', flush=True)
-        else:
-            print('No r_squared values to save!')
-
-    def save_residuals(self):
+    def save_residuals(self) -> None:
         """
         Save residuals as a FITS file to the results directory.
         """
@@ -347,16 +367,16 @@ class HalfSiblingRegression:
         save_fits(array=self.residuals, file_path=file_path)
         print('Done!', flush=True)
 
-    def save_coefficients(self):
+    def save_debugging_results(self) -> None:
         """
-        Save coefficients as a FITS file to the results directory.
+        Save results of debugging callbacks to FITS files.
         """
 
-        if self.store_coefficients:
-            print(f'Saving coefficients to FITS...', end=' ', flush=True)
+        # Loop over all debugging results and save them as FITS files
+        for key, value in self.debugging_results.items():
+
+            print(f'Saving {key} to FITS...', end=' ', flush=True)
             self._ensure_results_dir()
-            file_path = os.path.join(self.results_dir, 'coefficients.fits')
-            save_fits(array=self.coefficients, file_path=file_path)
+            file_path = os.path.join(self.results_dir, f'{key}.fits')
+            save_fits(array=value, file_path=file_path)
             print('Done!', flush=True)
-        else:
-            print('No coefficients to save!')
