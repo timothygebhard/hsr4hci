0d4208d877b9ea148957b516116907126c0e47a8
Timothy Gebhard <mail@timothygebhard.de>
2020-05-02 21:30:02 +0200

    Fix type hints

--- hsr4hci/utils/tqdm.py
+++ hsr4hci/utils/tqdm.py
@@ -6,9 +6,12 @@ Utility functions to augment the tqdm library.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
+from typing import Any, Iterator
+
 import contextlib
 
 import joblib
+import tqdm
 
 
 # -----------------------------------------------------------------------------
@@ -16,7 +19,7 @@ import joblib
 # -----------------------------------------------------------------------------
 
 @contextlib.contextmanager
-def tqdm_joblib(tqdm_object):
+def tqdm_joblib(tqdm_object: tqdm.std.tqdm) -> Iterator[None]:
     """
     Context manager to patch joblib to report into a tqdm progress bar
     given as an argument to this function.
@@ -24,8 +27,8 @@ def tqdm_joblib(tqdm_object):
     Source: https://stackoverflow.com/a/58936697/4100721
 
     Args:
-        tqdm_object: An object of type `tqdm._tqdm.tqdm`; for example,
-            an iterator that has been wrapped with tqdm().
+        tqdm_object: An tqdm object ; for example, an iterator that
+            has been wrapped with tqdm().
     """
 
     class TqdmBatchCompletionCallback:
@@ -33,11 +36,16 @@ def tqdm_joblib(tqdm_object):
         New tqdm callback that is invoked upon batch completion.
         """
 
-        def __init__(self, _, index, parallel):
+        def __init__(self,
+                     _: Any,
+                     index: int,
+                     parallel: joblib.parallel.Parallel):
+
             self.index = index
             self.parallel = parallel
 
-        def __call__(self, index):
+        def __call__(self,
+                     index: int) -> None:
 
             tqdm_object.update()
 
@@ -46,9 +54,13 @@ def tqdm_joblib(tqdm_object):
             if self.parallel._original_iterator is not None:
                 self.parallel.dispatch_next()
 
+    # Replace the default joblib.parallel callback for batch completion with
+    # the one we have just defined
     old_batch_callback = joblib.parallel.BatchCompletionCallBack
     joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
 
+    # Yield the updated tqdm object, and ensure that the original callback
+    # for joblib.parallel is restored at the end of the context manager
     try:
         yield tqdm_object
     finally:
