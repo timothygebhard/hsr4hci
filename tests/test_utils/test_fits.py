9e71ef0618166c2d583f8e06a7b9ac5af489bc4c
Timothy Gebhard <mail@timothygebhard.de>
2020-05-18 19:07:34 +0200

    Update tests to increase coverage

--- tests/test_utils/test_fits.py
+++ tests/test_utils/test_fits.py
@@ -6,8 +6,11 @@ Tests for fits.py
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-import numpy as np
 from _pytest.tmpdir import TempPathFactory
+
+import astropy.io.fits.card as card
+import numpy as np
+import pytest
 
 from hsr4hci.utils.fits import read_fits, save_fits
 
@@ -27,12 +30,22 @@ def test__save_fits(tmp_path_factory: TempPathFactory) -> None:
     header = dict(str_key='Test Name',
                   int_key=5,
                   float_key=8.2,
+                  list_key=['a', 'b', 'c'],
                   nan_key=np.nan,
                   array_key=np.array([1, 2, 3]),
                   nan_array_key=np.array([1, 2, np.nan]))
 
     # Save to FITS
-    save_fits(array=array, file_path=str(file_path), header=header)
+    with pytest.warns(card.VerifyWarning) as record:
+        save_fits(array=array, file_path=str(file_path), header=header)
+
+    # Catch astropy warnings about length of FITS keys
+    assert len(record) == 3
+    for i, key in enumerate(['FLOAT_KEY', 'ARRAY_KEY', 'NAN_ARRAY_KEY']):
+        assert record[i].message.args[0] == \
+               (f"Keyword name '{key}' is greater than 8 characters or "
+                f"contains characters not allowed by the FITS standard; a "
+                f"HIERARCH card will be created.")
 
 
 def test__read_fits(tmp_path_factory: TempPathFactory) -> None:
@@ -42,7 +55,10 @@ def test__read_fits(tmp_path_factory: TempPathFactory) -> None:
     file_path = test_dir / 'test.fits'
 
     # Read the test FITS file
+    array_no_header = read_fits(file_path=str(file_path), return_header=False)
     array, header = read_fits(file_path=str(file_path), return_header=True)
+
+    assert np.array_equal(array_no_header, array)
 
     # Define the expected file contents, including the various expected
     # changes (e.g., serialization of values, capitalization of keys) that
@@ -51,6 +67,7 @@ def test__read_fits(tmp_path_factory: TempPathFactory) -> None:
     expected_header = dict(STR_KEY='Test Name',
                            INT_KEY=5,
                            FLOAT_KEY=8.2,
+                           LIST_KEY='["a", "b", "c"]',
                            NAN_KEY='NaN',
                            ARRAY_KEY='[1, 2, 3]',
                            NAN_ARRAY_KEY='[1.0, 2.0, NaN]')
