9c08e12fd2dfabaa9c575d66f7eba68cb6f32c74
Timothy Gebhard <mail@timothygebhard.de>
2020-06-15 17:12:01 +0200

    Add get_fits_header_value() and get_fits_header_value_array()

--- hsr4hci/utils/fits.py
+++ hsr4hci/utils/fits.py
@@ -6,7 +6,7 @@ Utility functions for reading and writing FITS files.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from typing import Optional, Tuple, Union
+from typing import Any, Optional, Tuple, Type, Union
 
 import json
 
@@ -84,3 +84,71 @@ def save_fits(
 
     # Save the HDU to the specified FITS file
     fits.HDUList([hdu]).writeto(file_path, overwrite=overwrite)
+
+
+def get_fits_header_value(
+    file_path: str,
+    key: str,
+    dtype: Optional[Type] = None,
+) -> Any:
+    """
+    Get the value of a field in the header of a given FITS file.
+
+    Args:
+        file_path: Path to the FITS file to read the header from.
+        key: Key of the field in the header of the FITS file.
+        dtype: Target data type of the header value. If this value is
+            not None, we try to convert the value that was read from
+            the FITS file to the given data type.
+
+    Returns:
+        The value of the header field in the given FITS file.
+    """
+
+    # Open the FITS file and read the target header value
+    with fits.open(file_path) as hdu_list:
+        value = hdu_list[0].header[key]
+
+    # If desired, attempt to convert the value to the given data type
+    if dtype is not None and isinstance(dtype, type):
+        return dtype(value)
+    return value
+
+
+def get_fits_header_value_array(
+    file_path: str,
+    start_key: str,
+    end_key: str,
+) -> np.ndarray:
+    """
+    For some fields, the ESO/VLT FITS files define a start and an end
+    value (e.g., the seeing). This function will return a numpy array
+    that has as many entries as there are frames in the FITS file, and
+    that contains a linear interpolation between the start and the end
+    value of the given keys.
+
+    If the start and end key are identical, the resulting array will
+    contain the same value everywhere.
+
+    Args:
+        file_path: Path to the FITS file to read the header from.
+        start_key: The start key of the value we want to retrieve.
+        end_key: The end key of the value we want to retrieve.
+
+    Returns:
+        A numpy array (that has as many entries as there are frames
+        along NAXIS3 in the FITS file) containing a linear interpolation
+        of the values corresponding to the given start and end keys.
+    """
+
+    # Get the number of frames in the current cube
+    n_frames: int = get_fits_header_value(file_path, 'NAXIS3', int)
+
+    # Read the start and end value from the FITS file
+    start_value: str = get_fits_header_value(file_path, start_key, str)
+    end_value: str = get_fits_header_value(file_path, end_key, str)
+
+    # Create an array with a linear interpolation between those two values
+    array = np.linspace(start_value, end_value, n_frames)
+
+    return array
