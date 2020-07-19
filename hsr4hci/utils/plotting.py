9e2db7192c6e4599ed6679fe15990dfb5cd950fc
Timothy Gebhard <mail@timothygebhard.de>
2020-07-19 22:13:28 +0200

    Add plot_frame() function

--- hsr4hci/utils/plotting.py
+++ hsr4hci/utils/plotting.py
@@ -6,7 +6,7 @@ Utility functions for plotting.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from typing import Any, Tuple, Union
+from typing import Any, List, Optional, Tuple, Union
 
 import colorsys
 
@@ -16,8 +16,11 @@ from matplotlib.colors import Colormap, LinearSegmentedColormap, ListedColormap
 from matplotlib.figure import Figure
 from matplotlib.image import AxesImage
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from photutils import CircularAperture
 
 import matplotlib.colors as mc
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # -----------------------------------------------------------------------------
@@ -168,3 +171,140 @@ def disable_ticks(
 
     ax.tick_params(axis='both', which='both', top=False, bottom=False,
                    left=False, right=False, labelbottom=False, labelleft=False)
+
+
+def plot_frame(
+    frame: np.ndarray,
+    file_path: Optional[str] = None,
+    figsize: Tuple[float, float] = (4.0, 4.0),
+    aperture_radius: Optional[float] = None,
+    positions: Optional[List[Tuple[float, float]]] = None,
+    snrs: Optional[List[float]] = None,
+    draw_color: Optional[MatplotlibColor] = 'darkgreen',
+) -> Figure:
+    """
+    Plot a single frame (e.g., a signal estimate). If desired, also add
+    apertures and labels for the SNR at given positions.
+
+    Args:
+        frame: A 2D numpy array of shape `(width, height)` containing
+            the frame to be plotted.
+        file_path: A string containing the path at which to save the
+            resulting plot. If None is given, the plot is not saved.
+        figsize: A two-tuple `(x_size, y_size)` containing the size of
+            the figure in inches.
+        aperture_radius: If apertures are to be draw (see below), this
+            is the radius (in units of pixels) that is used for them.
+        positions: Optionally, a list of positions. At each position,
+            an aperture is drawn with the given aperture radius.
+        snrs: Optionally, each aperture can also be decorated with a
+            label containing the SNR. This list contains the values for
+            these SNR labels.
+        draw_color: The color to be used for drawing the aperture and
+            also the label.
+
+    Returns:
+
+    """
+
+    # -------------------------------------------------------------------------
+    # General set up, draw frame to canvas
+    # -------------------------------------------------------------------------
+
+    # Set up a new figure
+    fig, ax = plt.subplots(figsize=figsize)
+    fig.set_constrained_layout_pads(w_pad=0, h_pad=0)
+
+    # If apertures are to be drawn, we can define them here and use the
+    # photometry values from them to define the value limits of the plot
+    if (aperture_radius is not None) and (positions is not None):
+
+        # Define aperture to get plot limits, and also to for plotting later
+        aperture = CircularAperture(positions=positions, r=aperture_radius)
+        photometry, _ = aperture.do_photometry(data=frame)
+
+        # Determine the limits for the color map: Use 120% of the average pixel
+        # value in the aperture with the highest total flux
+        limit = 1.2 * np.nanmax(np.abs(photometry)) / aperture.area
+
+    # Otherwise, just compute the limit based on the entire frame
+    else:
+        aperture = None
+        limit = 1.2 * np.nanmax(np.abs(frame))
+
+    # Create the actual plot and use the limit we just computed
+    plt.imshow(
+        X=frame,
+        origin='lower',
+        cmap=get_cmap(),
+        vmin=-limit,
+        vmax=limit,
+        interpolation='none',
+    )
+
+    # -------------------------------------------------------------------------
+    # Plot apertures and labels
+    # -------------------------------------------------------------------------
+
+    # Plot the desired apertures
+    if aperture is not None:
+        aperture.plot(axes=ax, **dict(color=draw_color, lw=1, ls='-'))
+
+    # Add labels for their respective SNR
+    if (snrs is not None) and (positions is not None):
+
+        # If SNRs and positions do not have the same length, the zip will fail,
+        # so we do not have to add another check for this
+        for snr, position in zip(snrs, positions):
+
+            # Compute position of the label containing the SNR
+            angle = np.arctan2(
+                position[1] - frame.shape[1] / 2,
+                position[0] - frame.shape[0] / 2
+            )
+            x = position[0] + max((8, 0.1 * position[0])) * np.cos(angle)
+            y = position[1] + max((8, 0.1 * position[1])) * np.sin(angle)
+
+            # Actually add the label with the SNR at this position
+            ax.text(
+                x=x,
+                y=y,
+                s=f'{snr:.1f}',
+                ha='center',
+                va='center',
+                color='white',
+                fontsize=6,
+                bbox=dict(
+                    facecolor=draw_color,
+                    edgecolor='none',
+                    boxstyle='round,pad=0.15',
+                ),
+            )
+
+            # Draw connection between label and aperture
+            ax.plot(
+                [position[0] + aperture_radius * np.cos(angle), x],
+                [position[1] + aperture_radius * np.sin(angle), y],
+                color=draw_color,
+                lw=1,
+            )
+
+    # -------------------------------------------------------------------------
+    # Set plot options and save result
+    # -------------------------------------------------------------------------
+
+    # Remove ax ticks
+    ax.tick_params(
+        axis='both',
+        which='both',
+        bottom=False,
+        left=False,
+        labelleft=False,
+        labelbottom=False,
+    )
+
+    # Save the results
+    if file_path is not None:
+        plt.savefig(file_path, bbox_inches='tight', pad=0)
+
+    return fig
