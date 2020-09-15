6379b15f47366e32f7cad3e3dc3c414188300886
Timothy Gebhard <mail@timothygebhard.de>
2020-09-16 01:31:08 +0200

    Add three new major functions; some minor small fixes

--- hsr4hci/utils/development.py
+++ hsr4hci/utils/development.py
@@ -9,14 +9,18 @@ be useful in the end.
 # -----------------------------------------------------------------------------
 
 from cmath import polar
-from typing import List, Optional, Tuple
+from typing import Callable, Dict, List, Optional, Tuple, Union
 
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from tqdm.auto import tqdm
 
 import numpy as np
 
 from hsr4hci.utils.fitting import moffat_2d, fit_2d_function
 from hsr4hci.utils.general import crop_center, rotate_position
+from hsr4hci.utils.masking import get_positions_from_mask
+from hsr4hci.utils.splitting import TrainTestSplitter
 
 
 # -----------------------------------------------------------------------------
@@ -160,7 +164,7 @@ def get_noise_signal_masks(
     n_signal_times: int,
     frame_size: Tuple[int, int],
     psf_diameter: float,
-    signal_length_threshold: float = 0.7,
+    max_signal_length: float = 0.7,
 ) -> List[Tuple[np.ndarray, np.ndarray, int]]:
     """
     Generate the masks for training a series of models where different
@@ -182,11 +186,11 @@ def get_noise_signal_masks(
         frame_size: A tuple `(width, height)` specifying the spatial
             size of the stack.
         psf_diameter: The diameter of the PSF template (in pixels).
-        signal_length_threshold: A value in [0.0, 1.0] which describes
-            the maximum value of `expected_signal_length / n_frames`,
-            which will determine for which pixels we do not want to use
-            the "mask out a potential signal region"-approach, because
-            the potential signal region is too large to leave us with a
+        max_signal_length: A value in [0.0, 1.0] which describes the
+            maximum value of `expected_signal_length / n_frames`, which
+            will determine for which pixels we do not want to use the
+            "mask out a potential signal region"-approach, because the
+            potential signal region is too large to leave us with a
             reasonable amount of training data.
 
     Returns:
@@ -227,7 +231,7 @@ def get_noise_signal_masks(
         # Check if the expected signal length is larger than the threshold.
         # In this case, we do not compute the noise and signal masks, but
         # skip this signal time.
-        if (length_1 + length_2) / n_frames > signal_length_threshold:
+        if (length_1 + length_2) / n_frames > max_signal_length:
             continue
 
         # Construct the signal mask
@@ -357,7 +361,7 @@ def has_bump(
 
 def get_consistency_check_data(
     position: Tuple[int, int],
-    signal_time: np.ndarray,
+    signal_time: int,
     parang: np.ndarray,
     frame_size: Tuple[int, int],
     psf_diameter: float,
@@ -391,7 +395,7 @@ def get_consistency_check_data(
     starting_position = rotate_position(
         position=position,
         center=center,
-        angle=-float(parang[signal_time] - parang[0])
+        angle=-float(parang[signal_time] - parang[0]),
     )
 
     # Create `n_test_times` (uniformly distributed) points in time at which
@@ -408,7 +412,7 @@ def get_consistency_check_data(
         test_position = rotate_position(
             position=starting_position,
             center=center,
-            angle=float(parang[test_time] - parang[0])
+            angle=float(parang[test_time] - parang[0]),
         )
 
         # Round to the closest pixel position
@@ -438,3 +442,361 @@ def get_consistency_check_data(
         results.append(result)
 
     return results
+
+
+def get_baseline_results(
+    position: Tuple[int, int],
+    stack: np.ndarray,
+    obscon_array: np.ndarray,
+    selection_mask: np.ndarray,
+    get_model_instance: Callable,
+    n_splits: int,
+) -> Dict[str, np.ndarray]:
+    """
+    Get the baseline results for a given pixel, that is, the results
+    without masking out any potential signal region.
+
+    Args:
+        position:
+        stack:
+        obscon_array:
+        selection_mask:
+        get_model_instance:
+        n_splits:
+
+    Returns:
+
+    """
+
+    # Select the full targets and predictors for the current position
+    # using the given selection mask
+    full_predictors = stack[:, selection_mask]
+    full_targets = stack[:, position[0], position[1]].reshape(-1, 1)
+
+    # Add observing conditions to the predictors
+    full_predictors = np.hstack((full_predictors, obscon_array))
+
+    # Create splitter for indices
+    splitter = TrainTestSplitter(n_splits=n_splits, split_type='even_odd')
+
+    # Prepare array for predictions
+    full_predictions = np.full(len(full_targets), np.nan)
+
+    # Loop over splits
+    for train_idx, apply_idx in splitter.split(len(full_targets)):
+
+        # Apply a scaler to the predictors and targets
+        predictors_scaler = StandardScaler()
+        train_predictors = predictors_scaler.fit_transform(
+            full_predictors[train_idx]
+        )
+        apply_predictors = predictors_scaler.transform(
+            full_predictors[apply_idx]
+        )
+        targets_scaler = StandardScaler()
+        train_targets = targets_scaler.fit_transform(full_targets[train_idx])
+
+        # Instantiate a new model
+        model = get_model_instance()
+
+        # Fit the model to the data
+        model.fit(train_predictors, train_targets)
+
+        # Get the model predictions
+        predictions = model.predict(apply_predictors)
+
+        # Undo the normalization
+        predictions = targets_scaler.inverse_transform(predictions).ravel()
+
+        # Store the result
+        full_predictions[apply_idx] = predictions
+
+    # Compute full residuals
+    full_residuals = full_targets.ravel() - full_predictions.ravel()
+
+    return dict(predictions=full_predictions, residuals=full_residuals)
+
+
+def get_signal_masking_results(
+    position: Tuple[int, int],
+    stack: np.ndarray,
+    parang: np.ndarray,
+    obscon_array: np.ndarray,
+    selection_mask: np.ndarray,
+    get_model_instance: Callable,
+    n_signal_times: int,
+    frame_size: Tuple[int, int],
+    psf_diameter: float,
+    n_splits: int,
+    max_signal_length: float,
+) -> Dict[str, Dict[str, np.ndarray]]:
+    """
+    Get the results based on signal masking for given pixel: For
+    `n_signal_times` points in time, compute the expected length (in
+    time) of a planet signal here, mask out the corresponding temporal
+    region and train a model on the rest of the time series.
+
+    Args:
+        position:
+        stack:
+        parang:
+        obscon_array:
+        selection_mask:
+        get_model_instance:
+        n_signal_times:
+        frame_size:
+        psf_diameter:
+        n_splits:
+        max_signal_length:
+
+    Returns:
+
+    """
+
+    # Select the full targets and predictors for the current position
+    # using the given selection mask
+    full_predictors = stack[:, selection_mask]
+    full_targets = stack[:, position[0], position[1]].reshape(-1, 1)
+
+    # Add observing conditions to the predictors
+    full_predictors = np.hstack((full_predictors, obscon_array))
+
+    # Initialize results dictionary
+    results: Dict[str, Dict[str, Union[np.ndarray, int]]] = dict()
+
+    # Initialize metrics for finding "best" split
+    best_mean = -np.infty
+    best_predictions = np.full(len(full_targets), np.nan)
+    best_residuals = np.full(len(full_targets), np.nan)
+    best_signal_mask = np.full(len(full_targets), np.nan)
+    best_signal_time = np.nan
+
+    # Create splitter for indices
+    splitter = TrainTestSplitter(n_splits=n_splits, split_type='even_odd')
+
+    # Prepare array for predictions
+    full_predictions = np.full(len(full_targets), np.nan)
+
+    # Loop over different possible planet times and exclude them from the
+    # training data to find the "best" exclusion region, and thus the best
+    # model, which (ideally) was trained only on the part of the time series
+    # that does not contain any planet signal.
+    for i, (noise_mask, signal_mask, signal_time) in enumerate(
+        get_noise_signal_masks(
+            position=position,
+            parang=parang,
+            n_signal_times=n_signal_times,
+            frame_size=frame_size,
+            psf_diameter=psf_diameter,
+            max_signal_length=max_signal_length,
+        )
+    ):
+
+        # Add sub-dictionary in results
+        results[str(i)] = dict(
+            signal_mask=signal_mask, signal_time=signal_time
+        )
+
+        # Loop over cross-validation splits to avoid overfitting
+        for train_idx, apply_idx in splitter.split(len(full_targets)):
+
+            # Construct binary versions of the train_idx and apply_idx and
+            # mask out the signal region from both of them
+            binary_train_idx = np.full(len(noise_mask), False)
+            binary_train_idx[train_idx] = True
+            binary_train_idx[signal_mask] = False
+            binary_apply_idx = np.full(len(noise_mask), False)
+            binary_apply_idx[apply_idx] = True
+            binary_apply_idx[signal_mask] = False
+
+            # Select predictors and targets for training: Choose the training
+            # positions without the presumed signal region
+            train_predictors = full_predictors[binary_train_idx]
+            train_targets = full_targets[binary_train_idx]
+
+            # Apply a scaler to the predictors
+            predictors_scaler = StandardScaler()
+            train_predictors = predictors_scaler.fit_transform(
+                train_predictors
+            )
+            full_predictors_transformed = predictors_scaler.transform(
+                full_predictors
+            )
+
+            # Apply a predictor to the targets
+            targets_scaler = StandardScaler()
+            train_targets = targets_scaler.fit_transform(train_targets)
+
+            # Instantiate a new model for learning the noise
+            model = get_model_instance()
+
+            # Fit the model to the training data
+            model.fit(X=train_predictors, y=train_targets.ravel())
+
+            # Get the predictions for every point in time using the current
+            # model, and undo the target normalization
+            predictions = model.predict(X=full_predictors_transformed)
+            predictions = targets_scaler.inverse_transform(
+                predictions.reshape(-1, 1)
+            )
+
+            # Select the predictions on the "apply region" (including the
+            # signal region) and store them at the right positions
+            full_predictions[apply_idx] = predictions[apply_idx].ravel()
+
+            # Check for overfitting: If the standard deviation of the residuals
+            # in the train region is much smaller than the standard deviation
+            # of the residuals in the apply region (without the signal region),
+            # then this could be an indication that our model is memorizing the
+            # training data.
+            train_residuals = (full_targets - predictions)[binary_train_idx]
+            apply_residuals = (full_targets - predictions)[binary_apply_idx]
+            if 3 * np.std(train_residuals) < np.std(apply_residuals):
+                print(f'WARNING: Seeing signs of overfitting at {position}!')
+
+        # Compute the full residuals
+        full_residuals = full_targets.ravel() - full_predictions.ravel()
+
+        # Add predictions and residuals to results dictionary
+        results[str(i)]['predictions'] = full_predictions.ravel()
+        results[str(i)]['residuals'] = full_residuals
+
+        # Update our best guess for the planet position:
+        # Choose the "best" exclusion region based on the idea that the planet
+        # region, when not included in the training data, should have a higher
+        # average than the rest of the time series (it's a positive bump), and
+        # should exhibit a bump-like structure.
+        # We then simply pick the highest such bump here. This is by no means
+        # guaranteed to be ideal, but at least it is simple and fast...
+        current_mean = np.mean(full_residuals[signal_mask])
+        if current_mean > best_mean:
+            if has_bump(full_residuals, signal_mask, signal_time):
+                best_mean = current_mean
+                best_signal_mask = signal_mask
+                best_signal_time = signal_time
+                best_predictions = full_predictions
+                best_residuals = full_residuals
+
+    # Add the (final) best predictions and residuals to results dict
+    results['best'] = dict(
+        signal_mask=best_signal_mask,
+        signal_time=best_signal_time,
+        predictions=best_predictions,
+        residuals=best_residuals,
+    )
+
+    return results
+
+
+def get_match_fraction(
+    results: dict,
+    parang: np.ndarray,
+    psf_diameter: float,
+    roi_mask: np.ndarray,
+    n_test_positions: int,
+) -> np.ndarray:
+    """
+    Run consistency tests to compute the fraction of matches for each
+    position. This is the basis for choosing for which pixels we use the
+    baseline model and for which we use the best signal masking model.
+
+    Args:
+        results:
+        parang:
+        psf_diameter:
+        roi_mask:
+        n_test_positions:
+
+    Returns:
+
+    """
+
+    # Define some useful shortcuts
+    signal_times = results['signal_times']
+    frame_size = roi_mask.shape
+
+    # Initialize array in which we keep track of the fraction of test positions
+    # which are consistent with the best signal masking-model for each pixel
+    match_fraction = np.full(frame_size, np.nan)
+
+    # Loop over all positions in the ROI
+    for position in tqdm(get_positions_from_mask(roi_mask), ncols=80):
+
+        # Check if the best signal time for this pixel is NaN, which implies
+        # that for this pixel there exists no best signal-masking model. In
+        # this case, we do not have to run consistency checks and can set the
+        # match fraction directly to 0.
+        signal_time = results['best']['signal_time'][position[0], position[1]]
+        if np.isnan(signal_time):
+            match_fraction[position[0], position[1]] = 0
+            continue
+
+        # Otherwise, we can convert the best signal time to an integer
+        signal_time = int(signal_time)
+
+        # Get the data for the consistency check, that is, a list of spatial
+        # positions and temporal indices (plus masks) at which we also expect
+        # to see a planet if the hypothesis about the planet path that is
+        # implied by the above best signal masking-model is correct.
+        consistency_check_data = get_consistency_check_data(
+            position=position,
+            signal_time=signal_time,
+            parang=parang,
+            frame_size=frame_size,
+            psf_diameter=psf_diameter,
+            n_test_positions=n_test_positions,
+        )
+
+        # Initialize a list to keep track of the matching test position
+        matches: List[int] = list()
+
+        # Loop over all test positions and perform the consistency check
+        for (test_position, test_time, test_mask) in consistency_check_data:
+
+            # Get the number of the best-matching model, that is, the number
+            # of the model whose signal mask is most similar to the test_mask
+            split_number = np.abs(signal_times.ravel() - test_time).argmin()
+
+            # Select the residuals from this model
+            residuals = results[str(split_number)]['residuals'][
+                :, test_position[0], test_position[1]
+            ]
+
+            # Check if the residuals contain NaN, which would indicate that
+            # the test position is outside of the ROI, or was so close to the
+            # star that the `max_signal_length` threshold has caused us not to
+            # learn this model. In this case, we skip the test position instead
+            # of recording a 0 in the matches, because we actually do not know
+            # if this test position matches the original planet hypothesis.
+            if np.isnan(residuals).any():
+                continue
+
+            # Split the residual into the part that should contain a planet
+            # signal and a part that should only contain residual noise
+            signal_residual = residuals[test_mask]
+            noise_residual = residuals[~test_mask]
+
+            # This is the centerpiece of our consistency check: We define our
+            # criteria for counting a match:
+            # 1. Do the residuals have a bump at the expected position?
+            # 2. Is the mean of the (expected) signal part of the residual
+            #    greater than than of the (expected) noise part?
+            criterion_1 = has_bump(residuals, test_mask, test_time)
+            criterion_2 = np.mean(signal_residual) > np.mean(noise_residual)
+
+            # Only if all criteria are True do we count the current test
+            # position as a match for the consistency check
+            if criterion_1 and criterion_2:
+                matches.append(1)
+            else:
+                matches.append(0)
+
+        # Finally, compute the match fraction for the current position.
+        # In case the match list is empty (i.e., we have skipped all test
+        # positions), we manually set the match_fraction to 0.
+        if matches:
+            match_fraction[position[0], position[1]] = np.mean(matches)
+        else:
+            match_fraction[position[0], position[1]] = 0
+
+    return match_fraction
