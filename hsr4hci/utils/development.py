8dde7b4967f982d90efbbb7929e10d35c5a7f782
Timothy Gebhard <mail@timothygebhard.de>
2020-09-15 13:04:14 +0200

    Rename variables to make code easier to read

--- hsr4hci/utils/development.py
+++ hsr4hci/utils/development.py
@@ -72,7 +72,7 @@ def get_effective_pixel_width(
 
 def get_signal_length(
     position: Tuple[int, int],
-    signal_position: int,
+    signal_time: int,
     center: Tuple[float, float],
     parang: np.ndarray,
     psf_diameter: float,
@@ -84,7 +84,7 @@ def get_signal_length(
     produce, that is, the number of (consecutive) frames that will
     contain planet signal.
 
-    Taking the `frame_idx` into account is necessary, because the
+    Taking the `signal_time` into account is necessary because the
     temporal derivative of the parallactic angle is non-constant over
     the course of an observation, but can change up to around 50% for
     some data sets.
@@ -96,7 +96,7 @@ def get_signal_length(
 
     Args:
         position:
-        signal_position:
+        signal_time:
         center:
         parang:
         psf_diameter:
@@ -137,8 +137,8 @@ def get_signal_length(
     gamma = np.arccos(1 - side_length ** 2 / (2 * radius ** 2))
 
     # Find positions
-    value_1 = parang[signal_position] - np.rad2deg(gamma) / 2
-    value_2 = parang[signal_position] + np.rad2deg(gamma) / 2
+    value_1 = parang[signal_time] - np.rad2deg(gamma) / 2
+    value_2 = parang[signal_time] + np.rad2deg(gamma) / 2
     if ascending:
         position_1 = np.searchsorted(parang, value_1, side='left')
         position_2 = np.searchsorted(parang, value_2, side='right')
@@ -148,53 +148,53 @@ def get_signal_length(
 
     # Compute the length before and after the peak (because the signal will, in
     # general, not be symmetric around the peak)
-    length_1 = int(1.2 * (signal_position - position_1))
-    length_2 = int(1.2 * (position_2 - signal_position))
+    length_1 = int(1.2 * (signal_time - position_1))
+    length_2 = int(1.2 * (position_2 - signal_time))
 
     return length_1, length_2
 
 
-def get_noise_signal_idx(
+def get_noise_signal_masks(
     position: Tuple[int, int],
     parang: np.ndarray,
-    n_signal_positions: int,
+    n_signal_times: int,
     frame_size: Tuple[int, int],
     psf_diameter: float,
     signal_length_threshold: float = 0.7,
 ) -> List[Tuple[np.ndarray, np.ndarray, int]]:
     """
-    Loop over possible planet positions (in a time series), and for each
-    presumed position, return the corresponding train and apply indices.
-    Train indices are those that, under the assumption regarding the
-    signal position, do not contain planet signal, while the apply
-    indices are their complement.
+    Generate the masks for training a series of models where different
+    possible planet positions are masked out during training.
 
-    The results are returned as binary arrays that can, for example, be
-    used to select frames from the stack.
+    This function places `n_signal_times` points in time uniformly over
+    the course of the whole observation. For each such time point, we
+    then assume that the planet signal is present at this point in time,
+    and generate a binary mask that indicates all points in time that,
+    under this hypothesis, would also contain planet signal.
 
     Args:
-        position: An integer tuple `(x, y)` specifying the position of
-            the pixel for which we are computing the indices.
+        position: An integer tuple `(x, y)` specifying the spatial
+            position of the pixel for which we are computing the masks.
         parang: A numpy array of shape `(n_frames, )` containing the
             parallactic angles.
-        n_signal_positions: The number of different possible planet
-            positions for which to return indices.
+        n_signal_times: The number of different possible temporal
+            positions of the planet signal for which to return a mask.
         frame_size: A tuple `(width, height)` specifying the spatial
             size of the stack.
         psf_diameter: The diameter of the PSF template (in pixels).
         signal_length_threshold: A value in [0.0, 1.0] which describes
             the maximum value of `expected_signal_length / n_frames`,
-            which wil determine for which pixels we do not want to use
-            the "exclude a potential signal region"-approach, because
+            which will determine for which pixels we do not want to use
+            the "mask out a potential signal region"-approach, because
             the potential signal region is too large to leave us with a
             reasonable amount of training data.
 
     Returns:
         This function returns a list of up to `n_position` 3-tuples
         of the following form:
-            `(noise_idx, signal_idx, signal_position)`.
+            `(noise_mask, signal_mask, signal_time)`.
         The first two elements are 1D binary numpy arrays of length
-        `n_frames`, wheres the last element is an integer giving the
+        `n_frames`, whereas the last element is an integer giving the
         position of the peak of the planet signal.
     """
 
@@ -205,47 +205,43 @@ def get_noise_signal_idx(
     # Initialize lists in which we store the results
     results = list()
 
-    # Generate `n_positions` different possible signal positions (i.e.,
-    # temporal indices specifying the center of the planet signal) that are
-    # uniformly distributed over the entire time series
-    signal_positions = np.linspace(0, n_frames, n_signal_positions + 2)[1:-1]
+    # Generate `n_signal_times` different possible points in time (distributed
+    # uniformly over the observation) at which we planet signal could be
+    signal_times = np.linspace(0, n_frames - 1, n_signal_times)
 
-    # Loop over all positions to generate the corresponding indices
-    for signal_position in signal_positions:
+    # Loop over all these time points to generate the corresponding indices
+    for signal_time in signal_times:
 
-        # Make sure the signal position is an integer
-        signal_position = int(signal_position)
+        # Make sure the signal time is an integer (we use it as an index)
+        signal_time = int(signal_time)
 
-        # Compute the expected signal length at this position and index
+        # Compute the expected signal length at this position and time
         length_1, length_2 = get_signal_length(
             position=position,
-            signal_position=signal_position,
+            signal_time=signal_time,
             center=center,
             parang=parang,
             psf_diameter=psf_diameter,
         )
 
         # Check if the expected signal length is larger than the threshold.
-        # In this case, we do not compute the noise and signal indices, but
-        # skip this signal position entirely.
+        # In this case, we do not compute the noise and signal masks, but
+        # skip this signal time.
         if (length_1 + length_2) / n_frames > signal_length_threshold:
-            return []
+            continue
 
-        # Initialize apply_idx as all False
-        signal_idx = np.zeros(n_frames).astype(bool)
+        # Construct the signal mask
+        signal_mask = np.full(n_frames, False)
+        position_1 = max(0, signal_time - length_1)
+        position_2 = min(n_frames, signal_time + length_2)
+        signal_mask[position_1:signal_time] = True
+        signal_mask[signal_time:position_2] = True
 
-        # Now add a block of 1s (that matches the expected signal length) to
-        # the apply_idx centered on the current signal position
-        position_1 = max(0, signal_position - length_1)
-        position_2 = min(n_frames, signal_position + length_2)
-        signal_idx[position_1:signal_position] = True
-        signal_idx[signal_position:position_2] = True
+        # Compute noise_mask as the complement of the signal_mask
+        noise_mask = np.logical_not(signal_mask)
 
-        # Compute train_idx as the complement of the apply_idx
-        noise_idx = np.logical_not(signal_idx)
-
-        # Store the current (noise_idx, signal_idx, signal_position) tuple
-        results.append((noise_idx, signal_idx, signal_position))
+        # Store the current (noise_mask, signal_mask, signal_time) tuple
+        results.append((noise_mask, signal_mask, signal_time))
 
     return results
 
@@ -310,7 +306,7 @@ def get_psf_diameter(
 
 
 def has_bump(
-    array: np.ndarray, signal_idx: np.ndarray, signal_position: int,
+    array: np.ndarray, signal_idx: np.ndarray, signal_time: int,
 ) -> bool:
     """
     Check if a given `array` (typically residuals) has a positive bump
@@ -318,14 +314,14 @@ def has_bump(
 
     Currently, the used heuristic is extremely simple:
     We split the search region into two parts, based on the given
-    `signal_position`, and fit both parts with a linear model.
+    `signal_time`, and fit both parts with a linear model.
     If the first regression returns a positive slope, and the second
     regression returns a negative slope, the function returns True.
 
     Args:
         array: A 1D numpy array in which we search for a bump.
         signal_idx: A 1D numpy array indicating the search region.
-        signal_position: The index specifying the "exact" location
+        signal_time: The index specifying the "exact" location
             where the peak of the bump should be located.
 
     Returns:
@@ -338,19 +334,19 @@ def has_bump(
     signal_start, signal_end = all_idx[signal_idx][np.array([0, -1])]
 
     # Prepare predictors and targets for the two linear fits
-    predictors_1 = np.arange(signal_start, signal_position).reshape(-1, 1)
-    predictors_2 = np.arange(signal_position, signal_end).reshape(-1, 1)
-    targets_1 = array[signal_start:signal_position]
-    targets_2 = array[signal_position:signal_end]
+    predictors_1 = np.arange(signal_start, signal_time).reshape(-1, 1)
+    predictors_2 = np.arange(signal_time, signal_end).reshape(-1, 1)
+    targets_1 = array[signal_start:signal_time]
+    targets_2 = array[signal_time:signal_end]
 
     # Fit the two models to the two parts of the search region and get the
     # slope of the model
-    if len(predictors_1) > 0:
+    if len(predictors_1) > 2:
         model_1 = LinearRegression().fit(predictors_1, targets_1)
         slope_1 = model_1.coef_[0]
     else:
         slope_1 = 1
-    if len(predictors_2) > 0:
+    if len(predictors_2) > 2:
         model_2 = LinearRegression().fit(predictors_2, targets_2)
         slope_2 = model_2.coef_[0]
     else:
@@ -361,21 +357,21 @@ def has_bump(
 
 def get_consistency_check_data(
     position: Tuple[int, int],
-    signal_position: np.ndarray,
+    signal_time: np.ndarray,
     parang: np.ndarray,
     frame_size: Tuple[int, int],
     psf_diameter: float,
     n_test_positions: int = 5,
 ) -> List[Tuple[Tuple[int, int], int, np.ndarray]]:
     """
-    Given a (spatial) `position` and a (temporal) `signal_position`,
+    Given a (spatial) `position` and a (temporal) `signal_time`,
     construct the planet path that is implied by these values and
     return `n_test_positions` new positions on that arc with the
     respective expected temporal signal position at these positions.
 
     Args:
         position:
-        signal_position:
+        signal_time:
         parang:
         frame_size:
         psf_diameter:
@@ -390,12 +386,12 @@ def get_consistency_check_data(
     center = (frame_size[0] / 2, frame_size[1] / 2)
 
     # Assuming that the peak of the signal is at pixel `position` at the time
-    # t=`signal_position`, use our knowledge about the movement of the planet
+    # t=`signal_time`, use our knowledge about the movement of the planet
     # to compute the (spatial) position of the planet at point t=0.
     starting_position = rotate_position(
         position=position,
         center=center,
-        angle=-float(parang[signal_position] - parang[0])
+        angle=-float(parang[signal_time] - parang[0])
     )
 
     # Create `n_test_times` (uniformly distributed) points in time at which
@@ -421,7 +417,7 @@ def get_consistency_check_data(
         # Get the expected signal length at this position
         length_1, length_2 = get_signal_length(
             position=test_position,
-            signal_position=test_time,
+            signal_time=test_time,
             center=center,
             parang=parang,
             psf_diameter=psf_diameter,
