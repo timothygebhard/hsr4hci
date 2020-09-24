02fd502d660070101c6b66e7da0e02d48bb56574
Timothy Gebhard <mail@timothygebhard.de>
2020-09-24 12:38:52 +0200

    Add more unit tests

--- tests/test_utils/test_general.py
+++ tests/test_utils/test_general.py
@@ -6,13 +6,21 @@ Tests for general.py
 # IMPORTS
 # -----------------------------------------------------------------------------
 
+from _pytest.tmpdir import TempPathFactory
+from deepdiff import DeepDiff
+
 import numpy as np
 import pytest
 
-from hsr4hci.utils.general import crop_center, \
-    get_from_nested_dict, \
-    set_in_nested_dict, \
-    rotate_position
+from hsr4hci.utils.general import (
+    crop_center,
+    get_from_nested_dict,
+    get_md5_checksum,
+    prestack_array,
+    rotate_position,
+    set_in_nested_dict,
+    split_into_n_chunks
+)
 
 
 # -----------------------------------------------------------------------------
@@ -82,6 +90,48 @@ def test__set_in_nested_dict() -> None:
     assert dictionary == {'a': {'b': 23}}
 
 
+def test__prestack_array() -> None:
+
+    # Test case 0: stacking_factor = 1
+    array = np.random.normal(0, 1, 10)
+    output = prestack_array(
+        array=array, stacking_factor=1, stacking_function=np.median
+    )
+    assert np.allclose(output, array)
+
+    # Test case 1: 1D, median
+    array = np.array([1, 3, 5, 7, 9, 11])
+    target = np.array([2, 6, 10])
+    output = prestack_array(
+        array=array, stacking_factor=2, stacking_function=np.median
+    )
+    assert np.allclose(output, target)
+
+    # Test case 2: 1D, mean
+    array = np.array([1, 2, 3, 4, 5, 6])
+    target = np.array([2, 5])
+    output = prestack_array(
+        array=array, stacking_factor=3, stacking_function=np.mean
+    )
+    assert np.allclose(output, target)
+
+    # Test case 3: 1D, median, stacking factor does not fit array length
+    array = np.array([1, 3, 5, 7, 9])
+    target = np.array([2, 6, 9])
+    output = prestack_array(
+        array=array, stacking_factor=2, stacking_function=np.median
+    )
+    assert np.allclose(output, target)
+
+    # Test case 4: 2D, median
+    array = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    target = np.array([[2, 3], [6, 7]])
+    output = prestack_array(
+        array=array, stacking_factor=2, stacking_function=np.median
+    )
+    assert np.allclose(output, target)
+
+
 def test__rotate_position() -> None:
 
     position = (10, 10)
@@ -94,3 +144,49 @@ def test__rotate_position() -> None:
     assert np.allclose(rotate_position(position, center, 180), (-10, -10))
     assert np.allclose(rotate_position(position, center, 360), (10, 10))
     assert np.allclose(rotate_position(position, center, 720), (10, 10))
+
+
+def test__get_md5_checksum(tmp_path_factory: TempPathFactory) -> None:
+
+    # Define location of test file in temporary directory
+    test_dir = tmp_path_factory.mktemp('general', numbered=False)
+    file_path = test_dir / 'dummy.txt'
+
+    # Create dummy file with known checksum
+    with open(file_path, 'w') as text_file:
+        text_file.write('This is a dummy file.')
+
+    assert get_md5_checksum(file_path) == 'f30f68c2ec607099c3ade01dba0571d8'
+
+
+def test__split_into_n_chunks() -> None:
+
+    # Test case 0a: Check that an error is raised when len(sequence) < n_chunks
+    with pytest.raises(ValueError) as error:
+        sequence_0 = (0, 1)
+        split_into_n_chunks(sequence=sequence_0, n_chunks=3)
+    assert 'n_chunks is greater than len(sequence):' in str(error)
+
+    # Test case 0b: Check that an error is raised when n_chunks <= 0
+    with pytest.raises(ValueError) as error:
+        sequence_0 = (0, 1)
+        split_into_n_chunks(sequence=sequence_0, n_chunks=0)
+    assert 'n_chunks must be a positive integer!' in str(error)
+
+    # Test case 1: tuple, len(sequence) mod n_chunks == 0
+    sequence_1 = (1, 2, 3, 4, 5, 6)
+    target_1 = [(1, 2), (3, 4), (5, 6)]
+    output_1 = split_into_n_chunks(sequence=sequence_1, n_chunks=3)
+    assert not DeepDiff(output_1, target_1)
+
+    # Test case 2: list, len(sequence) mod n_chunks != 0
+    sequence_2 = [1, 2, 3, 4, 5]
+    target_2 = [[1, 2], [3, 4], [5]]
+    output_2 = split_into_n_chunks(sequence=sequence_2, n_chunks=3)
+    assert not DeepDiff(output_2, target_2)
+
+    # Test case 1: np.ndarray, len(sequence) mod n_chunks == 0
+    sequence_3 = np.array(['a', 'b', 'c', 'd'])
+    target_3 = [np.array(['a', 'b']), np.array(['c', 'd'])]
+    output_3 = split_into_n_chunks(sequence=sequence_3, n_chunks=2)
+    assert not DeepDiff(output_3, target_3)
