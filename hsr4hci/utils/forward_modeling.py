895fc0acb73fce6840ea41a102b31232a1dc68ea
Timothy Gebhard <mail@timothygebhard.de>
2020-11-26 22:29:28 +0100

    Rename get_signal_stack() -> compute_signal_stack() and refactor

--- hsr4hci/utils/forward_modeling.py
+++ hsr4hci/utils/forward_modeling.py
@@ -1,50 +1,55 @@
 """
-Utility functions for forward modeling (necessary for toy data sets!).
+Utility functions for forward modeling.
 """
 
 # -----------------------------------------------------------------------------
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from cmath import polar
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple, Union
+
+import math
 
 from scipy.interpolate import RegularGridInterpolator
 
 import numpy as np
 
 from hsr4hci.utils.general import add_array_with_interpolation, rotate_position
+from hsr4hci.utils.utils import check_frame_size, check_cartesian_position
 
 
 # -----------------------------------------------------------------------------
 # FUNCTION DEFINITIONS
 # -----------------------------------------------------------------------------
 
-def get_signal_stack(
+def compute_signal_stack(
     position: Tuple[float, float],
     frame_size: Tuple[int, int],
     parang: np.ndarray,
-    psf_cropped: np.ndarray,
-) -> Tuple[np.ndarray, list]:
+    psf_template: np.ndarray,
+    return_planet_positions: bool = False,
+) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
     """
-    Compute the forward model: Assume that at time t=0, the planet is
-    at the given `position. The apparent motion of the sky (and thus `
-    the planet) is given by the parallactic angles in `parang`. This
-    function now takes the provided cropped and masked PSF. Then, an
-    empty stack of size `(len(parang), *frame_size)` is created, and
-    for each time t, the cropped PSF template is added into the stack
-    at the correct position according to parang[t].
+    Compute a "forward model": Given the parallactic angles in `parang`
+    and the PSF template in `psf_cropped`, create a "signal only"-stack
+    of shape `(n_frames, width, height)` where in each frame a signal is
+    injected such that, if the whole stack is de-rotated and combined,
+    we find the planet signal at the target `position`.
 
     Args:
-        position: A tuple containing the initial planet position (i.e.,
-            we compute the forward model under the assumption that the
-            planet is at this position for t=0).
-        frame_size: A tuple with the spatial size `(x_size, y_size)`
-            of the forward model stack to be created.
-        parang: A numpy array containing the parallactic angle for
-            every frame.
-        psf_cropped: A numpy array containing the cropped and masked
-            PSF template, as it is returned by `crop_psf_template()`.
+        position: A tuple `(x, y)` containing the desired "final" (i.e.,
+            after de-rotating) position of the planet.
+            Coordinate convention:
+                system: Cartesian, origin: astropy, order: numpy
+        frame_size: A 2-tuple `(width, height)` of integers specifying
+            the spatial size of the signal stack to be created.
+        parang: A numpy array of shape `(n_frames, )` containing the
+            parallactic angle for every frame.
+        psf_template: The (normalized and cropped) PSF template that is
+            used for the signal
+        return_planet_positions: Whether or not to return a 2D numpy
+            array of shape `(n_frames, 2)` containing planet position
+            for each frame.
 
     Returns:
         A tuple consisting of a numpy array containing the forward model
@@ -54,39 +59,34 @@ def get_signal_stack(
         the position of the planet in the forward model for every frame.
     """
 
+    # Run basic sanity checks
+    check_frame_size(frame_size)
+    check_cartesian_position(position)
+
     # Define some shortcuts
     n_frames = len(parang)
-    frame_width, frame_height = frame_size
-    frame_center = (frame_width / 2, frame_height / 2)
+    center = (frame_size[0] / 2, frame_size[1] / 2)
 
-    # Compute relative rotation angles (i.e., the rotation relative to t=0)
-    rotation_angles = parang - parang[0]
+    # Convert parang from degrees to radians
+    parang = np.deg2rad(parang)
 
     # Compute polar representation of initial position
-    r, phi = polar(
-        complex(position[1] - frame_center[1], position[0] - frame_center[0])
-    )
+    x_centered = position[0] - center[0]
+    y_centered = position[1] - center[1]
+    rho = math.sqrt(x_centered ** 2 + y_centered ** 2)
+    phi = math.atan2(y_centered, x_centered)
+
+    # Compute planet positions for each frame
+    x_positions = rho * np.cos(phi - parang) + center[0]
+    y_positions = rho * np.sin(phi - parang) + center[1]
 
     # Initialize empty signal stack and list of planet positions
-    signal_stack = np.zeros((n_frames, frame_width, frame_height))
-    planet_positions = list()
+    signal_stack = np.zeros((n_frames, frame_size[0], frame_size[1]))
+    planet_positions = np.column_stack((x_positions, y_positions))
 
     # Loop over all frames and add the cropped and masked PSF template at
     # the position that corresponds to the respective parallactic angle
-    for i in range(n_frames):
-
-        # Compute injection position for the current frame: Rotate the initial
-        # position by the correct angle (using the exponential representation
-        # of 2D vectors) and convert the resulting position back to Cartesian
-        # coordinates. Store this value to a list that we will return along
-        # with the signal stack we have constructed.
-        theta = np.deg2rad(rotation_angles[i])
-        new_complex_position = r * np.exp(1j * (phi - theta))
-        injection_position = (
-            np.imag(new_complex_position) + frame_center[0],
-            np.real(new_complex_position) + frame_center[1],
-        )
-        planet_positions.append(injection_position)
+    for i, position in zip(np.arange(n_frames), planet_positions):
 
         # Add cropped and masked PSF template at the injection_position.
         # The add_array_with_interpolation() function is able to automatically
@@ -94,11 +94,14 @@ def get_signal_stack(
         # or the PSF template exceeds the bounds of the signal stack.
         signal_stack[i] = add_array_with_interpolation(
             array_large=signal_stack[i],
-            array_small=psf_cropped,
-            position=injection_position,
+            array_small=psf_template,
+            position=position[::-1],
         )
 
-    return signal_stack, planet_positions
+    # Only return planet positions if explicitly requested
+    if return_planet_positions:
+        return signal_stack, planet_positions
+    return signal_stack
 
 
 def get_time_series_for_position(
@@ -141,6 +144,10 @@ def get_time_series_for_position(
         The time series for `position` computed under the hypothesis for
         the planet movement explained above.
     """
+
+    # Run basic sanity checks
+    check_frame_size(frame_size)
+    check_cartesian_position(position)
 
     # Compute center of the frame
     center = (frame_size[0] / 2, frame_size[1] / 2)
@@ -242,11 +249,11 @@ def get_planet_paths(
         )
 
         # Compute a forward model for this planet
-        planet_model, planet_positions = get_signal_stack(
+        planet_model, planet_positions = compute_signal_stack(
             position=starting_position,
             frame_size=(x_size, y_size),
             parang=parang,
-            psf_cropped=psf_cropped,
+            psf_template=psf_cropped,
         )
 
         # Convert the planet positions to a 2D array and store them
