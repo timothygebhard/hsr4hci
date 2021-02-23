eb41a5d4a7460afb59bb48ecae5fb23dfa57bb77
Timothy Gebhard <mail@timothygebhard.de>
2021-02-23 15:19:32 +0100

    Update and simplify get_signal_length()

--- hsr4hci/utils/signal_masking.py
+++ hsr4hci/utils/signal_masking.py
@@ -6,7 +6,6 @@ Utility functions for signal masking and related tasks.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from cmath import polar
 from itertools import product
 from typing import Dict, List, Tuple, Union
 
@@ -63,9 +62,10 @@ def assemble_signal_masking_residuals(
 def get_signal_length(
     position: Tuple[float, float],
     signal_time: int,
-    center: Tuple[float, float],
+    frame_size: Tuple[int, int],
     parang: np.ndarray,
-    psf_radius: float,
+    psf_template: np.ndarray,
+    threshold: float = 0.2,
 ) -> Tuple[int, int]:
     """
     Get a simple analytical estimate of the length (in units of frames)
@@ -82,152 +82,39 @@ def get_signal_length(
     Args:
         position:
         signal_time:
-        center:
+        frame_size:
         parang:
-        psf_radius:
+        psf_template:
+        threshold:
 
     Returns:
 
     """
 
-    # Check if the parallactic angles are sorted in ascending order
-    if np.allclose(parang, sorted(parang)):
-        ascending = True
-    elif np.allclose(parang, sorted(parang, reverse=True)):
-        ascending = False
-    else:
-        raise ValueError('parang is not sorted!')
-
-    # Compute radius of position
-    radius = np.sqrt(
-        (position[0] - center[0]) ** 2 + (position[1] - center[1]) ** 2
+    # Compute the expected time series
+    expected_time_series = get_time_series_for_position(
+        position=position,
+        signal_time=signal_time,
+        frame_size=frame_size,
+        parang=parang,
+        psf_template=psf_template,
+        interpolation='bilinear',
     )
 
-    # Compute the effective pixel width of the position
-    effective_pixel_width = get_effective_pixel_width(
-        position=position, center=center
-    )
+    # Threshold it
+    signal_mask = expected_time_series > threshold
 
-    # Convert "effective pixel width + PSF diameter" to an angle at this radius
-    # using the cosine theorem. First, compute the length of the side that we
-    # want to convert into an angle:
-    side_length = effective_pixel_width + 2 * psf_radius
-
-    # Degenerate case: for too small separations, if the center is ever on the
-    # pixel, the pixel will *always* contain planet signal.
-    if side_length > 2 * radius:
-        return 0, len(parang)
-
-    # Otherwise, we can convert the side length into an angle
-    gamma = np.arccos(1 - side_length ** 2 / (2 * radius ** 2))
-
-    # Find positions
-    value_1 = parang[signal_time] - np.rad2deg(gamma) / 2
-    value_2 = parang[signal_time] + np.rad2deg(gamma) / 2
-    if ascending:
-        position_1 = np.searchsorted(parang, value_1, side='left')
-        position_2 = np.searchsorted(parang, value_2, side='right')
-    else:
-        position_2 = np.searchsorted(-parang, -value_1, side='left')
-        position_1 = np.searchsorted(-parang, -value_2, side='right')
+    # Find index of first and last 1 on this binary array
+    indices = np.arange(len(signal_mask))[signal_mask]
+    position_1 = np.min(indices)
+    position_2 = np.max(indices)
 
     # Compute the length before and after the peak (because the signal will, in
     # general, not be symmetric around the peak)
-    length_1 = int(1.0 * (signal_time - position_1))
-    length_2 = int(1.0 * (position_2 - signal_time))
+    before = signal_time - position_1
+    after = position_2 - signal_time
 
-    return length_1, length_2
-
-
-def get_signal_masks_analytically(
-    position: Tuple[int, int],
-    parang: np.ndarray,
-    n_signal_times: int,
-    frame_size: Tuple[int, int],
-    psf_radius: float,
-    max_signal_length: float = 0.7,
-) -> List[Tuple[int, np.ndarray, int]]:
-    """
-    Generate the masks for training a series of models where different
-    possible planet positions are masked out during training.
-
-    This function places `n_signal_times` points in time uniformly over
-    the course of the whole observation. For each such time point, we
-    then assume that the planet signal is present at this point in time,
-    and generate a binary mask that indicates all points in time that,
-    under this hypothesis, would also contain planet signal.
-
-    This function uses an "analytical" approximation based on
-    `get_effective_pixel_width()` and `get_signal_length()` to estimate
-    the signal masks, which is fast, but not as accurate as really
-    modeling the movement of the planet signal in time.
-
-    Args:
-        position: An integer tuple `(x, y)` specifying the spatial
-            position of the pixel for which we are computing the masks.
-        parang: A numpy array of shape `(n_frames, )` containing the
-            parallactic angles.
-        n_signal_times: The number of different possible temporal
-            positions of the planet signal for which to return a mask.
-        frame_size: A tuple `(width, height)` specifying the spatial
-            size of the stack.
-        psf_radius: The radius of the PSF template (in pixels).
-        max_signal_length: A value in [0.0, 1.0] which describes the
-            maximum value of `expected_signal_length / n_frames`, which
-            will determine for which pixels we do not want to use the
-            "mask out a potential signal region"-approach, because the
-            potential signal region is too large to leave us with a
-            reasonable amount of training data.
-
-    Returns:
-        This function returns a list of up to `n_position` 3-tuples
-        of the following form:
-            `(signal_time_index, signal_mask, signal_time)`.
-    """
-
-    # Define shortcuts
-    n_frames = len(parang)
-    center = (frame_size[0] / 2, frame_size[1] / 2)
-
-    # Initialize lists in which we store the results
-    results = list()
-
-    # Generate `n_signal_times` different possible points in time (distributed
-    # uniformly over the observation) at which we planet signal could be
-    signal_times = get_signal_times(n_frames, n_signal_times)
-
-    # Loop over all these time points to generate the corresponding indices
-    for i, signal_time in enumerate(signal_times):
-
-        # Make sure the signal time is an integer (we use it as an index)
-        signal_time = int(signal_time)
-
-        # Compute the expected signal length at this position and time
-        length_1, length_2 = get_signal_length(
-            position=position,
-            signal_time=signal_time,
-            center=center,
-            parang=parang,
-            psf_radius=psf_radius,
-        )
-
-        # Check if the expected signal length is larger than the threshold.
-        # In this case, we do not compute the noise and signal masks, but
-        # skip this signal time.
-        if (length_1 + length_2) / n_frames > max_signal_length:
-            continue
-
-        # Construct the signal mask
-        signal_mask = np.full(n_frames, False)
-        position_1 = max(0, signal_time - length_1)
-        position_2 = min(n_frames, signal_time + length_2)
-        signal_mask[position_1:signal_time] = True
-        signal_mask[signal_time:position_2] = True
-
-        # Store the current (signal_time_index, signal_mask, signal_time) tuple
-        results.append((i, signal_mask, signal_time))
-
-    return results
+    return before, after
 
 
 def get_signal_mask(
