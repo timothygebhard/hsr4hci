6ee838a03efaf92dfb8626a3757bbf32371d3498
Timothy Gebhard <mail@timothygebhard.de>
2021-02-23 15:35:42 +0100

    Simply get_match_fraction() (always use cosine similarity)

--- hsr4hci/utils/consistency_checks.py
+++ hsr4hci/utils/consistency_checks.py
@@ -6,9 +6,8 @@ Utility functions for consistency checks and related tasks.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics.pairwise import cosine_similarity
 
 from tqdm.auto import tqdm
@@ -17,7 +16,7 @@ import numpy as np
 
 from hsr4hci.utils.coordinates import get_center, cartesian2polar
 from hsr4hci.utils.forward_modeling import add_fake_planet
-from hsr4hci.utils.general import fast_corrcoef, find_closest, rotate_position
+from hsr4hci.utils.general import find_closest, rotate_position
 from hsr4hci.utils.masking import get_positions_from_mask
 
 
@@ -25,77 +24,11 @@ from hsr4hci.utils.masking import get_positions_from_mask
 # FUNCTION DEFINITIONS
 # -----------------------------------------------------------------------------
 
-def get_bump_height(
-    array: np.ndarray,
-    signal_mask: np.ndarray,
-    signal_time: int,
-) -> float:
-    """
-    Check if a given `array` (typically residuals) has a positive bump
-    in the region that is indicated by the given `signal_mask`, and
-    return the `bump_height` (which defaults to 0 if there is no bump).
-
-    To check if the selected region has a bump-like shape, we use a very
-    simple heuristic: We split the search region into two parts, based
-    on the given `signal_time`, and fit both parts with a linear model.
-    If the first regression returns a positive slope, and the second
-    regression returns a negative slope, we compute the bump height as
-    the median of the search region; otherwise, we return 0. We also
-    return 0 if the median of the search region is actually negative.
-
-    Args:
-        array: A 1D numpy array in which we search for a bump.
-        signal_mask: A 1D numpy array indicating the search region.
-        signal_time: The index specifying the "exact" location
-            where the peak of the bump should be located.
-
-    Returns:
-        The height of the bump (computed as the median of the part of
-        `array` selected by the `signal_mask`, or zero if the median is
-        negative), in case there is a positive bump; otherwise zero.
-    """
-
-    # Get the start and end position of the signal_mask
-    all_idx = np.arange(len(array))
-    signal_start, signal_end = all_idx[signal_mask][np.array([0, -1])]
-
-    # Prepare predictors and targets for the two linear fits
-    predictors_1 = np.arange(signal_start, signal_time).reshape(-1, 1)
-    predictors_2 = np.arange(signal_time, signal_end).reshape(-1, 1)
-    targets_1 = array[signal_start:signal_time]
-    targets_2 = array[signal_time:signal_end]
-
-    # Fit regions with linear models and get slopes
-    if len(predictors_1) > 2:
-        model_1 = LinearRegression().fit(predictors_1, targets_1)
-        slope_1 = model_1.coef_[0]
-    else:
-        slope_1 = 1
-    if len(predictors_2) > 2:
-        model_2 = LinearRegression().fit(predictors_2, targets_2)
-        slope_2 = model_2.coef_[0]
-    else:
-        slope_2 = -1
-
-    # Define criteria for "is there a positive bump at the given signal time?"
-    criterion_1 = bool(slope_1 > 0 > slope_2)
-    criterion_2 = bool(
-        np.nanmedian(array[signal_mask]) > np.nanmedian(array[~signal_mask])
-    )
-
-    # If there is a bump, we return its height; otherwise we return 0
-    if criterion_1 and criterion_2:
-        bump_height = float(np.nanmedian(array[signal_mask]))
-        return max((0, bump_height))
-    return 0
-
-
 def get_match_fraction(
     hypotheses: np.ndarray,
-    results: Dict[str, np.ndarray],
+    results: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]],
     parang: np.ndarray,
     psf_template: np.ndarray,
-    metric_function: str = 'cosine_similarity',
     verbose: bool = True,
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
@@ -131,11 +64,6 @@ def get_match_fraction(
         parang: A 1D numpy array of shape `(n_frames, )` containing the
             parallactic angle.
         psf_template: A 2D numpy array containing the PSF template.
-        metric_function: A string containing the metric that is used to
-            measure the similarity between an expected signal and the
-            observed residual. Must be one of the following:
-                "cosine_similarity" (default), "bump_height",
-                "correlation_coefficient"
         verbose: Whether or not to show a progress bar.
 
     Returns:
@@ -154,7 +82,7 @@ def get_match_fraction(
     signal_times = sorted(
         list(map(int, filter(lambda _: _.isdigit(), results.keys())))
     )
-    frame_size = hypotheses.shape
+    frame_size = (hypotheses.shape[0], hypotheses.shape[1])
     center = get_center(frame_size)
 
     # Define ROI mask: all positions for which we actually have a hypothesis
@@ -199,11 +127,12 @@ def get_match_fraction(
 
         # Compute the *full* expected signal stack under this hypothesis
         expected_stack = add_fake_planet(
-            stack=np.zeros((n_frames, ) + frame_size),
+            stack=np.zeros((n_frames,) + frame_size),
             parang=parang,
             psf_template=psf_template,
             polar_position=cartesian2polar(
-                position=final_position, frame_size=frame_size,
+                position=(final_position[0], final_position[1]),
+                frame_size=frame_size,
             ),
             magnitude=0,
             extra_scaling=1,
@@ -247,27 +176,15 @@ def get_match_fraction(
             if np.isnan(b).any():
                 continue
 
-            # Compute the desired target metric by comparing the residual
-            # with the expected planet signal
-            if metric_function == 'bump_height':
-                signal_mask = b > 0.2
-                metric = get_bump_height(b, signal_mask, peak_time)
-            elif metric_function == 'cosine_similarity':
-                metric = cosine_similarity(a.reshape(1, -1), b.reshape(1, -1))
-            elif metric_function == 'correlation_coefficient':
-                metric = fast_corrcoef(a, b)
-            else:
-                raise ValueError(
-                    'Invalid value for metric_function, must be one of '
-                    'the following: "bump_height", "cosine_similarity", '
-                    '"correlation_coefficient".'
+            # Compute the similarity between the expected signal and the
+            # actual "best" residual (and make sure it is between 0 and 1)
+            metric = float(
+                np.clip(
+                    cosine_similarity(X=a.reshape(1, -1), Y=b.reshape(1, -1)),
+                    a_min=0,
+                    a_max=1,
                 )
-
-            # Make sure that `metric` is in [0, 1]. Here, we can also clip the
-            # value of `metric` in case we use the bump height as the metric
-            # because we only check if there is a bump, and do not look for
-            # the "best" bump (unlike in the find_hypothesis() function).
-            metric = float(np.clip(metric, a_min=0, a_max=1))
+            )
             matches.append(metric)
 
         # Compute "correction factor" to down-weigh those pixels where not
