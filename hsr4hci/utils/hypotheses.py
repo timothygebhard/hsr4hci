b281aaaf6c4d390fd518372ca8e5efd6e445c02d
Timothy Gebhard <mail@timothygebhard.de>
2021-03-26 19:49:44 +0100

    Update functions to return not only hypotheses, but also similarities

--- hsr4hci/utils/hypotheses.py
+++ hsr4hci/utils/hypotheses.py
@@ -30,29 +30,34 @@ def get_all_hypotheses(
     n_signal_times: int,
     frame_size: Tuple[int, int],
     psf_template: np.ndarray,
-) -> np.ndarray:
+    minimum_similarity: float = 0.0,
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     This is a convenience function which wraps the loop over the ROI
     to call `find_hypothesis()` for every spatial pixel. See there for
     a full documentation of all parameters.
     """
 
-    # Initialize hypotheses array
+    # Initialize array for hypotheses and similarities
     hypotheses = np.full(frame_size, np.nan)
+    similarities = np.full(frame_size, np.nan)
 
     # Loop over all spatial positions and find the respective hypothesis
     for position in tqdm(get_positions_from_mask(roi_mask), ncols=80):
 
-        hypotheses[position[0], position[1]] = find_hypothesis(
+        signal_time, similarity = find_hypothesis(
             results=results,
             position=position,
             parang=parang,
             n_signal_times=n_signal_times,
             frame_size=frame_size,
             psf_template=psf_template,
+            minimum_similarity=minimum_similarity,
         )
+        hypotheses[position[0], position[1]] = signal_time
+        similarities[position[0], position[1]] = similarity
 
-    return hypotheses
+    return hypotheses, similarities
 
 
 def find_hypothesis(
@@ -62,7 +67,8 @@ def find_hypothesis(
     n_signal_times: int,
     frame_size: Tuple[int, int],
     psf_template: np.ndarray,
-) -> float:
+    minimum_similarity: float = 0.0,
+) -> Tuple[float, float]:
     """
     Take the dictionary containing the full training results and find,
     for a given spatial `position`, the time T at which we suspect there
@@ -90,16 +96,22 @@ def find_hypothesis(
             pixels) of the frames that we are working with.
         psf_template: A 2D numpy array containing the unsaturated PSF
             template for the data set.
+        minimum_similarity: Minimum cosine similarity between the
+            expected and the observed signal for the signal to be
+            counted as a hypothesis.
 
     Returns:
-        Either a time T, which is the best guess for the `signal_time`
-        of the planet signal in `position`, or `np.nan` if the position
-        does not seem to contain any planet signal at any time.
+        A tuple `(signal_time, similarity)`, where `signal_time` is
+        the best guess for the `signal_time` of the planet signal in
+        `position`, and `score` is the corresponding cosine similarity
+        between the expected and the observed signal.
+        If the `position` does not seem to contain a planet signal at
+        any time, both of these values are NaN.
     """
 
     # Initialize variables in which we store the optimum
     best_signal_time = np.nan
-    best_metric = 0.0
+    best_similarity = 0.0
 
     # Loop over the temporal grid to find the best hypothesis
     for signal_time in get_signal_times(
@@ -125,19 +137,21 @@ def find_hypothesis(
 
         # Compute the cosine similarity, which serves as a metric for how
         # well the expected time series and the residual match
-        metric = cosine_similarity(
+        similarity = cosine_similarity(
             expected_time_series.reshape(1, -1),
             residual.reshape(1, -1),
         )
 
-        # Clip the metric to [0, 1] (a signal time that leads to a negative
-        # cosine similarity should never become a hypothesis)
-        metric = float(np.clip(metric, a_min=0, a_max=1))
+        # Clip the similarity to [0, 1] (a signal time that leads to a
+        # negative cosine similarity should never become a hypothesis)
+        similarity = float(np.clip(similarity, a_min=0, a_max=1))
 
-        # Store the current metric if it is better than the optimum so far
-        if metric > best_metric:
-            best_metric = metric
+        # Store the current similarity if it is better than the optimum so far
+        if similarity > best_similarity:
+            best_similarity = similarity
             best_signal_time = signal_time
 
     # Return type must be float, because it can also be NaN
-    return float(best_signal_time)
+    if best_similarity > minimum_similarity:
+        return float(best_signal_time), best_similarity
+    return np.nan, np.nan
