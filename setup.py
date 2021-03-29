b61bee39a92f8a2ceeb775d277745c0e881f7e93
Timothy Gebhard <mail@timothygebhard.de>
2021-03-29 17:19:52 +0200

    Move requirements for development from requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,16 @@ setup(
         'seaborn',
         'tqdm',
     ],
-    packages=['hsr4hci'],
+    extras_require={
+        'develop': [
+            'coverage>=5.3',
+            'deepdiff>=5.0.2',
+            'flake8>=3.8.3',
+            'mypy>=0.782',
+            'pytest>=6.0.2',
+            'pytest-cov>=2.10.1',
+        ]
+    },
+    packages=find_packages(),
     zip_safe=False,
 )
