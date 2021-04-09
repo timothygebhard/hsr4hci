ffdb480459df31886ab3b71dc8f96cf4df2908c6
Timothy Gebhard <mail@timothygebhard.de>
2021-04-09 21:45:08 +0200

    Refactor hypotheses.py to allow computing hypotheses in parallel

--- hsr4hci/hypotheses.py
+++ hsr4hci/hypotheses.py
@@ -7,11 +7,13 @@ seems to contain a planet signal at time T.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from typing import Dict, Tuple, Union
+from pathlib import Path
+from typing import Tuple, Union
 
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm.auto import tqdm
 
+import h5py
 import numpy as np
 
 from hsr4hci.forward_modeling import get_time_series_for_position
@@ -25,43 +27,76 @@ from hsr4hci.signal_masking import get_signal_times
 
 def get_all_hypotheses(
     roi_mask: np.ndarray,
-    results: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]],
+    dict_or_path: Union[dict, Union[str, Path]],
     parang: np.ndarray,
     n_signal_times: int,
     frame_size: Tuple[int, int],
     psf_template: np.ndarray,
     minimum_similarity: float = 0.0,
+    n_roi_splits: int = 1,
+    roi_split: int = 0,
 ) -> Tuple[np.ndarray, np.ndarray]:
     """
     This is a convenience function which wraps the loop over the ROI
-    to call `find_hypothesis()` for every spatial pixel. See there for
-    a full documentation of all parameters.
+    to call `find_hypothesis_from_dict_like()` for every spatial pixel.
+    See there for a full documentation of all parameters.
     """
 
     # Initialize array for hypotheses and similarities
     hypotheses = np.full(frame_size, np.nan)
     similarities = np.full(frame_size, np.nan)
 
-    # Loop over all spatial positions and find the respective hypothesis
-    for position in tqdm(get_positions_from_mask(roi_mask), ncols=80):
+    # Define positions for which to run (= subset of the ROI)
+    positions = get_positions_from_mask(roi_mask)[roi_split::n_roi_splits]
 
-        signal_time, similarity = find_hypothesis(
-            results=results,
-            position=position,
-            parang=parang,
-            n_signal_times=n_signal_times,
-            frame_size=frame_size,
-            psf_template=psf_template,
-            minimum_similarity=minimum_similarity,
-        )
-        hypotheses[position[0], position[1]] = signal_time
-        similarities[position[0], position[1]] = similarity
+    # We can either work with a dict that holds all results...
+    if isinstance(dict_or_path, dict):
+
+        # Loop over all spatial positions, find the hypothesis and
+        # similarity for the current position, and store them
+        for position in tqdm(positions, ncols=80):
+            signal_time, similarity = find_hypothesis_from_dict_like(
+                results=dict_or_path,
+                position=position,
+                parang=parang,
+                n_signal_times=n_signal_times,
+                frame_size=frame_size,
+                psf_template=psf_template,
+                minimum_similarity=minimum_similarity,
+            )
+            hypotheses[position[0], position[1]] = signal_time
+            similarities[position[0], position[1]] = similarity
+
+    # ...or with the path to an HDF file that holds all results.
+    elif isinstance(dict_or_path, (str, Path)):
+
+        # Open the HDF file
+        with h5py.File(dict_or_path, 'r') as results:
+
+            # Loop over all spatial positions, find the hypothesis and
+            # similarity for the current position, and store them
+            for position in tqdm(positions, ncols=80):
+                signal_time, similarity = find_hypothesis_from_dict_like(
+                    results=results,
+                    position=position,
+                    parang=parang,
+                    n_signal_times=n_signal_times,
+                    frame_size=frame_size,
+                    psf_template=psf_template,
+                    minimum_similarity=minimum_similarity,
+                )
+                hypotheses[position[0], position[1]] = signal_time
+                similarities[position[0], position[1]] = similarity
+
+    # In all other cases, we raise an error
+    else:
+        raise ValueError('dict_or_path must either be a dict or path!')
 
     return hypotheses, similarities
 
 
-def find_hypothesis(
-    results: Dict[str, Union[np.ndarray, Dict[str, np.ndarray]]],
+def find_hypothesis_from_dict_like(
+    results: Union[h5py.File, dict],
     position: Tuple[int, int],
     parang: np.ndarray,
     n_signal_times: int,
@@ -70,20 +105,21 @@ def find_hypothesis(
     minimum_similarity: float = 0.0,
 ) -> Tuple[float, float]:
     """
-    Take the dictionary containing the full training results and find,
-    for a given spatial `position`, the time T at which we suspect there
-    to be a planet at the position. If these is no time at which the
-    residuals look like the contain a planet, this function returns NaN.
+    Take a dictionary-like object (either an actual dictionary, or an
+    open `h5py.File` object) containing the full training results and
+    find, for a given spatial `position`, the time T at which we suspect
+    there to be a planet at the position. If these is no time at which
+    the residuals look like the contain a planet, return NaN.
 
     Args:
-        results: A dictionary containing the full training results. In
-            particular, we expect that for each signal time T on the
-            temporal grid that was used during training, there is a key
-            T (as a string) that maps to another dictionary which has a
-            key `residuals`, which is a 3D numpy array consisting of the
-            residuals that were obtained when trained the signal masking
-            models under the hypothesis that there is a planet at T (for
-            each spatial position).
+        results: A dictionary-like object containing the full training
+            results. In particular, we expect that for each signal time
+            T on the temporal grid that was used during training, there
+            is a key T (as a string) that maps to another dictionary
+            which has a key `residuals`, which is a 3D numpy array
+            consisting of the residuals that were obtained when trained
+            the signal masking models under the hypothesis that there
+            is a planet at T (for each spatial position).
         position: A tuple `(x, y)` specifying the position for which we
             want to find the best hypothesis (i.e., the best guess for
             the time at which this pixel contains a planet).
@@ -118,9 +154,10 @@ def find_hypothesis(
         n_frames=len(parang), n_signal_times=n_signal_times
     ):
 
-        # Select residual for this position
+        # Select residual for this position. Note: `results` is either a
+        # numpy array or an open h5py.File object!
         signal_time_residuals = results[str(signal_time)]['residuals']
-        residual = signal_time_residuals[:, position[0], position[1]]
+        residual = np.array(signal_time_residuals[:, position[0], position[1]])
 
         # If the residual is NaN, we can't compute the metric function
         if np.isnan(residual).any():
