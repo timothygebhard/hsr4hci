3b55519f89a1809fef1dde54052014790c53cf9d
Timothy Gebhard <mail@timothygebhard.de>
2021-04-22 17:39:31 +0200

    Add scripts to run HSR based on a hypothesis

---
+++ scripts/experiments/hypothesis-based/00_make_submit_files.py
@@ -0,0 +1,175 @@
+"""
+Create submit files to run experiment on an HTCondor-based cluster.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from pathlib import Path
+
+import argparse
+import time
+import os
+
+from hsr4hci.config import get_hsr4hci_dir
+from hsr4hci.htcondor import SubmitFile, DAGFile
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # -------------------------------------------------------------------------
+    # Preliminaries
+    # -------------------------------------------------------------------------
+
+    script_start = time.time()
+    print('\nMAKE SUBMIT FILES\n', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Set up parser to get command line arguments
+    # -------------------------------------------------------------------------
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--experiment-dir',
+        type=str,
+        required=True,
+        metavar='PATH',
+        help='Path to experiment directory.',
+    )
+    parser.add_argument(
+        '--cpus',
+        type=int,
+        default=4,
+        help='Number of CPUs that the job should ask for.',
+    )
+    parser.add_argument(
+        '--memory',
+        type=int,
+        default=8192,
+        help='Memory (in MB) that the job should ask for.',
+    )
+    parser.add_argument(
+        '--bid',
+        type=int,
+        default=5,
+        help='Amount of cluster dollars to bid for each job.',
+    )
+    args = parser.parse_args()
+
+    # -------------------------------------------------------------------------
+    # Load config and data, get command line variables and define shortcuts
+    # -------------------------------------------------------------------------
+
+    # Get experiment directory
+    experiment_dir = Path(os.path.expanduser(args.experiment_dir))
+    if not experiment_dir.exists():
+        raise NotADirectoryError(f'{experiment_dir} does not exist!')
+
+    # Define a couple of path variables
+    htcondor_dir = experiment_dir / 'htcondor'
+    clusterlogs_dir = htcondor_dir / 'clusterlogs'
+
+    # Define shortcuts to remaining command line arguments
+    bid = args.bid
+    cpus = args.cpus
+    memory = args.memory
+
+    # Define scripts directory
+    scripts_dir = (
+        get_hsr4hci_dir() / 'scripts' / 'experiments' / 'hypothesis-based'
+    )
+
+    # -------------------------------------------------------------------------
+    # Instantiate a new DAG file
+    # -------------------------------------------------------------------------
+
+    print('Instantiating new DAG file...', end=' ', flush=True)
+    dag_file = DAGFile()
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Create a submit file for running the PCA
+    # -------------------------------------------------------------------------
+
+    name = '01_train_based_on_hypothesis'
+
+    # Create submit file and add job
+    print(f'Creating {name}.sub...', end=' ', flush=True)
+    submit_file = SubmitFile(
+        clusterlogs_dir=clusterlogs_dir.as_posix(),
+        memory=memory,
+        cpus=cpus,
+    )
+    submit_file.add_job(
+        name=name,
+        job_script=(scripts_dir / f'{name}.py').as_posix(),
+        arguments={
+            'experiment-dir': experiment_dir.as_posix(),
+        },
+        bid=bid,
+        queue=1,
+    )
+    file_path = htcondor_dir / f'{name}.sub'
+    submit_file.save(file_path=file_path.as_posix())
+    print('Done!', flush=True)
+
+    # Add submit file to DAG
+    print(f'Adding {name}.sub to DAG file...', end=' ', flush=True)
+    dag_file.add_submit_file(
+        name=name,
+        attributes=dict(file_path=file_path.as_posix(), bid=bid),
+    )
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Create a submit file for creating a plot of the signal estimate
+    # -------------------------------------------------------------------------
+
+    name = '02_get_contrast_and_snr'
+
+    print(f'Creating {name}.sub...', end=' ', flush=True)
+    submit_file = SubmitFile(
+        clusterlogs_dir=clusterlogs_dir.as_posix(),
+        memory=1024,
+        cpus=1,
+    )
+    submit_file.add_job(
+        name=name,
+        job_script=(scripts_dir / f'{name}.py').as_posix(),
+        arguments={
+            'experiment-dir': experiment_dir.as_posix(),
+        },
+        bid=bid,
+    )
+    file_path = htcondor_dir / f'{name}.sub'
+    submit_file.save(file_path=file_path)
+    print('Done!', flush=True)
+
+    # Add submit file to DAG
+    print(f'Adding {name}.sub to DAG file...', end=' ', flush=True)
+    dag_file.add_submit_file(
+        name=name,
+        attributes=dict(file_path=file_path.as_posix(), bid=bid),
+    )
+    dag_file.add_dependency('01_train_based_on_hypothesis', name)
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Save the DAG file
+    # -------------------------------------------------------------------------
+
+    print('Saving DAG file...', end=' ', flush=True)
+    file_path = htcondor_dir / 'run_experiment.dag'
+    dag_file.save(file_path=file_path)
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Postliminaries
+    # -------------------------------------------------------------------------
+
+    print(f'\nThis took {time.time() - script_start:.1f} seconds!\n')
---
+++ scripts/experiments/hypothesis-based/01_train_based_on_hypothesis.py
@@ -0,0 +1,293 @@
+"""
+Run a hypothesis-based version of half-sibling regression.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from pathlib import Path
+
+import argparse
+import os
+import time
+
+from astropy.units import Quantity
+from sklearn.metrics.pairwise import cosine_similarity
+from tqdm.auto import tqdm
+
+import numpy as np
+
+from hsr4hci.base_models import BaseModelCreator
+from hsr4hci.config import load_config
+from hsr4hci.data import load_dataset
+from hsr4hci.derotating import derotate_combine
+from hsr4hci.fits import save_fits
+from hsr4hci.forward_modeling import add_fake_planet
+from hsr4hci.masking import get_roi_mask, get_positions_from_mask
+from hsr4hci.splitting import AlternatingSplit
+from hsr4hci.training import train_model
+from hsr4hci.units import set_units_for_instrument
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # -------------------------------------------------------------------------
+    # Preliminaries
+    # -------------------------------------------------------------------------
+
+    script_start = time.time()
+    print('\nTRAIN HYPOTHESIS-BASED HALF-SIBLING REGRESSION\n', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Load experiment configuration and data; parse command line arguments
+    # -------------------------------------------------------------------------
+
+    # Set up parser for command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--experiment-dir',
+        type=str,
+        required=True,
+        metavar='PATH',
+        help='(Absolute) path to experiment directory.',
+    )
+    args = parser.parse_args()
+
+    # Get experiment directory
+    experiment_dir = Path(os.path.expanduser(args.experiment_dir))
+    if not experiment_dir.exists():
+        raise NotADirectoryError(f'{experiment_dir} does not exist!')
+
+    # Load experiment config from JSON
+    print('Loading experiment configuration...', end=' ', flush=True)
+    config = load_config(experiment_dir / 'config.json')
+    if not 'hypothesis' in config.keys():
+        raise RuntimeError('Experiment configuration contains no hypothesis!')
+    print('Done!', flush=True)
+
+    # Load frames, parallactic angles, etc. from HDF file
+    print('Loading data set...', end=' ', flush=True)
+    stack, parang, psf_template, observing_conditions, metadata = load_dataset(
+        **config['dataset']
+    )
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Define shortcuts; activate unit conversions
+    # -------------------------------------------------------------------------
+
+    # Quantities related to the size of the data set
+    n_frames, x_size, y_size = stack.shape
+    frame_size = (x_size, y_size)
+
+    # Other shortcuts
+    selected_keys = config['observing_conditions']['selected_keys']
+    selection_mask_config = config['selection_mask']
+
+    # Activate the unit conversions for this instrument
+    set_units_for_instrument(
+        pixscale=Quantity(metadata['PIXSCALE'], 'arcsec / pixel'),
+        lambda_over_d=Quantity(metadata['LAMBDA_OVER_D'], 'arcsec'),
+        verbose=False,
+    )
+
+    # -------------------------------------------------------------------------
+    # STEP 1: Compute expected stack based on hypothesis
+    # -------------------------------------------------------------------------
+
+    print('Computing hypothesized stack...', end=' ', flush=True)
+
+    # Make sure the PSF template is correctly normalized
+    psf_template -= np.min(psf_template)
+    psf_template /= np.max(psf_template)
+
+    # Initialize the hypothesized stack and mask of affected pixels
+    hypothesized_stack = np.zeros_like(stack)
+    hypotheses = np.zeros(frame_size)
+    affected_mask = np.full(frame_size, False)
+
+    # Loop over potentially multiple planet hypotheses and add them
+    for name, parameters in config['hypothesis'].items():
+
+        # Define hypothesized planet position
+        hypothesized_position = (
+            Quantity(*parameters['separation']),
+            Quantity(*parameters['position_angle']),
+        )
+
+        # Compute the corresponding forward model and normalize it
+        hypothesized_stack = np.array(
+            add_fake_planet(
+                stack=hypothesized_stack,
+                parang=parang,
+                psf_template=psf_template,
+                polar_position=hypothesized_position,
+                magnitude=0,
+                extra_scaling=1,
+                dit_stack=1,
+                dit_psf_template=1,
+                return_planet_positions=False,
+                interpolation='bilinear',
+            )
+        )
+        hypothesized_stack /= np.max(hypothesized_stack)
+
+        # Update the binary mask that we use to find pixels that contain a
+        # "reasonable" amount of planet signal (the threshold is somewhat
+        # arbitrary, of course)
+        affected_mask = np.logical_or(
+            affected_mask, (np.max(hypothesized_stack, axis=0) > 0.2)
+        )
+
+        # Find the hypotheses for the current stack, that is, the time at
+        # which the signal peaks in each pixel
+        hypotheses = np.maximum(
+            hypotheses, np.argmax(hypothesized_stack, axis=0)
+        )
+
+    # Only keep the hypothesis for pixels with enough planet signal
+    hypotheses[~affected_mask] = np.nan
+    print('Done!', flush=True)
+
+    # Ensure that the results directory exists
+    results_dir = experiment_dir / 'results'
+    results_dir.mkdir(exist_ok=True)
+
+    # Save hypotheses to HDF
+    print('Saving hypotheses...', end=' ', flush=True)
+    file_path = results_dir / 'hypotheses.fits'
+    save_fits(hypotheses, file_path=file_path)
+    file_path = results_dir / 'hypothesis_stack.fits'
+    save_fits(hypothesized_stack, file_path=file_path)
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # STEP 2: Train HSR models
+    # -------------------------------------------------------------------------
+
+    # Set up a BaseModelCreator to create instances of our base model
+    base_model_creator = BaseModelCreator(**config['base_model'])
+
+    # Construct the mask for the region of interest (ROI)
+    roi_mask = get_roi_mask(
+        mask_size=frame_size,
+        inner_radius=Quantity(*config['roi_mask']['inner_radius']),
+        outer_radius=Quantity(*config['roi_mask']['outer_radius']),
+    )
+
+    # Create splitter for indices
+    n_splits = int(config['n_splits'])
+    signal_masking_splitter = AlternatingSplit(n_splits=n_splits)
+
+    # Initialize full residuals
+    full_residuals = np.full(stack.shape, np.nan)
+    matches = np.full(frame_size, np.nan)
+    fractions = np.full(frame_size, np.nan)
+    full_alphas = np.full((n_splits,) + frame_size, np.nan)
+    full_pixel_coefs = np.full(frame_size + (n_splits,) + frame_size, np.nan)
+    full_planet_coefs = np.full((n_splits,) + frame_size, np.nan)
+
+    # Loop over ROI, train models, and compute residuals
+    # We use this manual loop here because we also want to access and store all
+    # the "debugging" information, such as the coefficients, alphas, ...
+    print('\nTraining HSR models:', flush=True)
+    for (x, y) in tqdm(get_positions_from_mask(roi_mask), ncols=80):
+
+        if np.isnan(hypotheses[x, y]):
+            mode = None
+            signal_time = None
+            expected_signal = None
+        else:
+            mode = config['mode']
+            signal_time = int(hypotheses[x, y])
+            expected_signal = hypothesized_stack[:, x, y]
+
+        # Train the model for this pixel
+        residuals, model_params = train_model(
+            stack=stack,
+            parang=parang,
+            obscon_array=observing_conditions.as_array(selected_keys),
+            position=(x, y),
+            mode=mode,
+            signal_time=signal_time,
+            expected_signal=expected_signal,
+            selection_mask_config=selection_mask_config,
+            psf_template=psf_template,
+            n_splits=n_splits,
+            base_model_creator=base_model_creator,
+            signal_masking_threshold=0.1,
+        )
+
+        # Unpack model_params
+        alphas = model_params['alphas']
+        selection_mask = model_params['selection_mask']
+        pixel_coefs = model_params['pixel_coefs']
+        planet_coefs = model_params['planet_coefs']
+
+        # Store the residuals, alphas, and coefficients
+        full_residuals[:, x, y] = residuals
+        full_alphas[:, x, y] = alphas
+        full_planet_coefs[:, x, y] = planet_coefs
+        for i in range(n_splits):
+            full_pixel_coefs[x, y, i][selection_mask] = pixel_coefs[i]
+
+        # Compute the match between the residual and the hypothesized stack
+        a = np.nan_to_num(hypothesized_stack[:, x, y])
+        b = np.nan_to_num(full_residuals[:, x, y])
+        matches[x, y] = cosine_similarity(a.reshape(1, -1), b.reshape(1, -1))
+
+    print('\nSaving residuals to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'residuals.fits'
+    save_fits(full_residuals, file_path)
+    print('Done!', flush=True)
+
+    print('Saving fractions to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'fractions.fits'
+    save_fits(fractions, file_path)
+    print('Done!', flush=True)
+
+    print('Saving alphas to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'alphas.fits'
+    save_fits(full_alphas, file_path)
+    print('Done!', flush=True)
+
+    print('Saving selection masks to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'pixel_coefficients.fits'
+    save_fits(full_pixel_coefs, file_path)
+    print('Done!', flush=True)
+
+    print('Saving planet coefs to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'planet_coefficients.fits'
+    save_fits(full_planet_coefs, file_path)
+    print('Done!', flush=True)
+
+    print('Saving matches to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'matches.fits'
+    save_fits(matches, file_path)
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # STEP 3: Compute and save signal estimate
+    # -------------------------------------------------------------------------
+
+    print('\nComputing signal estimate...', end=' ', flush=True)
+    signal_estimate = derotate_combine(
+        stack=full_residuals, parang=parang, mask=~roi_mask, combine='median'
+    )
+    print('Done!', flush=True)
+
+    print('\nSaving signal estimate to FITS...', end=' ', flush=True)
+    file_path = results_dir / 'signal_estimate.fits'
+    save_fits(signal_estimate, file_path)
+    print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Postliminaries
+    # -------------------------------------------------------------------------
+
+    print(f'\nThis took {time.time() - script_start:.1f} seconds!\n')
