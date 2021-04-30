2a6554e2af8b7415d4921b95c84a3e4c5da622ff
Timothy Gebhard <mail@timothygebhard.de>
2021-04-30 20:24:02 +0200

    Add unit tests for config.py

---
+++ tests/test_config.py
@@ -0,0 +1,61 @@
+"""
+Tests for config.py
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+import json
+
+from _pytest.tmpdir import TempPathFactory
+from deepdiff import DeepDiff
+
+import pytest
+
+from hsr4hci.config import load_config, get_hsr4hci_dir, get_datasets_dir
+
+
+# -----------------------------------------------------------------------------
+# TEST CASES
+# -----------------------------------------------------------------------------
+
+def test_load_config(tmp_path_factory: TempPathFactory) -> None:
+
+    # Define location of test config file in temporary directory
+    test_dir = tmp_path_factory.mktemp('config', numbered=False)
+    file_path = test_dir / 'test_config.json'
+
+    # Create dummy test configuration
+    test_config = {'a': 'a', 'b': 2, 'c': [1.0, 2.0, 3.0], 'd': {'x': 'y'}}
+
+    # Create test config file
+    with open(file_path, 'w') as json_file:
+        json.dump(test_config, json_file, indent=2)
+
+    # Test case 1
+    config = load_config(file_path)
+    deepdiff = DeepDiff(t1=config, t2=test_config)
+    assert not deepdiff
+
+    # Test case 2
+    file_path = test_dir / 'this_file_should_not_exist'
+    with pytest.raises(FileNotFoundError) as error:
+        load_config(file_path)
+    assert 'does not exist' in str(error)
+
+    # Test case 3
+    file_path_str = (test_dir / 'test_config.json').as_posix()
+    config = load_config(file_path_str)
+    deepdiff = DeepDiff(t1=config, t2=test_config)
+    assert not deepdiff
+
+
+def test__get_hsr4hci_dir() -> None:
+
+    assert get_hsr4hci_dir().exists()
+
+
+def test__get_datasets_dir() -> None:
+
+    assert get_datasets_dir().exists()
