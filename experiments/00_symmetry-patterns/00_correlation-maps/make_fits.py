a57eacc3075ee289ea44c488ccb3beffa1c04338
Timothy Gebhard <mail@timothygebhard.de>
2021-05-05 12:55:18 +0200

    Add experiment to create correlation map plots

---
+++ experiments/00_symmetry-patterns/00_correlation-maps/make_fits.py
@@ -0,0 +1,98 @@
+"""
+Compute the correlation coefficients between every position (x, y)
+with every other position (x', y').
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from pathlib import Path
+
+import numpy as np
+import time
+
+from hsr4hci.data import load_dataset
+from hsr4hci.fits import save_fits
+
+
+# -----------------------------------------------------------------------------
+# FUNCTION DEFINITIONS
+# -----------------------------------------------------------------------------
+
+def correlation_map(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+
+    epsilon = np.finfo(float).eps
+
+    mu_x = x.mean(axis=1)
+    mu_y = y.mean(axis=1)
+    n = x.shape[1]
+
+    s_x = x.std(1, ddof=n - 1)
+    s_y = y.std(1, ddof=n - 1)
+
+    cov = np.dot(x, y.T) - n * np.dot(mu_x[:, np.newaxis], mu_y[np.newaxis, :])
+
+    correlation_map = cov / (
+        np.dot(s_x[:, np.newaxis], s_y[np.newaxis, :]) + epsilon
+    )
+
+    correlation_map = np.clip(correlation_map, -1, 1)
+
+    return np.asarray(correlation_map)
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # -------------------------------------------------------------------------
+    # Preliminaries
+    # -------------------------------------------------------------------------
+
+    script_start = time.time()
+    print('\nCOMPUTE CORRELATION COEFFICIENT MAPS\n')
+
+    # -------------------------------------------------------------------------
+    # Compute full 4D correlation map for two example data sets
+    # -------------------------------------------------------------------------
+
+    # Ensure that the directory for the FITS files exists
+    fits_dir = Path('fits')
+    fits_dir.mkdir(exist_ok=True)
+
+    # Loop over different data sets
+    for dataset in ('beta_pictoris__mp', 'hr_8799__lp'):
+
+        start_time = time.time()
+        print(f'Running for {dataset}...', end=' ', flush=True)
+
+        # Load data set (and crop to some reasonable size)
+        stack, parang, psf_template, obs_con, metadata = load_dataset(
+            name=dataset,
+            frame_size=(51, 51),
+        )
+        n_frames, x_size, y_size = stack.shape
+
+        # Reshape and flatten the stack for use with correlation_map()
+        flat_stack = np.transpose(stack, (1, 2, 0)).reshape(-1, n_frames)
+
+        # Compute the correlations (along the temporal axis) between every
+        # position (x, y) and every other position (x', y'); the result is
+        # a 4D array of shape (x_size, y_size, x_size, y_size).
+        correlations = correlation_map(flat_stack, flat_stack)
+        correlations = correlations.reshape((x_size, y_size, x_size, y_size))
+
+        # Save correlations to FITS file
+        file_path = fits_dir / f'{dataset}.fits'
+        save_fits(array=correlations, file_path=file_path)
+
+        print(f'Done! ({time.time() - start_time:.1f} seconds)', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Postliminaries
+    # -------------------------------------------------------------------------
+
+    print(f'\nThis took {time.time() - script_start:.1f} seconds!\n')
---
+++ experiments/00_symmetry-patterns/00_correlation-maps/make_plots.py
@@ -0,0 +1,124 @@
+"""
+Apply clustering to time series and plot the results.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from pathlib import Path
+
+import time
+
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+import numpy as np
+
+from hsr4hci.coordinates import get_center
+from hsr4hci.fits import read_fits
+from hsr4hci.plotting import disable_ticks, add_colorbar_to_ax
+
+
+# -----------------------------------------------------------------------------
+# MAIN CODE
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # -------------------------------------------------------------------------
+    # Preliminaries
+    # -------------------------------------------------------------------------
+
+    script_start = time.time()
+    print('\nPLOT CORRELATION MAPS\n', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Run k-means clustering for two example data sets
+    # -------------------------------------------------------------------------
+
+    # Ensure that FITS directory exists
+    fits_dir = Path('fits')
+    if not fits_dir.exists():
+        raise NotADirectoryError('FITS directory not found!')
+
+    # Ensure the plots directory exists
+    plots_dir = Path('plots')
+    plots_dir.mkdir(exist_ok=True)
+
+    # Loop over different data sets
+    for dataset, (x, y) in (
+        ('beta_pictoris__mp', (30 - 1, 26 - 1)),
+        ('hr_8799__lp', (26 - 1, 13 - 1)),
+    ):
+
+        start_time = time.time()
+        print(f'Running for {dataset}...', end=' ', flush=True)
+
+        # Load correlation coefficients from FITS
+        file_path = fits_dir / f'{dataset}.fits'
+        correlations = np.asarray(read_fits(file_path=file_path))
+
+        # Select CC map that we want to plot (flip coordinates because numpy!)
+        cc_map = correlations[y, x]
+
+        # Define shortcuts
+        x_size, y_size = cc_map.shape
+        frame_size = (x_size, y_size)
+        center = get_center(frame_size)
+
+        # Plot the correlation map
+        fig, ax = plt.subplots(figsize=(5, 6))
+        img = ax.pcolormesh(
+            np.arange(x_size),
+            np.arange(y_size),
+            cc_map,
+            vmin=-0.6,
+            vmax=0.6,
+            shading='nearest',
+            cmap='RdBu_r',
+            snap=True,
+            rasterized=True,
+        )
+        disable_ticks(ax)
+        ax.set_aspect('equal')
+
+        # Plot markers for position and center
+        plt.plot(x, y, 'x', mew=2, ms=8, color='lime')
+        plt.plot(center[0], center[1], '+', color='black')
+
+        # Create the scale bar and add it to the frame
+        scalebar = AnchoredSizeBar(
+            transform=ax.transData,
+            size=0.5 / 0.0271,
+            label='0.5"',
+            loc=2,
+            pad=1,
+            color='black',
+            frameon=False,
+            size_vertical=0,
+            fontproperties=fm.FontProperties(size=12),
+        )
+        ax.add_artist(scalebar)
+
+        # Add a color bar
+        cbar = add_colorbar_to_ax(img, fig, ax, where='bottom')
+        cbar.set_ticks([-0.5, -0.25, 0, 0.25, 0.5])
+        cbar.ax.set_xlabel(
+            r'Correlation with target pixel (in green)', fontsize=12
+        )
+        cbar.ax.xaxis.set_label_position('bottom')
+
+        # Save plot as a PDF
+        fig.tight_layout()
+        file_path = plots_dir / f'{dataset}.pdf'
+        plt.savefig(file_path, bbox_inches='tight', pad_inches=0.025)
+
+        print(f'Done! ({time.time() - start_time:.1f} seconds)', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Postliminaries
+    # -------------------------------------------------------------------------
+
+    print(f'\nThis took {time.time() - script_start:.1f} seconds!\n')
