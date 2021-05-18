736f051611f719d3c635ce248bc2099a95310fbe
Timothy Gebhard <mail@timothygebhard.de>
2021-05-18 18:08:13 +0200

    Remove outdated functions; add overload decorator to read_fits()

--- hsr4hci/fits.py
+++ hsr4hci/fits.py
@@ -6,9 +6,9 @@ Utility functions for reading and writing FITS files.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from datetime import datetime, timezone
 from pathlib import Path
-from typing import Any, Optional, Tuple, Type, Union
+from typing import Optional, Tuple, Union, overload
+from typing_extensions import Literal
 
 import json
 
@@ -21,27 +21,22 @@ import numpy as np
 # FUNCTION DEFINITIONS
 # -----------------------------------------------------------------------------
 
-def is_fits_file(file_path: Union[Path, str]) -> bool:
-    """
-    Check the ending of a file_path to determine if it is a FITS file.
+@overload
+def read_fits(
+    file_path: Union[Path, str], return_header: Literal[True]
+) -> Tuple[np.ndarray, dict]:
+    ...  # pragma: no cover
 
-    Note: checking for magic numbers to determine the file type does
-        not work in general, because ESO FITS files seem to not always
-        have the correct MIME type.
 
-    Args:
-        file_path: Path to the target file to be checked.
-
-    Returns:
-        True if the given file is a FITS file; False otherwise.
-    """
-
-    return Path(file_path).name.endswith('.fits')
+@overload
+def read_fits(
+    file_path: Union[Path, str], return_header: Literal[False]
+) -> np.ndarray:
+    ...  # pragma: no cover
 
 
 def read_fits(
-    file_path: Union[Path, str],
-    return_header: bool = False,
+    file_path: Union[Path, str], return_header: bool = False
 ) -> Union[np.ndarray, Tuple[np.ndarray, dict]]:
     """
     Open a FITS file and return its contents as a numpy array.
@@ -112,151 +107,3 @@ def save_fits(
 
     # Save the HDU to the specified FITS file
     fits.HDUList([hdu]).writeto(file_path.as_posix(), overwrite=overwrite)
-
-
-def get_fits_header(file_path: Union[Path, str]) -> dict:
-    """
-    Open a FITS file and return only its header as a dictionary.
-
-    Args:
-        file_path: Path of the FITS file to be read in.
-
-    Returns:
-        A dictionary containing the header of the FITS file.
-    """
-
-    # Make sure that file_path is a proper Path
-    file_path = Path(file_path)
-
-    # Open the FITS file and read the contents as well as the header
-    with fits.open(file_path.as_posix()) as hdulist:
-        header = dict(hdulist[0].header)
-
-    return header
-
-
-def get_fits_header_value(
-    file_path: Union[Path, str],
-    key: str,
-    dtype: Optional[Type] = None,
-) -> Any:
-    """
-    Get the value of a field in the header of a given FITS file.
-
-    Args:
-        file_path: Path to the FITS file to read the header from.
-        key: Key of the field in the header of the FITS file.
-        dtype: Target data type of the header value. If this value is
-            not None, we try to convert the value that was read from
-            the FITS file to the given data type.
-
-    Returns:
-        The value of the header field in the given FITS file.
-    """
-
-    # Make sure that file_path is a proper Path
-    file_path = Path(file_path)
-
-    # Open the FITS file and read the target header value
-    with fits.open(file_path.as_posix()) as hdu_list:
-        value = hdu_list[0].header[key]
-
-    # If desired, attempt to convert the value to the given data type
-    if dtype is not None and isinstance(dtype, type):
-
-        # Datetime objects in ISO 8601 format require special treatment.
-        # Furthermore, we assume that all datetime values will be given in UTC.
-        if dtype == datetime:
-            value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-            value = value.replace(tzinfo=timezone.utc)
-
-        # Other values can be cast directly
-        else:
-            value = dtype(value)
-
-    return value
-
-
-def get_fits_header_value_array(
-    file_path: Union[Path, str],
-    start_key: str,
-    end_key: str,
-) -> np.ndarray:
-    """
-    For some fields, the ESO/VLT FITS files define a start and an end
-    value (e.g., the seeing). This function will return a numpy array
-    that has as many entries as there are frames in the FITS file, and
-    that contains a linear interpolation between the start and the end
-    value of the given keys.
-
-    If the start and end key are identical, the resulting array will
-    contain the same value everywhere.
-
-    Args:
-        file_path: Path to the FITS file to read the header from.
-        start_key: The start key of the value we want to retrieve.
-        end_key: The end key of the value we want to retrieve.
-
-    Returns:
-        A numpy array (that has as many entries as there are frames
-        along NAXIS3 in the FITS file) containing a linear interpolation
-        of the values corresponding to the given start and end keys.
-    """
-
-    # Make sure that file_path is a proper Path
-    file_path = Path(file_path)
-
-    # Get the number of frames in the current cube
-    n_frames: int = get_fits_header_value(file_path, 'NAXIS3', int)
-
-    # Read the start and end value from the FITS file
-    start_value: float = get_fits_header_value(file_path, start_key, float)
-    end_value: float = get_fits_header_value(file_path, end_key, float)
-
-    # Create an array with a linear interpolation between those two values
-    array = np.linspace(start_value, end_value, n_frames)
-
-    return array
-
-
-def header_value_exists(
-    file_path: Union[Path, str],
-    key: str,
-) -> bool:
-    """
-    Check if the header of a given FITS file contains a given key.
-
-    Args:
-        file_path: Path to the FITS file to check.
-        key: Name of the header field whose existence to check for.
-
-    Returns:
-        True if the header of the given FITS file contains the given
-        key, and False otherwise.
-    """
-
-    # Make sure that file_path is a proper Path
-    file_path = Path(file_path)
-
-    # Open the FITS file and check if the requested key is present.
-    # The following approach based on try/except is necessary because of a
-    # strange convention regarding the role of the "HIERARCH" part of the
-    # name of many ESO FITS files, which is best illustrated as follows:
-    #
-    #   >>> header["HIERARCH ESO TEL AMBI TAU0"]
-    #   0.0053
-    #   >>> header["ESO TEL AMBI TAU0"]
-    #   0.0053
-    #   >>> "HIERARCH ESO TEL AMBI TAU0" in header.keys()
-    #   False
-    #   >>> "ESO TEL AMBI TAU0" in header.keys()
-    #   True
-    #
-    # Just because a key is not in `header.keys()` does therefore *not* mean
-    # that we cannot access that key -- hence the try/except construct.
-    with fits.open(file_path.as_posix()) as hdu_list:
-        try:
-            _ = hdu_list[0].header[key]
-            return True
-        except KeyError:
-            return False
