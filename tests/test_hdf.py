de55c2d90daa7bfba0f98d695ff1ff79bd7434db
Timothy Gebhard <mail@timothygebhard.de>
2021-05-19 20:28:31 +0200

    Update unit tests

--- tests/test_hdf.py
+++ tests/test_hdf.py
@@ -6,52 +6,124 @@ Tests for hdf.py
 # IMPORTS
 # -----------------------------------------------------------------------------
 
+from pathlib import Path
+
 from _pytest.tmpdir import TempPathFactory
 from deepdiff import DeepDiff
 
+import h5py
 import numpy as np
+import pytest
 
-from hsr4hci.hdf import save_dict_to_hdf, load_dict_from_hdf
-
-
-# -----------------------------------------------------------------------------
-# CONSTANT DEFINITIONS
-# -----------------------------------------------------------------------------
-
-# Define test dictionary to be saved to and loaded from HDF
-TEST_DICT = {
-    'bool': False,
-    'complex': 1 + 2j,
-    'float': 3.0,
-    'int': 2,
-    'array': np.array([0.0, 1.0]),
-    'dict': {'a': 'string', 'b': np.array([1, 2, 3, 4, 5])},
-    'double_dict': {
-        'dict_1': {'1': 'a', '2': 123},
-        'dict_2': {'nan': np.nan, 'empty': np.empty((42, 23))},
-    },
-}
+from hsr4hci.hdf import save_data_to_hdf, save_dict_to_hdf, load_dict_from_hdf
 
 
 # -----------------------------------------------------------------------------
 # TEST CASES
 # -----------------------------------------------------------------------------
 
-def test__save_dict_to_hdf(tmp_path_factory: TempPathFactory) -> None:
+@pytest.fixture(scope="session")
+def hdf_dir(tmp_path_factory: TempPathFactory) -> Path:
+    return tmp_path_factory.mktemp('hdf', numbered=False)
+
+
+@pytest.fixture(scope="session")
+def test_data() -> dict:
+    return {
+        'bool': False,
+        'complex': 1 + 2j,
+        'float': 3.0,
+        'int': 2,
+        'array': np.array([0.0, 1.0]),
+        'dict': {'a': 'string', 'b': np.array([1, 2, 3, 4, 5])},
+        'double_dict': {
+            'dict_1': {'1': 'a', '2': 123},
+            'dict_2': {'nan': np.nan, 'empty': np.empty((42, 23))},
+        },
+    }
+
+
+def test__save_data_to_hdf(hdf_dir: Path) -> None:
+
+    data_1 = np.random.normal(0, 1, 10)
+    data_2 = np.random.normal(1, 2, 3)
+
+    # Case 1
+    file_path = hdf_dir / 'test_1.hdf'
+    with h5py.File(file_path, 'w') as hdf_file:
+        with pytest.raises(TypeError) as type_error:
+            save_data_to_hdf(
+                hdf_file=hdf_file,
+                location='test_group',
+                name='test_dataset',
+                data=Path('.'),
+                overwrite=True,
+            )
+        assert 'not supported by HDF format' in str(type_error)
+
+    # Case 2
+    file_path = hdf_dir / 'test_1.hdf'
+    with h5py.File(file_path, 'w') as hdf_file:
+        save_data_to_hdf(
+            hdf_file=hdf_file,
+            location='test_group',
+            name='test_dataset',
+            data=data_1,
+            overwrite=True,
+        )
+        assert np.array_equal(hdf_file['test_group']['test_dataset'], data_1)
+
+    # Case 3 + 4
+    file_path = hdf_dir / 'test_1.hdf'
+    with h5py.File(file_path, 'a') as hdf_file:
+
+        # Case 3
+        with pytest.raises(KeyError) as key_error:
+            save_data_to_hdf(
+                hdf_file=hdf_file,
+                location='test_group',
+                name='test_dataset',
+                data=data_2,
+                overwrite=False,
+            )
+        assert 'Data set with name' in str(key_error)
+
+        # Case 4
+        save_data_to_hdf(
+            hdf_file=hdf_file,
+            location='test_group',
+            name='test_dataset',
+            data=data_2,
+            overwrite=True,
+        )
+        assert np.array_equal(hdf_file['test_group']['test_dataset'], data_2)
+
+
+def test__save_dict_to_hdf(
+    tmp_path_factory: TempPathFactory, hdf_dir: Path, test_data: dict
+) -> None:
 
     # Define location of test file in temporary directory
-    test_dir = tmp_path_factory.mktemp('hdf', numbered=False)
-    file_path = test_dir / 'test.hdf'
+    file_path = hdf_dir / 'test_2.hdf'
 
-    # Save to HDF; this test succeeds if no error occurs here
-    save_dict_to_hdf(dictionary=TEST_DICT, file_path=file_path)
+    # Case 1
+    save_dict_to_hdf(dictionary=test_data, file_path=file_path)
+
+    # Case 2 (repeat case 1 to check if overwrites work)
+    save_dict_to_hdf(dictionary=test_data, file_path=file_path)
+
+    # Case 3
+    with pytest.raises(TypeError) as type_error:
+        save_dict_to_hdf(dictionary={'path': Path('.')}, file_path=file_path)
+    assert 'Unsupported type' in str(type_error)
 
 
-def test__load_dict_from_hdf(tmp_path_factory: TempPathFactory) -> None:
+def test__load_dict_from_hdf(
+    tmp_path_factory: TempPathFactory, hdf_dir: Path, test_data: dict
+) -> None:
 
     # Define location of test file in temporary directory
-    test_dir = tmp_path_factory.getbasetemp() / 'hdf'
-    file_path = test_dir / 'test.hdf'
+    file_path = hdf_dir / 'test_2.hdf'
 
     # Read the test HDF file
     data = load_dict_from_hdf(file_path=file_path)
@@ -61,7 +133,7 @@ def test__load_dict_from_hdf(tmp_path_factory: TempPathFactory) -> None:
     # type changes, because h5py automatically casts built-in Python types to
     # their corresponding numpy types.
     deepdiff = DeepDiff(
-        t1=TEST_DICT,
+        t1=test_data,
         t2=data,
         ignore_type_in_groups=[
             (bool, np.bool_),
