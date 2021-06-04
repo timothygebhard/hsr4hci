f514c9f5af79a960c1dfff96980bf2cf2a034fbf
Timothy Gebhard <mail@timothygebhard.de>
2021-06-04 13:05:31 +0200

    Update script after refactoring various functions

--- experiments/02_temporal-binning/02_collect_hsr_results.py
+++ experiments/02_temporal-binning/02_collect_hsr_results.py
@@ -1,5 +1,5 @@
 """
-Collect results (= SNR) for HSR experiments.
+Collect results (= SNR, FPF, ...) for HSR experiments.
 """
 
 # -----------------------------------------------------------------------------
@@ -12,16 +12,15 @@ import time
 
 from astropy.units import Quantity
 
-import numpy as np
 import pandas as pd
 
 from hsr4hci.config import load_config, get_hsr4hci_dir
-from hsr4hci.coordinates import polar2cartesian
-from hsr4hci.fits import read_fits
 from hsr4hci.data import load_planets, load_psf_template, load_metadata
-from hsr4hci.evaluation import compute_optimized_snr
+from hsr4hci.fits import read_fits
+from hsr4hci.general import flatten_nested_dict
+from hsr4hci.metrics import compute_metrics
 from hsr4hci.psf import get_psf_fwhm
-from hsr4hci.units import set_units_for_instrument
+from hsr4hci.units import InstrumentUnitsContext
 
 
 # -----------------------------------------------------------------------------
@@ -71,19 +70,18 @@ if __name__ == '__main__':
 
     # Load the metadata and set up the unit conversions
     print('Loading metadata and setting up units...', end=' ', flush=True)
-    metadata = load_metadata(name=dataset)
-    set_units_for_instrument(
+    metadata = load_metadata(name_or_path=dataset)
+    instrument_units_context = InstrumentUnitsContext(
         pixscale=Quantity(metadata['PIXSCALE'], 'arcsec / pix'),
         lambda_over_d=Quantity(metadata['LAMBDA_OVER_D'], 'arcsec'),
-        verbose=False,
     )
     print('Done!', flush=True)
 
     # Load the PSF template and estimate its FWHM
     print('Loading PSF template...', end=' ', flush=True)
-    psf_template = load_psf_template(name=dataset).squeeze()
-    psf_fwhm = round(get_psf_fwhm(psf_template), 2)
-    print(f'Done! (psf_radius = {psf_fwhm})', flush=True)
+    psf_template = load_psf_template(name_or_path=dataset)
+    psf_fwhm = get_psf_fwhm(psf_template)
+    print(f'Done! (psf_radius = {psf_fwhm:.2f})', flush=True)
 
     # -------------------------------------------------------------------------
     # Compute SNRs for each binning factor
@@ -116,64 +114,47 @@ if __name__ == '__main__':
 
         print(f'Collecting SNR for factor = {factor}...', end=' ', flush=True)
 
-        # Define path to experiment directory
+        # Define path to experiment directory; load experiment config
         experiment_dir = main_dir / f'factor_{factor}'
-
-        # Load experiment config
         config = load_config(experiment_dir / 'config.json')
-
-        # Load information about the planets in the dataset
-        planets = load_planets(**config['dataset'])
 
         # Load the FITS file with all signal estimates
         try:
             file_path = experiment_dir / 'results' / 'signal_estimate.fits'
-            signal_estimate = np.asarray(read_fits(file_path))
-            frame_size = signal_estimate.shape
+            signal_estimate = read_fits(file_path, return_header=False)
         except FileNotFoundError:
             print('Failed!', flush=True)
             continue
 
-        # Get planet parameters and position
-        parameters = planets[planet]
-        planet_position = polar2cartesian(
-            separation=Quantity(parameters['separation'], 'arcsec'),
-            angle=Quantity(parameters['position_angle'], 'degree'),
-            frame_size=frame_size,
+        # Get expected position of the planet (in polar coordinates)
+        planet_parameters = load_planets(**config['dataset'])[planet]
+        planet_position = (
+            Quantity(planet_parameters['separation'], 'arcsec'),
+            Quantity(planet_parameters['position_angle'], 'degree'),
         )
 
-        # Compute the figures of merit
-        try:
-            ignore_neighbors = 1
-            results_dict = compute_optimized_snr(
+        # Compute the metrics (SNR, FPF, ...), add binning factor to the
+        # flattened result dictionary, and store it
+        with instrument_units_context:
+            tmp_results_dict, _ = compute_metrics(
                 frame=signal_estimate,
-                position=planet_position,
+                polar_position=planet_position,
                 aperture_radius=Quantity(psf_fwhm / 2, 'pixel'),
-                ignore_neighbors=ignore_neighbors,
+                planet_mode='FS',
+                noise_mode='P',
+                search_radius=Quantity(1, 'pixel'),
+                exclusion_angle=None,
             )
-        except ValueError:
-            ignore_neighbors = 0
-            results_dict = compute_optimized_snr(
-                frame=signal_estimate,
-                position=planet_position,
-                aperture_radius=Quantity(psf_fwhm / 2, 'pixel'),
-                ignore_neighbors=ignore_neighbors,
-            )
+            results_dict = flatten_nested_dict(tmp_results_dict)
+            results_dict = {**{'factor': factor}, **results_dict}
+            results.append(results_dict)
 
-        # Store the results for the current combination of binning factor
-        # and number of principal components
-        results.append(
-            dict(
-                factor=factor,
-                snr=results_dict['snr'],
-            )
-        )
         print('Done!', flush=True)
 
     # Convert the results to a pandas data frame and save as a TSV file
     print('\nSaving results to TSV...', end=' ', flush=True)
     results_df = pd.DataFrame(results)
-    results_df.to_csv(main_dir / f'snr__{planet}.tsv', sep='\t')
+    results_df.to_csv(main_dir / f'metrics__{planet}.tsv', sep='\t')
     print('Done', flush=True)
 
     # -------------------------------------------------------------------------
