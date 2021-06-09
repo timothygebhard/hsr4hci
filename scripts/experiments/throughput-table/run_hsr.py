adc8ec328b2d8a7663e819ae49eb9808a1c035a1
Timothy Gebhard <mail@timothygebhard.de>
2021-06-09 17:01:03 +0200

    Update script after refactoring various functions

--- scripts/experiments/throughput-table/run_hsr.py
+++ scripts/experiments/throughput-table/run_hsr.py
@@ -24,10 +24,12 @@ from hsr4hci.fits import save_fits
 from hsr4hci.forward_modeling import add_fake_planet
 from hsr4hci.hypotheses import get_all_hypotheses
 from hsr4hci.masking import get_roi_mask, get_positions_from_mask
+from hsr4hci.match_fraction import get_all_match_fractions
 from hsr4hci.positions import get_injection_position
-from hsr4hci.match_fraction import get_all_match_fractions, get_selection_mask
+from hsr4hci.psf import get_psf_fwhm
+from hsr4hci.residuals import get_residual_selection_mask
 from hsr4hci.training import train_all_models
-from hsr4hci.units import set_units_for_instrument
+from hsr4hci.units import InstrumentUnitsContext
 
 
 # -----------------------------------------------------------------------------
@@ -99,12 +101,14 @@ if __name__ == '__main__':
     selected_keys = config['observing_conditions']['selected_keys']
     n_signal_times = config['n_signal_times']
 
-    # Activate the unit conversions for this instrument
-    set_units_for_instrument(
+    # Define the unit conversion context for this data set
+    instrument_units_context = InstrumentUnitsContext(
         pixscale=Quantity(pixscale, 'arcsec / pixel'),
         lambda_over_d=Quantity(lambda_over_d, 'arcsec'),
-        verbose=False,
     )
+
+    # Fit the FWHM of the PSF (in pixels)
+    psf_fwhm = get_psf_fwhm(psf_template)
 
     # -------------------------------------------------------------------------
     # Inject a fake planet into the stack
@@ -122,15 +126,18 @@ if __name__ == '__main__':
     # ... otherwise, add a fake planet with given parameters to the stack
     else:
 
+        # Convert separation from units of FWHM to pixel
+        separation *= psf_fwhm
+
         # Compute position at which to inject the fake planet
         print('Computing injection position...', end=' ', flush=True)
         injection_position = get_injection_position(
-            separation=Quantity(separation, 'lambda_over_d'),
+            separation=Quantity(separation, 'pixel'),
             azimuthal_position=azimuthal_position,
         )
         print(
-            f'Done! (separation = {injection_position[0]}, '
-            f'azimuthal_position = {injection_position[1]})',
+            f'Done! (separation = {separation:.1f} pixel, '
+            f'azimuthal_position = {azimuthal_position})',
             flush=True,
         )
 
@@ -160,28 +167,31 @@ if __name__ == '__main__':
     base_model_creator = BaseModelCreator(**config['base_model'])
 
     # Construct the mask for the region of interest (ROI)
-    roi_mask = get_roi_mask(
-        mask_size=frame_size,
-        inner_radius=Quantity(*config['roi_mask']['inner_radius']),
-        outer_radius=Quantity(*config['roi_mask']['outer_radius']),
-    )
+    with instrument_units_context:
+        roi_mask = get_roi_mask(
+            mask_size=frame_size,
+            inner_radius=Quantity(*config['roi_mask']['inner_radius']),
+            outer_radius=Quantity(*config['roi_mask']['outer_radius']),
+        )
 
     print('\nTraining models:', flush=True)
-    results = train_all_models(
-        roi_mask=roi_mask,
-        stack=stack,
-        parang=parang,
-        psf_template=psf_template,
-        obscon_array=observing_conditions.as_array(selected_keys),
-        selection_mask_config=config['selection_mask'],
-        base_model_creator=base_model_creator,
-        n_splits=config['n_splits'],
-        mode=config['mode'],
-        n_signal_times=n_signal_times,
-        n_roi_splits=1,
-        roi_split=0,
-        return_format='full',
-    )
+    with instrument_units_context:
+        results = train_all_models(
+            roi_mask=roi_mask,
+            stack=stack,
+            parang=parang,
+            psf_template=psf_template,
+            obscon_array=observing_conditions.as_array(selected_keys),
+            selection_mask_config=config['selection_mask'],
+            base_model_creator=base_model_creator,
+            n_train_splits=config['n_train_splits'],
+            train_mode=config['train_mode'],
+            n_signal_times=n_signal_times,
+            n_roi_splits=1,
+            roi_split=0,
+            return_format='full',
+        )
+    residuals = dict(results['residuals'])
     print()
 
     # NOTE: We *do not* save the full results here!
@@ -194,7 +204,7 @@ if __name__ == '__main__':
     print('Finding best hypothesis for each spatial pixel:', flush=True)
     hypotheses, similarities = get_all_hypotheses(
         roi_mask=roi_mask,
-        dict_or_path=results,
+        residuals=residuals,
         parang=parang,
         n_signal_times=n_signal_times,
         frame_size=frame_size,
@@ -214,7 +224,7 @@ if __name__ == '__main__':
     # Compute match fraction for every pixel
     print('\nComputing match fraction:', flush=True)
     mean_mf, median_mf, _ = get_all_match_fractions(
-        dict_or_path=results,
+        residuals=residuals,
         hypotheses=hypotheses,
         parang=parang,
         psf_template=psf_template,
@@ -235,7 +245,7 @@ if __name__ == '__main__':
     # Compute the selection mask that determines which residual type (default
     # or based on signal fitting / masking) is used for a pixel
     print('\nComputing selection mask for residuals...', end=' ', flush=True)
-    selection_mask, _, _, _ = get_selection_mask(
+    selection_mask, _, _, _, _ = get_residual_selection_mask(
         match_fraction=median_mf,
         parang=parang,
         psf_template=psf_template,
@@ -256,12 +266,8 @@ if __name__ == '__main__':
     # Keep track of the default and the hypothesis-based residuals, as we
     # will save those to ensure that a change in the way the selection mask
     # is determined does not require re-running all experiments.
-    default_residuals = np.array(results['default']['residuals'])
+    default_residuals = np.array(residuals['default'])
     hypothesis_residuals = np.full_like(default_residuals, np.nan)
-
-    # Keep track of the "final" residuals, that is, the combination of the
-    # default and hypothesis-based residuals determined by the selection_mask
-    residuals = np.full_like(default_residuals, np.nan)
 
     # Loop over all pixels in the ROI and select residuals
     for (x, y) in get_positions_from_mask(roi_mask):
@@ -270,16 +276,7 @@ if __name__ == '__main__':
         # residual for this pixel (i.e., the "best" residual that was obtained
         # with signal fitting / masking).
         signal_time = str(int(hypotheses[x, y]))
-        hypothesis_residuals[:, x, y] = np.array(
-            results[signal_time]['residuals'][:, x, y]
-        )
-
-        # Set the value of the "final" residuals for this pixel based on the
-        # value of the selection_mask
-        if bool(selection_mask[x, y]):
-            residuals[:, x, y] = hypothesis_residuals[:, x, y]
-        else:
-            residuals[:, x, y] = default_residuals[:, x, y]
+        hypothesis_residuals[:, x, y] = residuals[signal_time][:, x, y]
 
     # Save the default residuals to FITS
     print('Saving default residuals to FITS...', end=' ', flush=True)
@@ -293,6 +290,13 @@ if __name__ == '__main__':
     save_fits(array=hypothesis_residuals, file_path=file_path)
     print('Done!', flush=True)
 
+    # Determine the "final" residuals, that is, the combination of the default
+    # and the hypothesis-based residuals determined by the selection mask
+    final_residuals = np.copy(default_residuals)
+    final_residuals[:, selection_mask] = hypothesis_residuals[
+        :, selection_mask
+    ]
+
     # -------------------------------------------------------------------------
     # STEP 6: Compute signal estimate
     # -------------------------------------------------------------------------
@@ -300,13 +304,11 @@ if __name__ == '__main__':
     # Compute final signal estimate
     print('Computing signal estimate...', end=' ', flush=True)
     signal_estimate = derotate_combine(
-        stack=residuals,
-        parang=parang,
-        mask=~roi_mask,
+        stack=final_residuals, parang=parang, mask=~roi_mask
     )
     print('Done!', flush=True)
 
-    # (Always) save the final signal estimate
+    # Save the final signal estimate
     print('Saving signal estimate to FITS...', end=' ', flush=True)
     file_path = results_dir / 'signal_estimate.fits'
     save_fits(array=signal_estimate, file_path=file_path)
