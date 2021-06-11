6e219897f31cfebcca994be117ec5352e339ba0b
Timothy Gebhard <mail@timothygebhard.de>
2021-06-11 11:58:15 +0200

    Refactor script to allow training in parallel

--- scripts/experiments/hypothesis-based/01_train_based_on_hypothesis.py
+++ scripts/experiments/hypothesis-based/01_train_based_on_hypothesis.py
@@ -13,7 +13,6 @@ import os
 import time
 
 from astropy.units import Quantity
-from sklearn.metrics.pairwise import cosine_similarity
 from tqdm.auto import tqdm
 
 import numpy as np
@@ -21,10 +20,13 @@ import numpy as np
 from hsr4hci.base_models import BaseModelCreator
 from hsr4hci.config import load_config
 from hsr4hci.data import load_dataset
-from hsr4hci.derotating import derotate_combine
 from hsr4hci.fits import save_fits
 from hsr4hci.forward_modeling import add_fake_planet
-from hsr4hci.masking import get_roi_mask, get_positions_from_mask
+from hsr4hci.masking import (
+    get_roi_mask,
+    get_positions_from_mask,
+    get_partial_roi_mask,
+)
 from hsr4hci.training import train_model_for_position
 from hsr4hci.units import InstrumentUnitsContext
 
@@ -55,6 +57,21 @@ if __name__ == '__main__':
         metavar='PATH',
         help='(Absolute) path to experiment directory.',
     )
+    parser.add_argument(
+        '--roi-split',
+        type=int,
+        default=0,
+        help='Index of the split to process; must be in [0, n_roi_splits).',
+    )
+    parser.add_argument(
+        '--n-roi-splits',
+        type=int,
+        default=1,
+        help=(
+            'Number of splits into which the ROI is divided (e.g., for '
+            'parallel training).'
+        ),
+    )
     args = parser.parse_args()
 
     # Get experiment directory
@@ -83,6 +100,10 @@ if __name__ == '__main__':
     # Quantities related to the size of the data set
     n_frames, x_size, y_size = stack.shape
     frame_size = (x_size, y_size)
+
+    # Command line arguments
+    roi_split = args.roi_split
+    n_roi_splits = args.n_roi_splits
 
     # Other shortcuts
     selected_keys = config['observing_conditions']['selected_keys']
@@ -154,14 +175,19 @@ if __name__ == '__main__':
     hypotheses[~affected_mask] = np.nan
     print('Done!', flush=True)
 
-    # Ensure that the results directory exists
-    results_dir = experiment_dir / 'results'
-    results_dir.mkdir(exist_ok=True)
+    # Ensure that the results directories exists
+    fits_dir = experiment_dir / 'fits'
+    fits_dir.mkdir(exist_ok=True)
+    partial_fits_dir = fits_dir / 'partial'
+    partial_fits_dir.mkdir(exist_ok=True)
 
     # Save hypotheses to HDF
     print('Saving hypotheses...', end=' ', flush=True)
-    file_path = results_dir / 'hypotheses.fits'
-    save_fits(hypotheses, file_path=file_path)
+    file_path = fits_dir / 'hypotheses.fits'
+    try:
+        save_fits(hypotheses, file_path=file_path)
+    except FileExistsError:
+        pass
     print('Done!', flush=True)
 
     # -------------------------------------------------------------------------
@@ -179,15 +205,15 @@ if __name__ == '__main__':
             outer_radius=Quantity(*config['roi_mask']['outer_radius']),
         )
 
+    # Create partial ROI mask
+    partial_roi_mask = get_partial_roi_mask(
+        roi_mask=roi_mask,
+        roi_split=args.roi_split,
+        n_roi_splits=args.n_roi_splits,
+    )
+
     # Initialize full residuals
     full_residuals = np.full(stack.shape, np.nan)
-    matches = np.full(frame_size, np.nan)
-    fractions = np.full(frame_size, np.nan)
-    full_alphas = np.full((n_train_splits,) + frame_size, np.nan)
-    full_pixel_coefs = np.full(
-        frame_size + (n_train_splits,) + frame_size, np.nan
-    )
-    full_planet_coefs = np.full((n_train_splits,) + frame_size, np.nan)
 
     # Loop over ROI, train models, and compute residuals
     # We use this manual loop here because we also want to access and store all
@@ -220,68 +246,12 @@ if __name__ == '__main__':
                 base_model_creator=base_model_creator,
                 expected_signal=expected_signal,
             )
-
-        # Unpack model_params
-        alphas = model_params['alphas']
-        selection_mask = model_params['selection_mask']
-        pixel_coefs = model_params['pixel_coefs']
-        planet_coefs = model_params['planet_coefs']
-
-        # Store the residuals, alphas, and coefficients
         full_residuals[:, x, y] = residuals
-        full_alphas[:, x, y] = alphas
-        full_planet_coefs[:, x, y] = planet_coefs
-        for i in range(n_train_splits):
-            full_pixel_coefs[x, y, i][selection_mask] = pixel_coefs[i]
 
-        # Compute the match between the residual and the hypothesized stack
-        a = np.nan_to_num(hypothesized_stack[:, x, y])
-        b = np.nan_to_num(full_residuals[:, x, y])
-        matches[x, y] = cosine_similarity(a.reshape(1, -1), b.reshape(1, -1))
-
-    print('\nSaving residuals to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'residuals.fits'
-    save_fits(full_residuals, file_path)
-    print('Done!', flush=True)
-
-    print('Saving fractions to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'fractions.fits'
-    save_fits(fractions, file_path)
-    print('Done!', flush=True)
-
-    print('Saving alphas to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'alphas.fits'
-    save_fits(full_alphas, file_path)
-    print('Done!', flush=True)
-
-    print('Saving selection masks to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'pixel_coefficients.fits'
-    save_fits(full_pixel_coefs, file_path)
-    print('Done!', flush=True)
-
-    print('Saving planet coefs to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'planet_coefficients.fits'
-    save_fits(full_planet_coefs, file_path)
-    print('Done!', flush=True)
-
-    print('Saving matches to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'matches.fits'
-    save_fits(matches, file_path)
-    print('Done!', flush=True)
-
-    # -------------------------------------------------------------------------
-    # STEP 3: Compute and save signal estimate
-    # -------------------------------------------------------------------------
-
-    print('\nComputing signal estimate...', end=' ', flush=True)
-    signal_estimate = derotate_combine(
-        stack=full_residuals, parang=parang, mask=~roi_mask
-    )
-    print('Done!', flush=True)
-
-    print('\nSaving signal estimate to FITS...', end=' ', flush=True)
-    file_path = results_dir / 'signal_estimate.fits'
-    save_fits(signal_estimate, file_path)
+    print('\nSaving (partial) residuals to FITS...', end=' ', flush=True)
+    file_name = f'residuals_{roi_split + 1:04d}-{n_roi_splits:04d}.fits'
+    file_path = partial_fits_dir / file_name
+    save_fits(full_residuals[:, partial_roi_mask], file_path)
     print('Done!', flush=True)
 
     # -------------------------------------------------------------------------
