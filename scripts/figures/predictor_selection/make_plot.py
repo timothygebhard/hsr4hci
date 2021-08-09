1fa76f0ddb8658147d111a14cc1199c1639edfd7
Timothy Gebhard <mail@timothygebhard.de>
2021-08-09 20:07:54 +0200

    Update script

--- scripts/figures/predictor_selection/make_plot.py
+++ scripts/figures/predictor_selection/make_plot.py
@@ -17,11 +17,12 @@ from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
 import numpy as np
 
-from hsr4hci.coordinates import get_center, cartesian2polar
+from hsr4hci.coordinates import get_center
 from hsr4hci.data import load_parang, load_psf_template
-from hsr4hci.forward_modeling import add_fake_planet
-from hsr4hci.general import rotate_position
-from hsr4hci.masking import get_exclusion_mask, get_predictor_pixel_selection_mask
+from hsr4hci.masking import (
+    get_exclusion_mask,
+    get_predictor_pixel_selection_mask,
+)
 from hsr4hci.plotting import get_transparent_cmap, disable_ticks
 
 
@@ -46,63 +47,45 @@ if __name__ == '__main__':
     frame_size = (65, 65)
     center = get_center(frame_size)
 
-    parang = load_parang(name=dataset)
-    psf_template = load_psf_template(name=dataset)
+    parang = load_parang(name_or_path=dataset)
+    psf_template = load_psf_template(name_or_path=dataset)
 
-    n_frames = len(parang)
+    position = (45, 16)
 
     # -------------------------------------------------------------------------
     # Load the data set; set up units; define shortcuts
     # -------------------------------------------------------------------------
 
-    signal_time = int(0.5 * (n_frames - 1))
-    position = (42, 23)
-
-    # Compute assumed planet path
-    n = n_frames // 100
-    parang_resampled = parang[::n]
-    final_position = rotate_position(
-        position=position,
-        center=center,
-        angle=float(parang[int(signal_time)]),
+    # Set up a new figure and adjust margins
+    fig, axes = plt.subplots(
+        ncols=2, figsize=(18.4 / 2.54 / 2, 18.4 / 2.54 / 4)
     )
-    _, planet_positions = add_fake_planet(
-        stack=np.zeros((len(parang_resampled), frame_size[0], frame_size[1])),
-        parang=parang_resampled,
-        psf_template=psf_template,
-        polar_position=cartesian2polar(
-            position=(final_position[0], final_position[1]),
-            frame_size=frame_size,
-        ),
-        magnitude=1,
-        extra_scaling=1,
-        dit_stack=1,
-        dit_psf_template=1,
-        return_planet_positions=True,
+    fig.subplots_adjust(
+        left=0.0025, bottom=0.0025, top=0.9975, right=0.9975, wspace=0
     )
-    planet_positions = np.asarray(planet_positions)
 
-    # Set up a new figure
-    fig, ax = plt.subplots(figsize=(4, 4))
-    ax.set_aspect('equal')
-    disable_ticks(ax)
+    # Disable ticks, fix aspect ratio
+    for ax in axes:
+        ax.set_aspect('equal')
+        disable_ticks(ax)
+    axes[1].axis('off')
 
     # Prepare grid for the pcolormesh()
     x_range = np.arange(frame_size[0])
     y_range = np.arange(frame_size[1])
     x, y = np.meshgrid(x_range, y_range)
 
+    # Get the predictor pixel selection mask
     selection_mask = get_predictor_pixel_selection_mask(
         mask_size=frame_size,
         position=position,
-        parang=parang,
-        signal_time=signal_time,
         psf_template=psf_template,
-        annulus_width=Quantity(0, 'pixel'),
-        radius_position=Quantity(12, 'pixel'),
-        radius_mirror_position=Quantity(12, 'pixel'),
+        radius_position=Quantity(16, 'pixel'),
+        radius_opposite=Quantity(16, 'pixel'),
     )
-    ax.pcolormesh(
+
+    # Plot the predictor pixel selection mask
+    axes[0].pcolormesh(
         x,
         y,
         selection_mask,
@@ -110,14 +93,15 @@ if __name__ == '__main__':
         shading='nearest',
     )
 
+    # Get the exclusion mask
     exclusion_mask = get_exclusion_mask(
         mask_size=frame_size,
         position=position,
-        parang=parang,
-        signal_time=signal_time,
         psf_template=psf_template,
     )
-    ax.pcolormesh(
+
+    # Plot the exclusion mask
+    axes[0].pcolormesh(
         x,
         y,
         exclusion_mask,
@@ -125,18 +109,11 @@ if __name__ == '__main__':
         shading='nearest',
     )
 
-    # Plot planet positions
-    ax.plot(
-        planet_positions[:, 0],
-        planet_positions[:, 1],
-        lw=3,
-        color='gray',
-        solid_capstyle='round',
-    )
+    # Plot the center of the frame
+    axes[0].plot(center[0], center[1], '*', color='gold', mew=1.5, ms=6)
 
-    # Plot the center of the frame and the target pixel
-    plt.plot(center[0], center[1], '+k')
-    plt.plot(
+    # Plot the target pixel
+    axes[0].plot(
         position[0],
         position[1],
         ms=3,
@@ -145,7 +122,40 @@ if __name__ == '__main__':
         markeredgecolor='black',
     )
 
-    legend_elements = [
+    axes[0].text(
+        2 * center[0] - position[0],
+        2 * center[1] - position[1],
+        "1",
+        ha="center",
+        va="center",
+        size=5.5,
+        bbox=dict(boxstyle="circle,pad=0.3", fc="white", ec="k", lw=0.5),
+    )
+    axes[0].text(
+        position[0] - 6,
+        position[1] - 10,
+        "2",
+        ha="center",
+        va="center",
+        size=6,
+        bbox=dict(boxstyle="circle,pad=0.3", fc="white", ec="k", lw=0.5),
+    )
+
+    # -------------------------------------------------------------------------
+    # Add custom legend to the plot
+    # -------------------------------------------------------------------------
+
+    handles = [
+        Line2D(
+            [0],
+            [0],
+            marker='*',
+            ls='',
+            color='gold',
+            mew=1.5,
+            ms=6,
+            label='Position of the host star',
+        ),
         Line2D(
             [],
             [],
@@ -156,33 +166,22 @@ if __name__ == '__main__':
             markerfacecolor='white',
             markeredgecolor='black',
         ),
-        Line2D(
-            [],
-            [],
-            lw=3,
-            color='gray',
-            solid_capstyle='round',
-            label='Assumed planet path',
-        ),
-        Patch(facecolor='C0', label='Used as predictors'),
+        Patch(facecolor='C0', label='Pixels used as predictors'),
         Patch(facecolor='C1', label='Exclusion region'),
     ]
 
-    ax.legend(
-        handles=legend_elements,
-        bbox_to_anchor=(0, 1.02, 1, 0.2),
-        loc="lower left",
-        mode="expand",
-        borderaxespad=0,
+    axes[1].legend(
+        handles=handles,
+        loc='center left',
+        borderaxespad=1,
+        borderpad=0,
         frameon=False,
-        ncol=2,
-        fontsize=8,
+        fontsize=6,
     )
 
     # Save plot to PDF
-    fig.tight_layout()
     file_path = Path('.') / 'predictor_selection.pdf'
-    plt.savefig(file_path, bbox_inches='tight', pad_inches=0.025)
+    plt.savefig(file_path, pad_inches=0.0)
 
     # -------------------------------------------------------------------------
     # Postliminaries
