9ed7757287d802d8755b7925c4596ca5022f7350
Timothy Gebhard <mail@timothygebhard.de>
2021-08-10 00:01:14 +0200

    Update script

--- experiments/05_pixel-coefficients/make_plots.py
+++ experiments/05_pixel-coefficients/make_plots.py
@@ -10,17 +10,14 @@ from pathlib import Path
 
 import time
 
-from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 from photutils import CircularAperture
 
-import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
-import numpy as np
 
 from hsr4hci.coordinates import get_center
-from hsr4hci.data import load_psf_template
+from hsr4hci.data import load_psf_template, load_metadata
 from hsr4hci.fits import read_fits
-from hsr4hci.plotting import disable_ticks, add_colorbar_to_ax, get_cmap
+from hsr4hci.plotting import plot_frame
 from hsr4hci.psf import get_psf_fwhm
 
 
@@ -46,13 +43,13 @@ if __name__ == '__main__':
     plots_dir.mkdir(exist_ok=True)
 
     # Loop over different data sets and positions
-    for dataset, binning_factor, (x, y) in (
-        ('beta_pictoris__lp', 1, (8 - 1, 30 - 1)),
-        ('beta_pictoris__lp', 1, (38 - 1, 10 - 1)),
-        ('beta_pictoris__mp', 1, (42 - 1, 19 - 1)),
-        ('beta_pictoris__mp', 1, (28 - 1, 40 - 1)),
-        ('r_cra__lp', 1, (12 - 1, 17 - 1)),
-        ('r_cra__lp', 1, (33 - 1, 45 - 1)),
+    for dataset, binning_factor, (x, y), loc in (
+        ('beta_pictoris__lp', 1, (8 - 1, 30 - 1), 1),
+        ('beta_pictoris__lp', 1, (38 - 1, 10 - 1), 1),
+        ('beta_pictoris__mp', 1, (42 - 1, 19 - 1), 1),
+        ('beta_pictoris__mp', 1, (28 - 1, 40 - 1), 1),
+        ('r_cra__lp', 1, (12 - 1, 17 - 1), 1),
+        ('r_cra__lp', 1, (33 - 1, 45 - 1), 2),
     ):
 
         start_time = time.time()
@@ -61,6 +58,10 @@ if __name__ == '__main__':
         # Load PSF and fit FWHM
         psf_template = load_psf_template(name_or_path=dataset)
         psf_fwhm = get_psf_fwhm(psf_template)
+
+        # Load metadata
+        metadata = load_metadata(name_or_path=dataset)
+        pixscale = metadata['PIXSCALE']
 
         # Load pixel coefficients from FITS
         fits_dir = Path(dataset) / f'binning-factor_{binning_factor}' / 'fits'
@@ -76,57 +77,27 @@ if __name__ == '__main__':
         center = get_center(frame_size)
 
         # Plot the frame with the coefficients
-        fig, ax = plt.subplots(figsize=(2.4, 4))
-        img = ax.pcolormesh(
-            np.arange(x_size),
-            np.arange(y_size),
-            frame,
-            vmin=-1,
-            vmax=1,
-            shading='nearest',
-            cmap=get_cmap('RdBu_r'),
-            snap=True,
-            rasterized=True,
+        fig, ax, cbar = plot_frame(
+            frame=frame,
+            positions=[],
+            labels=[],
+            pixscale=pixscale,
+            figsize=(2.9 / 2.54, 3.5 / 2.54),
+            subplots_adjust=dict(left=0.005, top=1, right=0.995, bottom=0.105),
+            scalebar_color='black',
+            limits=(-1, 1),
+            scalebar_loc=loc,
         )
-        disable_ticks(ax)
-        ax.set_aspect('equal')
 
         # Plot markers for position and center
-        plt.plot(x, y, 'x', mew=2, ms=5, color='lime')
-        plt.plot(center[0], center[1], '+', color='black')
-
-        # Plot circular aperture for opposite position
-        aperture = CircularAperture(
+        ax.plot(x, y, 'x', mew=1 , ms=3, color='lime')
+        CircularAperture(
             positions=(2 * center[0] - x, 2 * center[0] - y), r=psf_fwhm
-        )
-        # noinspection PyTypeChecker
-        aperture.plot(ls=':', lw=0.5, color='black')
-
-        # Create the scale bar and add it to the frame
-        scalebar = AnchoredSizeBar(
-            transform=ax.transData,
-            size=0.5 / 0.0271,
-            label='0.5"',
-            loc=2,
-            pad=1,
-            color='black',
-            frameon=False,
-            size_vertical=0,
-            fontproperties=fm.FontProperties(size=6),
-        )
-        ax.add_artist(scalebar)
-
-        # Add a color bar
-        cbar = add_colorbar_to_ax(img, fig, ax, where='bottom')
-        cbar.set_ticks([-0.66, -0.33, 0, 0.33, 0.66])
-        cbar.ax.set_xlabel('Normalized coefficient value', fontsize=6)
-        cbar.ax.xaxis.set_label_position('bottom')
-        cbar.ax.tick_params(labelsize=6)
+        ).plot(axes=ax, **dict(ls=':', lw=1, color='black'))
 
         # Save plot as a PDF
-        fig.tight_layout()
         file_path = plots_dir / f'{dataset}__{binning_factor}__{x}_{y}.pdf'
-        plt.savefig(file_path, bbox_inches='tight', dpi=600, pad_inches=0.005)
+        plt.savefig(file_path, dpi=600, pad_inches=0)
 
         print(f'Done! ({time.time() - start_time:.1f} seconds)', flush=True)
 
